9b720e9f8ba4563fcec6af59a644036e78f563d8
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-06-02 00:11:30 +0530

    Time: 2 ms (81.27%), Space: 44.4 MB (46.74%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,39 +10,44 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
+
         if(lists.length == 0 || lists == null) {
             return null;
-        }
-        return mergeLists(lists, 0, lists.length -1);
+        } 
+
+        return mergeLists(lists, 0, lists.length - 1);
     }
 
-    ListNode mergeLists(ListNode[] lists, int start, int end) {
+    ListNode mergeLists(ListNode[] lists, int start,int end) {
+
         if(start == end) {
-            return lists[start];
+           return lists[start];
         }
+
         int mid = start + (end - start)/2;
+
         ListNode left = mergeLists(lists, start, mid);
         ListNode right = mergeLists(lists, mid+1, end);
 
         return merge(left, right);
     }
 
-    ListNode merge(ListNode list1, ListNode list2) {
-        if(list1 == null) {
-            return list2;
+    ListNode merge(ListNode l1, ListNode l2) {
+
+        if(l1 == null) {
+            return l2;
         }
 
-        if(list2 == null) {
-            return list1;
+        if(l2 == null) {
+            return l1;
         }
 
-        if(list1.val < list2.val) {
-            list1.next = merge(list1.next, list2);
-            return list1;
+        if(l1.val <= l2.val) {
+            l1.next = merge(l1.next, l2);
+            return l1;
         } else {
-            list2.next = merge(list1, list2.next);
-            return list2;
+            l2.next = merge(l1, l2.next);
+            return l2;
         }
     }
-
 }
