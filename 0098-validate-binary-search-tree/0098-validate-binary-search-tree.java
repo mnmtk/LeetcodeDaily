472fd8dd18e52ec705f34ab099b84e7142a16435
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-06-03 03:19:04 +0530

    Time: 0 ms (100%), Space: 43.4 MB (48.96%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) {
+            return false;
+        }
+
+        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    boolean isValid(TreeNode root, long min, long max) {
+
+        if(root.val <= min || root.val >= max) {
+            return false;
+        } 
+
+        if(root.left != null) {
+           if(!isValid(root.left, min, root.val)) return false;
+        }
+
+        if(root.right != null) {
+            if(!isValid(root.right, root.val, max)) return false;
+        }
+
+        return true;
+    }
+}
