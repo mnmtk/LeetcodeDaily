f1ffeeb0c37face1f5d6af1c2ef1960871b4acd5
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-06-03 01:25:33 +0530

    Time: 134 ms (5%), Space: 56.4 MB (6.58%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,18 +1,17 @@
 class TrieNode{
-    TrieNode[] next = new TrieNode[26];
+    TrieNode [] next = new TrieNode[26];
     String word = null;
 }
-
 class Trie {
     TrieNode root;
     public Trie() {
-       root = new TrieNode();
+        root = new TrieNode();
     }
-    
+
     public void insert(String word) {
         TrieNode p = root;
         for(char c : word.toCharArray()) {
-            if(p.next[c - 'a'] == null) {
+            if (p.next[c  - 'a'] == null) {
                 p.next[c - 'a'] = new TrieNode();
             }
             p = p.next[c - 'a'];
@@ -25,26 +24,34 @@ class Trie {
     }
 
     public boolean searchHelper(String word, int index, TrieNode dict) {
-        if(index == word.length()) return dict.word != null;
+        if (index == word.length()) {
+            return dict.word != null;
+        }
 
-        char c = word.charAt(index);    
-        return dict.next[c - 'a'] != null 
-        ? searchHelper(word, index +1, dict.next[c - 'a'])
-        : false;
+        if(dict.next[word.charAt(index) - 'a'] != null) {
+             System.out.print("here" + index + word.charAt(index));
+            return searchHelper(word, index + 1, dict.next[word.charAt(index) - 'a']);
+        } else {
+             System.out.print("here" + word.charAt(index));
+            return false;
+        }
     }
     
     public boolean startsWith(String prefix) {
-      return searchHelperWith(prefix, 0, root);
+       return searchHelperWith(prefix, 0, root);
     }
 
-
     public boolean searchHelperWith(String word, int index, TrieNode dict) {
-        if(index == word.length()) return true;
+        if (index == word.length()) {
+            return true;
+        }
 
-        char c = word.charAt(index);    
-        return dict.next[c - 'a'] != null 
-        ? searchHelperWith(word, index +1, dict.next[c - 'a'])
-        : false;
+        if(dict.next[word.charAt(index) - 'a'] != null) {
+           
+            return searchHelperWith(word, index + 1, dict.next[word.charAt(index) - 'a']);
+        } else {
+            return false;
+        }
     }
 }
 
