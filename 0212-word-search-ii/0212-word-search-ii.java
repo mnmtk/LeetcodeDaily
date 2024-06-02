edb83aad136cd3163473936241469f4a96316c68
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-06-02 22:06:43 +0530

    Time: 227 ms (57.18%), Space: 44.8 MB (70.02%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -3,42 +3,37 @@ class Solution {
         Trie[] next = new Trie[26];
         String word = null;
     }
-    Set<String> ans = new HashSet<>();
+
+    Set<String> foundWords = new HashSet<>();
+    
     public List<String> findWords(char[][] board, String[] words) {
-       Trie trie = buildTrie(words);   
-
-       int row = board.length;
-       int col = board[0].length;
-
-       for(int i = 0 ; i< row ;i++) {
-        for(int j = 0 ; j< col ; j++) {
-            dfs(trie, i, j, board);
+        Trie trie = buildTrie(words);
+        for(int i = 0 ;i<board.length;i++) {
+            for(int j = 0;j<board[0].length;j++) {
+                dfs(board, i, j, trie);
+            }
         }
-       } 
-    return new ArrayList<>(ans);
-
+        return new ArrayList<>(foundWords);
     }
 
-    void dfs (Trie trie, int i, int j, char[][] board) {
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length 
-        || board[i][j] == '#' || trie.next[board[i][j] - 'a'] == null) {
-            return;
-        }
+    void dfs(char[][] grid, int i, int j, Trie trie) {
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '#' ||
+        trie.next[grid[i][j] - 'a'] == null) return;
 
-        if(trie.next[board[i][j] - 'a'].word != null){
-            ans.add(trie.next[board[i][j] - 'a'].word);
+        if(trie.next[grid[i][j] - 'a'].word != null) {
+            foundWords.add(trie.next[grid[i][j] - 'a'].word);
         }
-        trie = trie.next[board[i][j] - 'a'];
-        char c = board[i][j];
-        board[i][j] = '#';
+        trie = trie.next[grid[i][j] - 'a'];
+        char c = grid[i][j];
+        grid[i][j] = '#';
 
         //dfs
-        dfs(trie, i-1, j, board);
-        dfs( trie, i+1, j,  board);
-        dfs( trie, i, j-1,  board);
-        dfs( trie, i, j+1,  board);
-    
-        board[i][j] = c;
+        dfs(grid, i+1, j, trie);
+        dfs(grid, i-1, j, trie);
+        dfs(grid, i, j+1, trie);
+        dfs(grid, i, j-1, trie);
+        
+        grid[i][j] = c;
     }
 
     Trie buildTrie(String[] words) {
@@ -54,5 +49,5 @@ class Solution {
             p.word = w;
         }
         return root;
-    }
+    } 
 }
