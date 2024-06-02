b89ce573afc117ab61f63cc3a26a627f3c4fa9e8
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-06-02 23:07:02 +0530

    Time: 228 ms (57.05%), Space: 44.8 MB (70.02%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -1,41 +1,41 @@
-
 class Solution {
-    class Trie {
+    class Trie{
         Trie[] next = new Trie[26];
         String word = null;
     }
-
     Set<String> ans = new HashSet<>();
     public List<String> findWords(char[][] board, String[] words) {
-        Trie trie = buildTrie(words);
-        for(int i = 0; i< board.length ; i++) {
-            for(int j = 0 ; j<board[0].length;j++) {
-                dfs(board, i, j, trie);
-            }
-        }
+       Trie trie = buildTrie(words);   
 
-        return new ArrayList<>(ans);
+       int row = board.length;
+       int col = board[0].length;
+
+       for(int i = 0 ; i< row ;i++) {
+        for(int j = 0 ; j< col ; j++) {
+            dfs(trie, i, j, board);
+        }
+       }
+
+       return new ArrayList<>(ans);
     }
 
-    void dfs(char[][] board, int i, int j, Trie trie) {
-        if(i <0 || j<0 || i >= board.length || j >=board[0].length || board[i][j] == '#'
-        || trie.next[board[i][j] - 'a'] == null) {
-            return;
-        }
+    void dfs(Trie trie, int i, int j, char[][] board) {
+        if(i<0 || j< 0 || i >= board.length || j>= board[0].length || board[i][j] == '#' || trie.next[board[i][j] - 'a'] == null) return;
 
-        if(trie.next[board[i][j] - 'a'].word != null) {
+        if(trie.next[board[i][j] - 'a'].word != null){
             ans.add(trie.next[board[i][j] - 'a'].word);
         }
         trie = trie.next[board[i][j] - 'a'];
         char c = board[i][j];
         board[i][j] = '#';
-        
-        dfs(board, i+1, j, trie);
-        dfs(board, i, j+1, trie);
-        dfs(board, i-1, j, trie);
-        dfs(board, i, j-1, trie);
+        //dfs
+        dfs(trie, i-1, j, board);
+              dfs(trie, i+1, j, board);
+                    dfs(trie, i, j+1, board);
+                          dfs(trie, i, j-1, board);
 
         board[i][j] = c;
+
     }
 
     Trie buildTrie(String[] words) {
