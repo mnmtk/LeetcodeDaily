aeb7804d37f55a488932b70874034941d15d62db
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-06-07 00:27:24 +0530

    Time: 30 ms (14.67%), Space: 55 MB (21.81%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.java
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.java
@@ -11,11 +11,8 @@ class RecentCounter {
     public int ping(int t) {
         pings.add(t);
         int lowerBound = t - 3000;
-
-        // Find the index of the first element >= lowerBound using custom binary search
         int index = binarySearch(pings, lowerBound);
 
-        // The number of pings within the last 3000 milliseconds is from index to the end of the list
         return pings.size() - index;
     }
 
@@ -23,10 +20,10 @@ class RecentCounter {
         int left = 0;
         int right = array.size() - 1;
 
-        while (right >= left) {
+        while (right > left) {
             int midpoint = (left + right) / 2;
             if (array.get(midpoint) == value) return midpoint;
-            if (array.get(midpoint) > value) right = midpoint - 1;
+            if (array.get(midpoint) > value) right = midpoint;
             if (array.get(midpoint) < value) left = midpoint + 1;
         }
         return left;
