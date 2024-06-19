1f871681c1eb53b29ca7620d5be60255a616d3fd
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-06-19 13:52:44 +0530

    Time: 15 ms (40.72%), Space: 48.4 MB (60.81%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -6,23 +6,23 @@ class Solution {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(
-            (n1,n2) -> map.get(n1) - map.get(n2)
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(
+            (n1, n2) -> map.get(n1) - map.get(n2)
         );
-
+        
         for(int key : map.keySet()) {
-            pq.offer(key);
-
-            if(pq.size() > k) {
-                pq.remove();
+            minHeap.offer(key);
+            if(minHeap.size() > k) {
+                minHeap.remove();
             }
         }
 
         int[] ans = new int[k];
-        for(int i = 0 ;i<k;i++) {
-            ans[i] = pq.poll();
+
+        for(int i=0; i<k;i++) {
+            ans[i] = minHeap.poll();
         }
-        
+
         return ans;
     }
 }
