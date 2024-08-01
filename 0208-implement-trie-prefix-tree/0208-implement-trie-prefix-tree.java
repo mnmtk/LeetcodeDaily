d97332d4c15189fa8fefb91f04208162e6ed78ec
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-08-01 14:21:34 +0530

    Time: 32 ms (93.44%), Space: 55.6 MB (21.24%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,57 +1,59 @@
-class TrieNode{
-    TrieNode [] next = new TrieNode[26];
-    String word = null;
+
+class TrieNode {
+        TrieNode[] next = new TrieNode[26];
+        String word = null;
 }
 class Trie {
     TrieNode root;
     public Trie() {
         root = new TrieNode();
     }
-
+    
     public void insert(String word) {
-        TrieNode p = root;
+        TrieNode dict = root;
         for(char c : word.toCharArray()) {
-            if (p.next[c  - 'a'] == null) {
-                p.next[c - 'a'] = new TrieNode();
+            if (dict.next[c - 'a'] == null) {
+                dict.next[c - 'a'] = new TrieNode();
             }
-            p = p.next[c - 'a'];
+            
+            dict = dict.next[c - 'a'];
         }
-        p.word = word;
+
+       dict.word = word;
     }
     
     public boolean search(String word) {
         return searchHelper(word, 0, root);
     }
 
-    public boolean searchHelper(String word, int index, TrieNode dict) {
-        if (index == word.length()) {
-            return dict.word != null;
+    public boolean searchHelper(String word, int index,TrieNode trie) {
+        if(index == word.length()) {
+            return trie.word != null;
         }
 
-        if(dict.next[word.charAt(index) - 'a'] != null) {
-             System.out.print("here" + index + word.charAt(index));
-            return searchHelper(word, index + 1, dict.next[word.charAt(index) - 'a']);
+        if(trie.next[word.charAt(index) - 'a'] != null) {
+            return searchHelper(word, index+1, trie.next[word.charAt(index) - 'a']);
         } else {
-             System.out.print("here" + word.charAt(index));
+            return false;
+        }
+    }
+
+
+    public boolean searchHelper2(String word, int index, TrieNode trie) {
+        if(index == word.length()) {
+            return true;
+        }
+
+        if(trie.next[word.charAt(index) - 'a'] != null) {
+            return searchHelper2(word, index+1, trie.next[word.charAt(index) - 'a']);
+        } else {
             return false;
         }
     }
     
+    
     public boolean startsWith(String prefix) {
-       return searchHelperWith(prefix, 0, root);
-    }
-
-    public boolean searchHelperWith(String word, int index, TrieNode dict) {
-        if (index == word.length()) {
-            return true;
-        }
-
-        if(dict.next[word.charAt(index) - 'a'] != null) {
-           
-            return searchHelperWith(word, index + 1, dict.next[word.charAt(index) - 'a']);
-        } else {
-            return false;
-        }
+         return searchHelper2(prefix, 0, root);
     }
 }
 
