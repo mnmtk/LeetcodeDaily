3c16ec692710650411bf90b9a8a3c1262e0331a9
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-08-06 21:53:15 +0530

    Time: 0 ms (100%), Space: 42 MB (45.95%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,29 +1,31 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low = 0, high = nums.length - 1;
+        int left = 0;
+        int right = nums.length - 1;
 
-        while (low <= high) {
-            int mid = (low + high) / 2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
             if (nums[mid] == target) {
-                return mid;
+                return mid; // Return the index of the target
             }
 
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
+            // Determine which segment is sorted
+            if (nums[left] <= nums[mid]) { // Left segment is sorted
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1; // Search in the left segment
                 } else {
-                    low = mid + 1;
+                    left = mid + 1; // Search in the right segment
                 }
-            } else {
-                if (nums[mid] < target && target <= nums[high]) {
-                    low = mid + 1;
+            } else { // Right segment is sorted
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1; // Search in the right segment
                 } else {
-                    high = mid - 1;
+                    right = mid - 1; // Search in the left segment
                 }
             }
         }
 
-        return -1;
+        return -1; // Target not found
     }
 }
