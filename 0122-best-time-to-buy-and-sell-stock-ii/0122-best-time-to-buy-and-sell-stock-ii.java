241dd078228115121adfdf9716a799b9373e109b
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-08-25 05:42:47 +0530

    Time: 1 ms (90.02%), Space: 46.3 MB (8.27%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,15 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        List<List<Integer>> ans = new ArrayList<>();
         int profit = 0;
-        for(int i=1;i<prices.length;i++) {
-            if(prices[i] > prices[i-1]) {
-                profit += prices[i] - prices[i-1];
-                ans.add(Arrays.asList(i-1, i));
-            }
-        }    
 
-        System.out.print(ans);    
+        for(int i=1 ; i < prices.length ; i++) {
+
+            if(prices[i] > prices[i-1]) {
+            profit += prices[i] - prices[i - 1];
+            }
+        }
+
         return profit;
+        
     }
 }
