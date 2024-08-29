e16977f9117dcbc5376408198eb8f3cad647d994
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-08-29 19:12:21 +0530

    Time: 533 ms (22.01%), Space: 53 MB (12.9%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,18 +1,19 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
-        Arrays.sort(nums); //nlogn
+        Arrays.sort(nums); //nlogn list is sorted
         int n = nums.length;
 
         Set<List<Integer>> ans = new HashSet<>();
       
-        for(int i = 0; i < n; i++) {
-            int p1=i+1;
+        for(int i = 0; i < n; i++) { //for every number, 
+            int p1=i + 1;
             int p2=n - 1;
             while(p1 < p2) {
                 int sum = nums[p1] + nums[p2] + nums[i];
 
                 if(sum == 0) {
+                    
                     ArrayList<Integer> sp = new ArrayList<>();
                     sp.add(nums[i]);
                     sp.add(nums[p1]);
