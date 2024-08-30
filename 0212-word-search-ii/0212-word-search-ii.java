d565f436fa26b71a8f65a56fc64b044532eb4d18
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-08-31 02:14:15 +0530

    Time: 220 ms (63.22%), Space: 44.9 MB (67.54%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -13,7 +13,7 @@ class Solution {
 
 
         for(int i = 0 ; i < board.length ; i ++) {
-            for(int j = 0 ; j < board[0].length - 1; j ++) {
+            for(int j = 0 ; j < board[0].length; j ++) {
                 dfs(board, i , j, root);
             }
         }
