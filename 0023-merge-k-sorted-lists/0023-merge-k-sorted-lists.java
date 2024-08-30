e2ac84fac5081c831df1040dc9553f0ae69222ec
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-08-31 01:15:40 +0530

    Time: 1 ms (100%), Space: 45 MB (8.71%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -13,45 +13,47 @@ class Solution {
         if(lists == null || lists.length == 0) {
             return null;
         }
-        return mergeLists(lists, 0, lists.length - 1);
+        return merge(lists, 0, lists.length - 1);
     }
 
-    public ListNode mergeLists(ListNode[] lists, int start, int end) {
+    public ListNode merge(ListNode[] lists, int start, int end) {
+
         if(start == end) {
             return lists[start];
         }
-        int mid = start + (end - start)/2;
-        ListNode left = mergeLists(lists, start, mid);
-        ListNode right = mergeLists(lists, mid + 1, end);
+        
+        int mid = start + (end - start) / 2;
+        ListNode left = merge(lists, start, mid);
+        ListNode right = merge(lists, mid + 1, end);
 
         return merge(left, right);
     }
 
-    ListNode merge(ListNode l1, ListNode l2) {
+    public ListNode merge(ListNode l1, ListNode l2) {
+
         ListNode dummy = new ListNode(0);
         ListNode dt = dummy;
 
-        while(l1!= null && l2!=null ) {
-
-            if(l1.val <=l2.val) {
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
                 dt.next = l1;
                 l1 = l1.next;
-                dt = dt.next;
-            }
-            else if(l2.val <= l1.val) {
+            } else {
                 dt.next = l2;
-                l2 =l2.next;
-                dt = dt.next;
+                l2 = l2.next;
             }
+            dt = dt.next;
         }
 
         if(l1 != null) {
             dt.next = l1;
-        } else {
+        } 
+
+        if(l2 !=null ) {
             dt.next = l2;
         }
 
         return dummy.next;
-
     }
+    
 }
