eb67d0783a2dece99ef87aca2a71511a8785eb23
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-09-05 22:40:57 +0530

    Time: 223 ms (32.57%), Space: 77.4 MB (5.44%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,32 +1,63 @@
 class Solution {
-    public int longestConsecutive(int[] nums) {
 
-        Set<Integer> set = new HashSet<>();
-        int max = 0;
 
-        for(int num : nums) {
-            set.add(num);
+    public class DisjointSet {
+
+    final Map<Integer, Integer> parents;
+    final Map<Integer, Integer> size;
+
+    DisjointSet(int[] nums) {
+        this.parents = new HashMap<>();
+        this.size = new HashMap<>();
+
+        for (Integer n : nums) {
+            parents.put(n, n);
+            size.put(n, 1);
+        }
+    }
+
+    int union(int a, int b) {
+        if(parents.containsKey(a) && parents.containsKey(b)) {
+            int parentA = findParent(a);
+            int parentB = findParent(b);
+
+            if(parentA != parentB) {
+                if(size.get(parentA) > size.get(parentB)) {
+                    parents.put(parentB, parentA);
+                    return size.compute(parentA, (k, v) -> v + size.get(parentB));
+                } else {
+                    parents.put(parentA, parentB);
+                    return size.compute(parentB, (k, v) -> v + size.get(parentA));
+                }
+            }
         }
 
-        for(int num : nums) {
-            int thisMax = 1;
-            int prev = num -1;
-            int next = num+1;
+        return 1;
+    }
 
-            while(set.remove(prev--)) {
-                thisMax++;
-            }
+    int findParent(int i) {
+        while(i != parents.get(i)) {
+            //path compression ?
+            parents.put(i, i = parents.get(i));
+        }
 
-            while(set.remove(next++)) {
-                thisMax++;
-            }
+        return parents.get(i);
+    }
 
+    }
 
-            max = Math.max(max, thisMax);
-            
+  
+
+    public int longestConsecutive(int[] nums) {
+        DisjointSet sequence = new DisjointSet(nums);
+
+        int max = 0;
+        for(Integer n : nums) {
+            max = Math.max(max, Math.max(sequence.union(n, n-1), sequence.union(n, n + 1)));
         }
 
         return max;
+        
         
     }
 }
