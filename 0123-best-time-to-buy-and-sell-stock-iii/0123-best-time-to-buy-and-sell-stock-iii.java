98a19aab95e281416dcc35804f44a7dc580614b8
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-09-12 00:03:12 +0530

    Time: 2 ms (94.01%), Space: 61.2 MB (57.01%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,25 +1,24 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0) return 0;
 
-        int n = prices.length;
-        int[] firstBuySell = new int[n];
+        if(prices == null || prices.length == 0) return 0;
 
-        
+        int[] maxProfitLeft = new int[prices.length];
         int minPrice = prices[0];
-        for (int i = 1; i < n; i++) {
-            minPrice = Math.min(minPrice, prices[i]);
-            firstBuySell[i] = Math.max(firstBuySell[i - 1], prices[i] - minPrice);
+        for(int i = 1; i < prices.length; i++) {
+           minPrice = Math.min(minPrice, prices[i]);
+           maxProfitLeft[i] = Math.max(prices[i] - minPrice, maxProfitLeft[i-1]);
         }
 
-       
-        int maxPrice = prices[n - 1];
-        int maxTotalProfit = firstBuySell[n - 1]; 
-        for (int i = n - 2; i >= 0; i--) {
+        int[] maxPricesRight = new int [prices.length];
+        int max = 0;
+        int maxPrice = prices[ prices.length - 1];
+        for(int i = prices.length - 1; i > 0; i--) {
             maxPrice = Math.max(maxPrice, prices[i]);
-            maxTotalProfit = Math.max(maxTotalProfit, maxPrice - prices[i] + firstBuySell[i]);
+            maxPricesRight[i] = maxPrice - prices[i];
+            max = Math.max(maxPricesRight[i] + maxProfitLeft[i], max);
         }
-
-        return maxTotalProfit;
+        
+        return max;
     }
 }
