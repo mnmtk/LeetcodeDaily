d4f4e63e2daa1d9032386bbab9807f869f5ee76a
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-10-11 22:12:35 +0530

    Time: 21 ms (9.48%), Space: 55.1 MB (85.84%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -13,35 +13,35 @@
 //     }
 // }
 
-// class Solution {
-//     public boolean containsDuplicate(int[] nums) {
-//         Arrays.sort(nums); 
-
-//         for (int i = 0; i < nums.length - 1; i++) {
-//                 if (nums[i] == nums[i+1]) {
-//                     return true;
-//                 }
-//         }
-//         return false;
-//     }
-    
-// }
-
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i - 1] == nums[i])
-                return true;
-            else if (nums[i] < nums[i - 1]){
-                int temp = nums[i - 1];
-                for(int j = i - 2; j >= 0; j--){
-                    if(nums[i] == nums[j])
-                        return true;
+        Arrays.sort(nums); 
+
+        for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] == nums[i+1]) {
+                    return true;
                 }
-                nums[i - 1] = nums[i];
-                nums[i] = temp;
-            }
         }
         return false;
     }
+    
 }
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         for(int i = 1; i < nums.length; i++){
+//             if(nums[i - 1] == nums[i])
+//                 return true;
+//             else if (nums[i] < nums[i - 1]){
+//                 int temp = nums[i - 1];
+//                 for(int j = i - 2; j >= 0; j--){
+//                     if(nums[i] == nums[j])
+//                         return true;
+//                 }
+//                 nums[i - 1] = nums[i];
+//                 nums[i] = temp;
+//             }
+//         }
+//         return false;
+//     }
+// }
