534693bf382266446fcbdab00ad878f0be98e36d
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-10-11 17:17:54 +0530

    Time: 3 ms (90.41%), Space: 60.7 MB (81.42%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -5,20 +5,25 @@ class Solution {
 
         int[] maxProfitLeft = new int[prices.length];
         int minPrice = prices[0];
-        for(int i = 1; i < prices.length; i++) {
-           minPrice = Math.min(minPrice, prices[i]);
-           maxProfitLeft[i] = Math.max(prices[i] - minPrice, maxProfitLeft[i-1]);
+
+        for(int i=1; i<prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            maxProfitLeft[i] = Math.max(prices[i] - minPrice, maxProfitLeft[i-1]);
         }
+
 
         int[] maxPricesRight = new int [prices.length];
         int max = 0;
-        int maxPrice = prices[ prices.length - 1];
+        int maxPrice = prices[prices.length - 1];
+
         for(int i = prices.length - 1; i > 0; i--) {
             maxPrice = Math.max(maxPrice, prices[i]);
             maxPricesRight[i] = maxPrice - prices[i];
+
             max = Math.max(maxPricesRight[i] + maxProfitLeft[i], max);
         }
-        
+
         return max;
+        
     }
 }
