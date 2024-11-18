61c38840d6b7e617f912eaff3a42a3495bb55428
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-11-19 00:02:49 +0530

    Time: 0 ms (100%), Space: 40.5 MB (30%) - LeetHub

--- 1307-ugly-number-iii/1307-ugly-number-iii.java
+++ 1307-ugly-number-iii/1307-ugly-number-iii.java
@@ -1,33 +1,44 @@
 class Solution {
     public int nthUglyNumber(int n, int a, int b, int c) {
-        int low = 1, high =2*(int)1e9;
+        int low = 1, high = 2 * (int) 1e9;
         
-        int ab = lcm(a,b), bc = lcm(b,c), ca = lcm(c,a), abc = lcm(a,bc);
-        
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            
-            int count = mid/a + mid/b + mid/c - mid/ab - mid/bc - mid/ca + mid/abc;
-            if(count>=n){
+        int ab = lcm(a, b);
+        int bc = lcm(b, c);
+        int ca = lcm(c, a);
+        int abc = lcm(a, bc);
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            int count = mid / a + mid / b + mid / c 
+                      - mid / ab - mid / bc - mid / ca 
+                      + mid / abc;
+
+            if (count >= n) {
                 high = mid - 1;
+            } else {
+                low = mid + 1;
             }
-            else low = mid + 1;
         }
+
         return low;
     }
-    private int lcm(int x,int y){
-        int a = x, b = y; 
-        while(a!=0){
-            int t = a;
-            a = b%a;
-            b = t;
+
+    private int gcd(int x, int y) {
+        while (y != 0) {
+            int temp = y;
+            y = x % y;
+            x = temp;
         }
-        int ans = 0;
-        try{
-            ans = (x>y) ? Math.multiplyExact(y,(x/b)) : Math.multiplyExact(x,(y/b));  // x*y/gcd(x,y)
-        }catch(Exception e){
-            ans = Integer.MAX_VALUE;
+        return x;
+    }
+
+    private int lcm(int x, int y) {
+        try {
+            int gcdValue = gcd(x, y);
+            return Math.multiplyExact(x / gcdValue, y); 
+        } catch (ArithmeticException e) {
+            return Integer.MAX_VALUE;
         }
-        return ans;
     }
 }
