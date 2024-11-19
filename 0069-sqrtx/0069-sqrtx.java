9dda77093ace8b47395a239dc1065a2f20c92800
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-11-19 22:29:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,20 +1,19 @@
 class Solution {
-    public int mySqrt(long x) {
-        
-        long left = 1;
-        long right = x + 1;
+    public int mySqrt(int x) {
+
+        int left = 1;
+        int right = x;
 
         while(left < right) {
-            long mid = left + (right-left)/2;
-
-            if(mid*mid <= x) {
-                left = mid+1;
-            } else {
+            int mid = right + (left - right)/2 ;
+            if(mid*mid > x) {
                 right = mid;
+            } else {
+                left = mid + 1;
             }
-        } 
 
-        return (int)left - 1;
-        
+        }
+
+        return left -1 ;
     }
 }
