67503fa46c145cd50968e8eb9ece1b256a1febba
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-12-24 01:43:45 +0530

    Time: 1 ms (100%), Space: 45.8 MB (43.52%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,14 +1,14 @@
-class Solution {
+public class Solution {
     public boolean canJump(int[] nums) {
-        int furthest = 0;
-        int last = nums.length - 1;
 
-        for(int i =0 ;i <=furthest && furthest < last; ++i) {
-            furthest = Math.max(furthest, i + nums[i]);
+        int lastPos = nums.length - 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
         }
-
-        return furthest >= last;
-
         
+        return lastPos == 0;
     }
 }
