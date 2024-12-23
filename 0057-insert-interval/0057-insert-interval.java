3d100fdbac7fe2c3de6bb31d6213f644cf26a0e2
mnmtk <137998025+mnmtk@users.noreply.github.com>
2024-12-24 02:02:43 +0530

    Time: 1 ms (99.13%), Space: 45 MB (28.18%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,19 +1,20 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-
         int start = newInterval[0];
         int end = newInterval[1];
         boolean inserted = false;
+
         ArrayList<int []> ans = new ArrayList<>();
 
         for(int[] interval : intervals) {
             int currStart = interval[0];
             int currEnd = interval[1];
-            if(start > currEnd || end < currStart) {
+
+            if(currStart > end  || currEnd < start) {
                 if(!inserted && start < currStart) {
-                    ans.add(new int[]{start, end});
                     inserted = true;
-                } 
+                    ans.add(new int[]{start, end});
+                }
                 ans.add(interval);
             } else {
                 start = Math.min(start, currStart);
@@ -22,8 +23,10 @@ class Solution {
         }
 
         if(!inserted) {
-            ans.add(new int[] {start, end});
+            ans.add(new int[]{start,end});
         }
+
         return ans.toArray(new int[ans.size()][2]);
+        
     }
 }
