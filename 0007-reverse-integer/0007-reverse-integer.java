4ed99fc6e6488681898c78dd3ace5411f9bfac2b
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-01-07 00:05:59 +0530

    Time: 1 ms (85.04%), Space: 41.2 MB (29.83%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,12 +1,23 @@
 class Solution {
     public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (
+                rev > Integer.MAX_VALUE / 10 ||
+                (rev == Integer.MAX_VALUE / 10 && pop > 7)
+            ) return 0;
 
-       String reversed = new StringBuilder().append(Math.abs(x)).reverse().toString();
-try {
-    return (x < 0) ? Integer.parseInt(reversed) * -1 : Integer.parseInt(reversed);
-} catch (NumberFormatException e) {
-    return 0;
-}
-        
+
+            if (
+                rev < Integer.MIN_VALUE / 10 ||
+                (rev == Integer.MIN_VALUE / 10 && pop < -8)
+            ) return 0;
+
+            
+            rev = rev * 10 + pop;
+        }
+        return rev;
     }
 }
