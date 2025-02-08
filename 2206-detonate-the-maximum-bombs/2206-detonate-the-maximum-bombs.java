82e6bc02a78329fde8b3a791120c7cd5c5f32d25
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-02-08 15:57:16 +0530

    Time: 185 ms (16.77%), Space: 44.8 MB (65.48%) - LeetHub

---
+++ 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int maximumDetonation(int[][] bombs) {
+
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        for(int i = 0;i < bombs.length; i++) { //O(Nsquare)
+            for(int j = 0; j < bombs.length; j++) {
+                int x1 = bombs[i][0];
+                int x2 = bombs[j][0];
+
+                int y1 = bombs[i][1];
+                int y2 = bombs[j][1];
+
+                int r = bombs[i][2];
+
+
+                if((long)r*r >= (long)(x1 - x2) * (x1 - x2) + (long)(y1 - y2) * (y1 - y2)) {
+                    graph.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
+                }
+
+            }
+        }
+
+        int answer = 0;
+
+        for(int i =0; i< bombs.length; i++) {
+            int count = dfs(i, graph, new HashSet<>());
+            answer = Math.max(count, answer);
+        }
+
+        return answer;
+    }
+
+    public int dfs(int curr, Map<Integer, List<Integer>> graph, Set<Integer> visited) {
+        int count = 1;
+        visited.add(curr);
+
+        for(int neigh : graph.getOrDefault(curr, new ArrayList<>())) {
+            if(!visited.contains(neigh)) {
+                count+=dfs(neigh, graph, visited);
+            }
+        }
+
+
+        return count;
+    }
+}
