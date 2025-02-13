c4b098a8f5592840215d54ddb8b916bfbabc519e
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-02-13 09:31:13 +0530

    Time: 11 ms (5.88%), Space: 45.4 MB (17.37%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.java
+++ 0261-graph-valid-tree/0261-graph-valid-tree.java
@@ -1,69 +1,29 @@
 class Solution {
-
-
-    public class DisjointSetUnion {
-        int[] parents;
-        int[] size; 
-
-        DisjointSetUnion(int n) {
-            parents = new int[n];
-            size = new int[n];
-
-            for(int i = 0 ; i < n ; i++) {
-                parents[i] = i;
-                size[i] = 1;
-            }
-        }
-
-        int find(int x) {
-            if(parents[x] == x) return x;
-            return parents[x] = find(parents[x]);
-        }
-
-        boolean union(int u, int v) {
-            int u1 = find(u);
-            int v1 = find(v);
-
-            if(u1 == v1) {
-                return false;
-            }
-
-            if(size[u1] >= size[v1]) {
-                parents[v1] = u1;
-                size[u1] += size[v1];
-
-            } else {
-                parents[u1] = v1;
-                size[v1] += size[u1];
-            }
-
-            return true;
-
-        }
-    }
-
     public boolean validTree(int n, int[][] edges) {
 
-        DisjointSetUnion dsu = new DisjointSetUnion(n);
-        for(int[] edge : edges) {
-            if (!dsu.union(edge[0], edge[1])) return false;
-        }
-        
-     
-        int sim = dsu.parents[0];
-
-        for(int i=0; i<n; i++) {
-            dsu.find(i);
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int i = 0; i < n; i++) map.put(i, new HashSet<>());
+        for (int[] edge : edges) {
+            map.get(edge[0]).add(edge[1]);
+            map.get(edge[1]).add(edge[0]);
         }
 
-        for(int each : dsu.parents) 
-            System.out.println(each);
+        Set<Integer> set = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
 
-        for(int each : dsu.parents) {
+        while(!queue.isEmpty()) {
+            int node = queue.remove();
+            if(set.contains(node)) return false;
 
-            if (each != sim) return false;
+            for(int node2 : map.get(node)) {
+                // we should remove the edge: node -> top
+                queue.add(node2);
+                map.get(node2).remove(node);
+            }
+
+            set.add(node);
         }
-
-        return true;
+     return set.size() == n;   
     }
 }
