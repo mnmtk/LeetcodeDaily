9abdfa4a6feff60742368a647791e04feb1480b9
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-02-24 17:42:08 +0530

    Time: 13 ms (11.58%), Space: 45.5 MB (41.31%) - LeetHub

--- 0490-the-maze/0490-the-maze.java
+++ 0490-the-maze/0490-the-maze.java
@@ -1,37 +1,39 @@
 class Solution {
-    public boolean dfs(int m, int n, int[][] maze, int[] curr, int[] destination,
-            boolean[][] visit) {
-        if (visit[curr[0]][curr[1]]) {
-            return false;
-        }
-        if (curr[0] == destination[0] && curr[1] == destination[1]) {
-            return true;
-        }
-
-        visit[curr[0]][curr[1]] = true;
-        int[] dirX = { 0, 1, 0, -1 };
-        int[] dirY = { -1, 0, 1, 0 };
-
-        for (int i = 0; i < 4; i++) {
-            int r = curr[0], c = curr[1];
-            // Move the ball in the chosen direction until it can.
-            while (r + dirX[i] >= 0 && r + dirX[i] < m && c + dirY[i] >= 0 && c + dirY[i] < n
-                    && maze[r + dirX[i]][c + dirY[i]] == 0) {
-                r += dirX[i];
-                c += dirY[i];
-            }
-            // No need to revert the last move.
-            if (dfs(m, n, maze, new int[] { r, c }, destination, visit)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
         int m = maze.length;
         int n = maze[0].length;
         boolean[][] visit = new boolean[m][n];
-        return dfs(m, n, maze, start, destination, visit);
+        int[] dirX = {0, 1, 0, -1};
+        int[] dirY = {-1, 0, 1, 0};
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(start);
+        visit[start[0]][start[1]] = true;
+
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            if (curr[0] == destination[0] && curr[1] == destination[1]) {
+                return true;
+            }
+
+              visit[curr[0]][curr[1]] = true;
+            for (int i = 0; i < 4; i++) {
+                int r = curr[0];
+                int c = curr[1];
+                // Move the ball in the chosen direction until it can.
+                while (r >= 0 && r < m && c >= 0 && c < n && maze[r][c] == 0) {
+                    r += dirX[i];
+                    c += dirY[i];
+                }
+                // Revert the last move to get the cell to which the ball rolls.
+                r -= dirX[i];
+                c -= dirY[i];
+                if (!visit[r][c]) {
+                    queue.offer(new int[]{r, c});
+                
+                }
+            }
+        }
+        return false;
     }
 }
