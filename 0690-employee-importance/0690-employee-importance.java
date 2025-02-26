6125014d4f0beeef18a1358fcc059c4a30f280af
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-02-26 12:11:15 +0530

    Time: 4 ms (99.62%), Space: 46.2 MB (81.54%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.java
@@ -0,0 +1,31 @@
+/*
+// Definition for Employee.
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+};
+*/
+
+class Solution {
+    Map<Integer, Employee> emap;
+    public int getImportance(List<Employee> employees, int id) {
+        emap = new HashMap();
+        for(Employee e : employees) {
+            emap.put(e.id, e);
+        }
+
+        return dfs(id);
+    }
+
+    public int dfs(int eid) {
+        Employee emp = emap.get(eid);
+        int ans = emp.importance;
+
+        for(Integer subId : emp.subordinates) {
+            ans+= dfs(subId);
+        }
+
+        return ans;
+    }
+}
