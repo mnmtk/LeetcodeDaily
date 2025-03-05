442d5739cc3836b2e6aa2f0c6f7249927d938dbe
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-05 20:38:50 +0530

    Time: 3 ms (45.27%), Space: 44.9 MB (37.23%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.java
+++ 0261-graph-valid-tree/0261-graph-valid-tree.java
@@ -1,44 +1,35 @@
 class Solution {
     public boolean validTree(int n, int[][] edges) {
+    
+    if (edges.length != n - 1) return false;
+    
 
-        List<List<Integer>> adjajencyList = new ArrayList<>();
-
-        for(int i = 0; i<n; i++) {
-            adjajencyList.add(new ArrayList<>());
-        }
-
-        for(int[] edge : edges) {
-        adjajencyList.get(edge[0]).add(edge[1]);
-        adjajencyList.get(edge[1]).add(edge[0]);
-        }
-
-        Map<Integer, Integer> parent = new HashMap<>();
-        parent.put(0, -1);
-
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(0);
-
-        while(!queue.isEmpty()) {
-            int node = queue.poll();
-            for(int neighbor : adjajencyList.get(node)) {
-                if(parent.get(node) == neighbor) {
-                    continue;
-                }
-
-                if(parent.containsKey(neighbor)) {
-                    return false;
-                }
-
-                queue.offer(neighbor);
-                parent.put(neighbor, node);
-
-            }
-        }
-
-
-        return parent.size() == n;
-
-
-        
+    //tp checkk disconnected ones
+    // Make the adjacency list.
+    List<List<Integer>> adjacencyList = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+        adjacencyList.add(new ArrayList<>());
     }
+    for (int[] edge : edges) {
+        adjacencyList.get(edge[0]).add(edge[1]);
+        adjacencyList.get(edge[1]).add(edge[0]);
+    }
+    
+    Queue<Integer> queue = new LinkedList<>();
+    Set<Integer> seen = new HashSet<>();
+    queue.offer(0);
+    seen.add(0);
+    
+    while (!queue.isEmpty()) {
+        int node = queue.poll();
+        for (int neighbour : adjacencyList.get(node)) {
+            if (seen.contains(neighbour)) continue;
+            seen.add(neighbour);
+            queue.offer(neighbour);
+        }
+    }
+
+
+    return seen.size() == n;   
+}
 }
