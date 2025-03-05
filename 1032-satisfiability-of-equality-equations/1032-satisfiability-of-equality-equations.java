750d9e3ceb01d9ac71c05ee37854db4134243272
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-05 21:27:08 +0530

    Time: 1 ms (90.47%), Space: 42.6 MB (18.48%) - LeetHub

--- 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.java
+++ 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.java
@@ -1,24 +1,15 @@
 class Solution {
     public boolean equationsPossible(String[] equations) {
-        List<Integer>[] graph = new ArrayList[26];
-        for (int i = 0; i < 26; ++i)
-            graph[i] = new ArrayList();
+        int root[] = new int[26];
+        for (int i = 0; i < 26; i++) {
+            root[i] = i;
+        }
 
         for (String eqn : equations) {
             if (eqn.charAt(1) == '=') {
                 int x = eqn.charAt(0) - 'a';
                 int y = eqn.charAt(3) - 'a';
-                graph[x].add(y);
-                graph[y].add(x);
-            }
-        }
-
-        int[] color = new int[26];
-        Arrays.fill(color, -1);
-
-        for (int i = 0; i < 26; i++) {
-            if (color[i] == -1) {
-                dfs(i, i, color, graph);
+                union(root, x, y);
             }
         }
 
@@ -26,7 +17,7 @@ class Solution {
             if (eqn.charAt(1) == '!') {
                 int x = eqn.charAt(0) - 'a';
                 int y = eqn.charAt(3) - 'a';
-                if (color[x] == color[y])
+                if (find(root, x) == find(root, y))
                     return false;
             }
         }
@@ -34,12 +25,16 @@ class Solution {
         return true;
     }
 
-    // mark the color of `node` as `c`
-    private static void dfs(int node, int c, int[] color, List<Integer>[] graph) {
-        if (color[node] == -1) {
-            color[node] = c;
-            for (int nei : graph[node])
-                dfs(nei, c, color, graph);
-        }
+    private static int find(int[] root, int x) {
+        if (root[x] != x)
+            root[x] = find(root, root[x]);
+        return root[x];
+    }
+
+    private static void union(int[] root, int x, int y) {
+        x = find(root, x);
+        y = find(root, y);
+        if (x != y)
+            root[x] = y;
     }
 }
