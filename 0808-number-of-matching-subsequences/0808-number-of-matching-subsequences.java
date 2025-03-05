e2c865e9cf98b91e7835c6c1c02cd3b297dbe9fa
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-05 17:22:14 +0530

    Time: 42 ms (85.84%), Space: 47.2 MB (50.89%) - LeetHub

--- 0808-number-of-matching-subsequences/0808-number-of-matching-subsequences.java
+++ 0808-number-of-matching-subsequences/0808-number-of-matching-subsequences.java
@@ -1,32 +1,34 @@
 class Solution {
     public int numMatchingSubseq(String S, String[] words) {
+
         int ans = 0;
         ArrayList<Node>[] heads = new ArrayList[26];
-
-        for (int i = 0; i < 26; ++i)
+        for(int i = 0; i < 26; i ++) {
             heads[i] = new ArrayList<Node>();
+        }
 
-        for (String word: words)
+        for(String word : words) {
             heads[word.charAt(0) - 'a'].add(new Node(word, 0));
+        }
 
-        for (char c: S.toCharArray()) {
-            ArrayList<Node> old_bucket = heads[c - 'a'];
+        for(char c : S.toCharArray()) {
+            ArrayList<Node> old = heads[c - 'a'];
             heads[c - 'a'] = new ArrayList<Node>();
 
-            for (Node node: old_bucket) {
+            for(Node node : old) {
                 node.index++;
-                if (node.index == node.word.length()) {
+                if(node.index == node.word.length()) {
                     ans++;
                 } else {
                     heads[node.word.charAt(node.index) - 'a'].add(node);
                 }
             }
-            // old_bucket.clear();
-
         }
-        return ans;
-    }
 
+        return ans;
+
+        
+    }
 }
 
 class Node {
