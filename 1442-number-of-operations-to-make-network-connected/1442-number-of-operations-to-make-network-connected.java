53e562f36437f7a6b7326f8236bc9c789a6ed00d
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-07 15:46:54 +0530

    Time: 7 ms (60.42%), Space: 61.1 MB (65.89%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.java
@@ -1,39 +1,53 @@
-class Solution {
+class UnionFind {
+    int[] parent;
+    int[] rank;
 
-    public void dfs(int node, Map<Integer, List<Integer> > adj, boolean[] visit) {
-        visit[node] = true;
-        if(!adj.containsKey(node)) {
-            return;
-        }
+    public UnionFind(int size) {
+        parent = new int[size];
+        for (int i = 0; i < size; i++)
+            parent[i] = i;
 
-        for(int neighbor : adj.get(node)) {
-            if(!visit[neighbor]) {
-                visit[neighbor] = true;
-                dfs(neighbor, adj, visit);
-            }
-        }
+        rank = new int[size];
     }
 
+    public int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
 
+    public void union_set(int x, int y) {
+        int xset = find(x), yset = find(y);
 
+        if (xset == yset) {
+            return;
+        } else if (rank[xset] < rank[yset]) {
+            parent[xset] = yset;
+        } else if (rank[xset] > rank[yset]) {
+            parent[yset] = xset;
+        } else {
+            parent[yset] = xset;
+            rank[xset]++;
+        }
+        
+    }
+}
+
+class Solution {
     public int makeConnected(int n, int[][] connections) {
-        if(connections.length < n -1) return -1;
-
-        Map<Integer, List<Integer>> adj = new HashMap<>();
-       for (int[] connection : connections) {
-            adj.computeIfAbsent(connection[0], k -> new ArrayList<Integer>()).add(connection[1]);
-            adj.computeIfAbsent(connection[1], k -> new ArrayList<Integer>()).add(connection[0]);
+        if(connections.length < n-1) {
+            return -1;
         }
 
-        int num = 0;
-        boolean[] visit = new boolean[n];
-        for(int i=0; i<n;i++) {
-            if(!visit[i]) {
-                num++;
-                dfs(i, adj, visit);
+       UnionFind dsu = new UnionFind(n);
+        int numberOfConnectedComponents = n;
+        for (int[] connection : connections) {
+            if (dsu.find(connection[0]) != dsu.find(connection[1])) {
+                numberOfConnectedComponents--;
+                dsu.union_set(connection[0], connection[1]);
             }
         }
 
-         return num - 1;
+        return numberOfConnectedComponents - 1;
     }
 }
