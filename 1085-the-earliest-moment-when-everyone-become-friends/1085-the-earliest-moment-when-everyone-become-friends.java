267bd0c50effb3c8fb3f706e84ee93a337bbf9c1
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-08 17:20:23 +0530

    Time: 6 ms (72.93%), Space: 45.1 MB (19.44%) - LeetHub

--- 1085-the-earliest-moment-when-everyone-become-friends/1085-the-earliest-moment-when-everyone-become-friends.java
+++ 1085-the-earliest-moment-when-everyone-become-friends/1085-the-earliest-moment-when-everyone-become-friends.java
@@ -1,7 +1,7 @@
 class Solution {
     public int earliestAcq(int[][] logs, int n) {
 
-       
+        // First, we need to sort the events in chronological order.
         Arrays.sort(logs, new Comparator<int[]>() {
             @Override
             public int compare(int[] log1, int[] log2) {
@@ -11,20 +11,26 @@ class Solution {
             }
         });
 
-
+        // Initially, we treat each individual as a separate group.
         int groupCount = n;
         UnionFind uf = new UnionFind(n);
 
         for (int[] log : logs) {
             int timestamp = log[0], friendA = log[1], friendB = log[2];
-            
+
+            // We merge the groups along the way.
             if (uf.union(friendA, friendB)) {
                 groupCount -= 1;
             }
+
+            // The moment when all individuals are connected to each other.
             if (groupCount == 1) {
                 return timestamp;
             }
         }
+
+        // There are still more than one groups left,
+        //  i.e. not everyone is connected.
         return -1;
     }
 }
