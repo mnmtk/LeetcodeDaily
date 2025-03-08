0096306eec6c8836c8de8c82c43aeec8d9afd837
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-09 02:40:46 +0530

    Time: 33 ms (20.11%), Space: 44.6 MB (51.27%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,34 +1,22 @@
 class Solution {
-    private Integer[][] dp;
     public int coinChange(int[] coins, int amount) {
-        if(amount == 0) return 0;
-        if(coins.length == 0) return -1;
-
-        dp = new Integer[coins.length][amount + 1];
-        
-        int res = coinChangeFrom(coins, amount, 0);
-
-        return res == Integer.MAX_VALUE ? -1 : res;
+        if(amount < 1) return 0;
+        return coinChange(coins, amount, new int[amount]);
     }
 
-    private int coinChangeFrom(int[] coins, int amount, int currentIndex) {
-        if(amount == 0) return 0;
+    private int coinChange(int[] coins, int rem, int[] count) {
+        if(rem < 0) return -1;
+        if(rem == 0) return 0;
 
-        if(amount < 0 || currentIndex == coins.length) return Integer.MAX_VALUE;
+        if(count[rem - 1] != 0) return count[rem - 1];
 
-        if(dp[currentIndex][amount] != null) return dp[currentIndex][amount];
-
-
-        int count1= Integer.MAX_VALUE;
-        int res = coinChangeFrom(coins, amount - coins[currentIndex], currentIndex);
-
-        if(res != Integer.MAX_VALUE) {
-            count1 = res + 1; 
+        int min  = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int res = coinChange(coins, rem - coin, count);
+            if (res >= 0 && res < min) min = 1 + res;
         }
-
-        int coint2 = coinChangeFrom(coins, amount, currentIndex + 1);
-
-        return dp[currentIndex][amount] = Math.min(count1, coint2);
-
+        
+        count[rem - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
+        return count[rem - 1];
     }
 }
