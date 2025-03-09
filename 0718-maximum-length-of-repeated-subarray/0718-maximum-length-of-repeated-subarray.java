33f4735e08e2d7f947f62f0f5e14330a6b93668c
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-09 20:56:16 +0530

    Time: 25 ms (40.91%), Space: 54.9 MB (59.99%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.java
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.java
@@ -1,29 +1,17 @@
 class Solution {
-    public boolean check(int length, int[] A, int[] B) {
-        Set<String> seen = new HashSet();
-        
-        for (int i = 0; i + length <= A.length; ++i) {
-            seen.add(Arrays.toString(Arrays.copyOfRange(A, i, i+length)));
-        }
-
-        for (int j = 0; j + length <= B.length; ++j) {
-            if (seen.contains(Arrays.toString(Arrays.copyOfRange(B, j, j+length)))) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
     public int findLength(int[] A, int[] B) {
-        int lo = 0, hi = Math.min(A.length, B.length) + 1;
-        while (lo < hi) {
-            int mi = (lo + hi) / 2;
-            if (check(mi, A, B)) {
-                lo = mi + 1;
+        int ans = 0;
+        int[][] memo = new int[A.length + 1][B.length + 1];
+        for (int i = A.length - 1; i >= 0; --i) {
+            for (int j = B.length - 1; j >= 0; --j) {
+                if (A[i] == B[j]) {
+                    memo[i][j] = memo[i+1][j+1] + 1;
+                    if (ans < memo[i][j]) {
+                        ans = memo[i][j];
+                    }
+                }
             }
-            else hi = mi;
         }
-        return lo - 1;
+        return ans;
     }
 }
