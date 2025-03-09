bdc46cf2f9894ee0a3e00639f8f90f8a5d21e31d
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-09 23:30:37 +0530

    Time: 0 ms (100%), Space: 40.2 MB (87.75%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,20 +1,10 @@
-class Solution {
+public class Solution {
     public int uniquePaths(int m, int n) {
-
-        int[][] dp = new int[m][n];
-
-        for (int[] arr : dp) {
-            Arrays.fill(arr, 1);
+        long answer = 1;
+        for (int i = n; i < (m + n - 1); i++) {
+            answer *= i;
+            answer /= (i - n + 1);
         }
-
-        for(int i = 1; i < m; i++) {
-            for(int j =1; j < n; j++) {
-                int left = dp[i-1][j];
-                int up = dp[i][j-1];
-                dp[i][j] = left + up;
-            }
-        }
-
-        return dp[m -1 ][n - 1];
+        return (int) answer;
     }
 }
