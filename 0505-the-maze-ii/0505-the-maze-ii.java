118c4cd819e4aa19ae77758d6f32a4d70aae542b
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-12 01:16:22 +0530

    Time: 9 ms (69.36%), Space: 45.6 MB (37.02%) - LeetHub

--- 0505-the-maze-ii/0505-the-maze-ii.java
+++ 0505-the-maze-ii/0505-the-maze-ii.java
@@ -1,22 +1,21 @@
 public class Solution {
     public int shortestDistance(int[][] maze, int[] start, int[] dest) {
         int[][] distance = new int[maze.length][maze[0].length];
-
-
         for (int[] row: distance)
             Arrays.fill(row, Integer.MAX_VALUE);
-
-
         distance[start[0]][start[1]] = 0;
-        int[][] dirs={{0, 1} ,{0, -1}, {-1, 0}, {1, 0}};
+        dijkstra(maze, start, distance);
+        return distance[dest[0]][dest[1]] == Integer.MAX_VALUE ? -1 : distance[dest[0]][dest[1]];
+    }
 
-
-        Queue < int[] > queue = new LinkedList < > ();
-        queue.add(start);
-
+    public void dijkstra(int[][] maze, int[] start, int[][] distance) {
+        int[][] dirs={{0,1},{0,-1},{-1,0},{1,0}};
+        PriorityQueue < int[] > queue = new PriorityQueue < > ((a, b) -> a[2] - b[2]);
+        queue.offer(new int[]{start[0],start[1],0});
         while (!queue.isEmpty()) {
-            int[] s = queue.remove();
-
+            int[] s = queue.poll();
+            if(distance[s[0]][s[1]] < s[2])
+                continue;
             for (int[] dir: dirs) {
                 int x = s[0] + dir[0];
                 int y = s[1] + dir[1];
@@ -28,11 +27,9 @@ public class Solution {
                 }
                 if (distance[s[0]][s[1]] + count < distance[x - dir[0]][y - dir[1]]) {
                     distance[x - dir[0]][y - dir[1]] = distance[s[0]][s[1]] + count;
-                    queue.add(new int[] {x - dir[0], y - dir[1]});
+                    queue.offer(new int[]{x - dir[0], y - dir[1], distance[x - dir[0]][y - dir[1]]});
                 }
             }
-            
         }
-        return distance[dest[0]][dest[1]] == Integer.MAX_VALUE ? -1 : distance[dest[0]][dest[1]];
     }
 }
