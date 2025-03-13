38df0efe9bc891bfe4304e1def7796462c375b59
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-13 21:12:26 +0530

    Time: 0 ms (100%), Space: 42 MB (32.8%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -4,13 +4,11 @@ class Solution {
         dummy.next = head;
         ListNode first = dummy;
         ListNode second = dummy;
-        // Advances first pointer so that the gap between first and second is n nodes apart
 
-        for (int i = 1; i <= n + 1; i++) {
+        for (int i = 0; i <= n ; i++) {
             first = first.next;
         }
-        
-        // Move first to the end, maintaining the gap
+
         while (first != null) {
             first = first.next;
             second = second.next;
