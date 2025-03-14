848d17f9cb4f8164119a1bff520d7de71dac1c97
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-14 23:58:16 +0530

    Time: 172 ms (22.87%), Space: 62.5 MB (27.35%) - LeetHub

---
+++ 2526-longest-increasing-subsequence-ii/2526-longest-increasing-subsequence-ii.java
@@ -0,0 +1,55 @@
+class Node {
+    Node leftChild;
+    Node rightChild;
+    int start;
+    int end;
+    int value;
+
+    public Node(int start, int end, int value) {
+        this.start = start;
+        this.end = end;
+        this.value = value;
+    }
+}
+
+class Solution {
+    Node buildSegmentTree(int start, int end) {
+        if (start == end)
+            return new Node(start, end, 0);
+        Node node = new Node(start, end, 0);
+        int mid = (start + end) / 2;
+        node.leftChild = buildSegmentTree(start, mid);
+        node.rightChild = buildSegmentTree(mid + 1, end);
+        return node;
+    }
+
+    int queryRangeMax(Node node, int l, int r) {
+        if (node == null || l > node.end || r < node.start)
+            return 0; // Return 0 for out-of-bound queries
+        if (l <= node.start && r >= node.end)
+            return node.value; // Total overlap
+        // Partial overlap
+        return Math.max(queryRangeMax(node.leftChild, l, r), queryRangeMax(node.rightChild, l, r));
+    }
+
+    void updateSegmentTree(Node node, int index, int value) {
+        if (node == null || index < node.start || index > node.end)
+            return; // Out of bounds
+        node.value = Math.max(value, node.value); // Update the current node
+        if (node.start != node.end) { // If it's not a leaf node
+            updateSegmentTree(node.leftChild, index, value);
+            updateSegmentTree(node.rightChild, index, value);
+        }
+    }
+
+    public int lengthOfLIS(int[] nums, int k) {
+        Node root = buildSegmentTree(0, 100001);
+        int ans = 1;
+        for (int num : nums) {
+            int maxValInRange = queryRangeMax(root, Math.max(0, num - k), num - 1) + 1;
+            ans = Math.max(ans, maxValInRange);
+            updateSegmentTree(root, num, maxValInRange);
+        }
+        return ans;
+    }
+}
