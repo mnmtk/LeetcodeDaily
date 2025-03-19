2c1b8c5e2f75cace0f74e8ea062b48ca64ba7e56
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-20 01:34:47 +0530

    Time: 3 ms (98.46%), Space: 55.9 MB (31.95%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.java
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.java
@@ -1,22 +1,17 @@
 class Solution {
-
     public int longestNiceSubarray(int[] nums) {
-        int usedBits = 0; 
+        int usedBits = 0;
         int windowStart = 0;
         int maxLength = 0;
 
-        for (int windowEnd = 0; windowEnd < nums.length; ++windowEnd) {
-            // If current number shares bits with window, shrink window from left
-            // until there's no bit conflict
-            while ((usedBits & nums[windowEnd]) != 0) {
-                usedBits ^= nums[windowStart]; // Remove leftmost element's bits
-                windowStart++;
+        for(int windowEnd = 0; windowEnd < nums.length; ++windowEnd) {
+
+            while((usedBits & nums[windowEnd]) != 0) {
+                usedBits ^= nums[windowStart]; // XOR
+                windowStart++; 
             }
 
-            // Add current number to the window
             usedBits |= nums[windowEnd];
-
-            // Update max length if current window is longer
             maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
         }
 
