83bb9386f14774b77f7b87b54ea7a9d4abf5a5bc
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-23 01:36:11 +0530

    Time: 351 ms (5.56%), Space: 51.8 MB (34.12%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.java
+++ 0218-the-skyline-problem/0218-the-skyline-problem.java
@@ -1,47 +1,45 @@
 class Solution {
     public List<List<Integer>> getSkyline(int[][] buildings) {
-        // Sort the unique positions of all the edges.
+        // Collect and sort the unique positions of all the edges.
         SortedSet<Integer> edgeSet = new TreeSet<Integer>();
         for (int[] building : buildings) {
             int left = building[0], right = building[1];
             edgeSet.add(left);
             edgeSet.add(right);
         }
-        List<Integer> edges = new ArrayList<Integer>(edgeSet);
+        List<Integer> positions = new ArrayList<Integer>(edgeSet);
+        Collections.sort(positions);
         
-        // Hast table 'edgeIndexMap' record every {position : index} pairs in edges.
-        Map<Integer, Integer> edgeIndexMap = new HashMap<>();
-        for (int i = 0; i < edges.size(); ++i) {
-            edgeIndexMap.put(edges.get(i), i);
-        }
-
-        // Initialize 'heights' to record maximum height at each index.
-        int[] heights = new int[edges.size()];
-
-        // Iterate over all the buildings.
-        for (int[] building : buildings) {
-            // For each building, find the indexes of its left
-            // and right edges.
-            int left = building[0], right = building[1], height = building[2];
-            int leftIndex = edgeIndexMap.get(left), rightIndex = edgeIndexMap.get(right);
-            
-            // Update the maximum height within the range [left_idx, right_idx)
-            for (int idx = leftIndex; idx < rightIndex; ++idx) {
-                heights[idx] = Math.max(heights[idx], height);
-            }
-        }
-        
+        // 'answer' for skyline key points.
         List<List<Integer>> answer = new ArrayList<>();
-
-        // Iterate over 'heights'.
-        for (int i = 0; i < heights.length; ++i) {
-            int currHeight = heights[i], currPos = edges.get(i);
-
-            // Add all the positions where the height changes to 'answer'.
-            if (answer.isEmpty() || answer.get(answer.size() - 1).get(1) != currHeight) {
-                answer.add(Arrays.asList(currPos, currHeight));
+        int maxHeight, left, right, height;
+        
+        // For each position, draw an imaginary vertical line.
+        for (int position : positions) {
+            // The current max height.
+            maxHeight = 0;
+            
+            // Iterate over all the buildings:
+            for (int[] building : buildings) {
+                left = building[0];
+                right = building[1];
+                height = building[2];
+                
+                // If the current building intersects with the line,
+                // update 'maxHeight'.
+                if (left <= position && position < right) {
+                    maxHeight = Math.max(maxHeight, height);
+                }
+            }
+            
+            // If its the first key point or the height changes, 
+            // we add [position, maxHeight] to 'answer'.
+            if (answer.isEmpty() || answer.get(answer.size() - 1).get(1) != maxHeight) {
+                answer.add(Arrays.asList(position, maxHeight));
             }
         }
+        
+        // Return 'answer' as the skyline.
         return answer;
     }
 }
