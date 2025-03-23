c9b5940fa700686b2c940b2e98b825b772987a9a
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-23 16:11:30 +0530

    Time: 35 ms (62.54%), Space: 52.6 MB (18.99%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.java
+++ 0218-the-skyline-problem/0218-the-skyline-problem.java
@@ -1,55 +1,55 @@
 class Solution {
     public List<List<Integer>> getSkyline(int[][] buildings) {
-        // Iterate over all buildings, for each building i
-        // add (position, i) to edges.
+
         List<List<Integer>> edges = new ArrayList<>();
         for (int i = 0; i < buildings.length; ++i){
-            edges.add(Arrays.asList(buildings[i][0], i));
-            edges.add(Arrays.asList(buildings[i][1], i));
+            edges.add(Arrays.asList(buildings[i][0], buildings[i][2]));
+            edges.add(Arrays.asList(buildings[i][1], -buildings[i][2]));
         }
         Collections.sort(edges, (a, b) -> {
             return a.get(0) - b.get(0);
         });
         
-        // Initailize an empty Priority Queue 'live' to store all the newly 
-        // added buildings, an empty list answer to store the skyline key points.
-        Queue<List<Integer>> live = new PriorityQueue<>((a, b) -> {
-            return b.get(0) - a.get(0);
+        Queue<Integer> live = new PriorityQueue<>((a, b) -> {
+            return b - a;
+        });
+        Queue<Integer> past = new PriorityQueue<>((a, b) -> {
+            return b - a;
         });
         List<List<Integer>> answer = new ArrayList<>();
         
-        int idx = 0;
         
-      
+        int idx = 0;
+
         while (idx < edges.size()){
-            
+
             int currX = edges.get(idx).get(0);
             
-          
             while (idx < edges.size() && edges.get(idx).get(0) == currX){
-
-                int b = edges.get(idx).get(1);
                
-                if (buildings[b][0] == currX){
-                    int right = buildings[b][1];
-                    int height = buildings[b][2];
-                    live.offer(Arrays.asList(height, right));
+                int height = edges.get(idx).get(1);
+                
+                if (height > 0){
+                    live.offer(height);
+                } else {
+                    past.offer(-height);
                 }
-                idx += 1;
+                idx++;
+            }
+
+            while (!past.isEmpty() && live.peek().equals(past.peek())) {
+                live.remove();
+                past.remove();
             }
             
-      
-            while (!live.isEmpty() && live.peek().get(1) <= currX)
-                live.poll();
-            
-           
-            int currHeight = live.isEmpty() ? 0 : live.peek().get(0);
-            
-            
-            if (answer.isEmpty() || answer.get(answer.size() - 1).get(1) != currHeight)
+
+            int currHeight = live.isEmpty() ? 0 : live.peek();
+
+            if (answer.isEmpty() || answer.get(answer.size() - 1).get(1) != currHeight) {
                 answer.add(Arrays.asList(currX, currHeight));
+            }   
         }
-        
+            
         return answer;
     }
 }
