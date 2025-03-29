fbec7dcab77011fd4b61e4acb56ac0457b60735c
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-30 00:57:25 +0530

    Time: 170 ms (80.67%), Space: 57.6 MB (5.39%) - LeetHub

--- 0976-minimum-area-rectangle/0976-minimum-area-rectangle.java
+++ 0976-minimum-area-rectangle/0976-minimum-area-rectangle.java
@@ -1,25 +1,26 @@
 class Solution {
     public int minAreaRect(int[][] points) {
-        Set<Integer> pointSet = new HashSet();
-        for (int[] point: points)
-            pointSet.add(40001 * point[0] + point[1]);
+        Map<Integer, List<Integer>> rows = new TreeMap();
+        for (int[] point: points) {
+            int x = point[0], y = point[1];
+            
+            rows.computeIfAbsent(x, z-> new ArrayList()).add(y);
+        }
 
         int ans = Integer.MAX_VALUE;
-        for (int i = 0; i < points.length; ++i)
-            for (int j = i+1; j < points.length; ++j) {
-
-                if (points[i][0] != points[j][0] 
-                 && points[i][1] != points[j][1]) {
-
-                    if (pointSet.contains(40001 * points[i][0] + points[j][1]) 
-                     && pointSet.contains(40001 * points[j][0] + points[i][1])) {
-                        
-                        ans = Math.min(ans, Math.abs(points[j][0] - points[i][0]) *
-                                            Math.abs(points[j][1] - points[i][1]));
-                    }
-
+        Map<Integer, Integer> lastx = new HashMap();
+        for (int x: rows.keySet()) {
+            List<Integer> row = rows.get(x);
+            Collections.sort(row);
+            for (int i = 0; i < row.size(); ++i)
+                for (int j = i+1; j < row.size(); ++j) {
+                    int y1 = row.get(i), y2 = row.get(j);
+                    int code = 40001 * y1 + y2;
+                    if (lastx.containsKey(code))
+                        ans = Math.min(ans, (x - lastx.get(code)) * (y2-y1));
+                    lastx.put(code, x);
                 }
-            }
+        }
 
         return ans < Integer.MAX_VALUE ? ans : 0;
     }
