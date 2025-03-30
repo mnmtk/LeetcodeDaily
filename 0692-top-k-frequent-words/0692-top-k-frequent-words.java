768016086c8e009bab2cb771fb794850f71a64d4
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-31 05:13:04 +0530

    Time: 13 ms (6.47%), Space: 45.1 MB (22.2%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,24 +1,73 @@
 class Solution {
+    private int k;
+    private List<String> res;
+
+    class TrieNode {
+        TrieNode[] children;
+        boolean isWord;
+
+        public TrieNode() {
+            children = new TrieNode[26];
+            isWord = false;
+        }
+    }
+
     public List<String> topKFrequent(String[] words, int k) {
+        this.k = k;
+        res = new ArrayList<>();
+        int n = words.length;
+        TrieNode[] bucket = new TrieNode[n + 1];
         Map<String, Integer> cnt = new HashMap<>();
+
         for (String word : words) {
             cnt.put(word, cnt.getOrDefault(word, 0) + 1);
         }
-        PriorityQueue<String> h = new PriorityQueue<>(
-                (w1, w2) -> cnt.get(w1).equals(cnt.get(w2)) ? w2.compareTo(w1) : cnt.get(w1) - cnt.get(w2));
 
-        for (String word : cnt.keySet()) {
-            h.offer(word);
-            if (h.size() > k) {
-                h.poll();
+        for (var entry : cnt.entrySet()) {
+            if (bucket[entry.getValue()] == null) {
+                bucket[entry.getValue()] = new TrieNode();
+            }
+            addWord(bucket[entry.getValue()], entry.getKey());
+        }
+
+        for (int i = n; i > 0; i--) {
+            if (bucket[i] != null) {
+                getWords(bucket[i], "");
+            }
+            if (this.k == 0) {
+                break;
+            }
+        }
+        return res;
+
+    }
+
+    private void addWord(TrieNode root, String word) {
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            if (cur.children[c - 'a'] == null) {
+                cur.children[c - 'a'] = new TrieNode();
+            }
+            cur = cur.children[c - 'a'];
+        }
+        cur.isWord = true;
+    }
+
+    private void getWords(TrieNode root, String prefix) {
+        if(k == 0) {
+            return;
+        }
+
+        if(root.isWord) {
+            k--;
+            res.add(prefix);
+        }
+
+        for(int i = 0; i < 26; i++) {
+            if(root.children[i] != null) {
+                getWords(root.children[i], prefix + (char)(i + 'a'));
             }
         }
 
-        List<String> res = new ArrayList<>();
-        while (!h.isEmpty()) {
-            res.add(h.poll());
-        }
-        Collections.reverse(res);
-        return res;
     }
 }
