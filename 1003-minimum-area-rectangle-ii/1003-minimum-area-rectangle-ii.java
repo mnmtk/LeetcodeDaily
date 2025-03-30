0b00579f037a32fd31e48de0e568b3a94bd4d431
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-30 11:43:19 +0530

    Time: 25 ms (70.69%), Space: 45.4 MB (62.5%) - LeetHub

--- 1003-minimum-area-rectangle-ii/1003-minimum-area-rectangle-ii.java
+++ 1003-minimum-area-rectangle-ii/1003-minimum-area-rectangle-ii.java
@@ -3,36 +3,42 @@ import java.awt.Point;
 class Solution {
     public double minAreaFreeRect(int[][] points) {
         int N = points.length;
-
         Point[] A = new Point[N];
-        Set<Point> pointSet = new HashSet();
-        for (int i = 0; i < N; ++i) {
+        for (int i = 0; i < N; ++i)
             A[i] = new Point(points[i][0], points[i][1]);
-            pointSet.add(A[i]);
-        }
+
+        Map<Integer, Map<Point, List<Point>>> seen = new HashMap();
+        for (int i = 0; i < N; ++i)
+            for (int j = i+1; j < N; ++j) {
+                // center is twice actual to keep integer precision
+                Point center = new Point(A[i].x + A[j].x, A[i].y + A[j].y);
+
+                int r2 = (A[i].x - A[j].x) * (A[i].x - A[j].x);
+                r2 += (A[i].y - A[j].y) * (A[i].y - A[j].y);
+                if (!seen.containsKey(r2))
+                    seen.put(r2, new HashMap<Point, List<Point>>());
+                if (!seen.get(r2).containsKey(center))
+                    seen.get(r2).put(center, new ArrayList<Point>());
+                seen.get(r2).get(center).add(A[i]);
+            }
 
         double ans = Double.MAX_VALUE;
-        for (int i = 0; i < N; ++i) {
-            Point p1 = A[i];
+        for (Map<Point, List<Point>> info: seen.values()) {
+            for (Point center: info.keySet()) {  // center is twice actual
+                List<Point> candidates = info.get(center);
+                int clen = candidates.size();
+                for (int i = 0; i < clen; ++i)
+                    for (int j = i+1; j < clen; ++j) {
+                        Point P = candidates.get(i);
+                        Point Q = candidates.get(j);
+                        
+                        Point Q2 = new Point(center);
+                        Q2.translate(-Q.x, -Q.y);
 
-            for (int j = 0; j < N; ++j) if (j != i) {
-                Point p2 = A[j];
-
-                for (int k = j+1; k < N; ++k) if (k != i) {
-                    Point p3 = A[k];
-                    
-                    Point p4 = new Point(p2.x + p3.x - p1.x, p2.y + p3.y - p1.y);
-
-                    if (pointSet.contains(p4)) {
-                        int dot = ((p2.x - p1.x) * (p3.x - p1.x) +
-                                   (p2.y - p1.y) * (p3.y - p1.y));
-                        if (dot == 0) {
-                            double area = p1.distance(p2) * p1.distance(p3);
-                            if (area < ans)
-                                ans = area;
-                        }
+                        double area = P.distance(Q) * P.distance(Q2);
+                        if (area < ans)
+                            ans = area;
                     }
-                }
             }
         }
 
