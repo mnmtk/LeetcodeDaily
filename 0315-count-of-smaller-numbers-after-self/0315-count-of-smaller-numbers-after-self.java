0ecc0c3be33ce42e24f5d9fec39bef42c7ae7f53
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-04-01 00:49:30 +0530

    Time: 109 ms (53.01%), Space: 63 MB (37.11%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
@@ -1,57 +1,53 @@
 class Solution {
     public List<Integer> countSmaller(int[] nums) {
-        int offset = 10000;
-        int size = 2*10000 + 1;
-
+        int offset = 10000; // offset negative to non-negative
+        int size = 2 * 10000 + 1; // total possible values in nums
         int[] tree = new int[size * 2];
         List<Integer> result = new ArrayList<Integer>();
 
-        for(int i = nums.length - 1; i >= 0; i--) {
-            //add offset for negatvoe vals;
-            int smallerCount = query(0, nums[i] + offset, tree, size);
-
-            result.add(smallerCount);
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int smaller_count = query(0, nums[i] + offset, tree, size);
+            result.add(smaller_count);
             update(nums[i] + offset, 1, tree, size);
         }
-
         Collections.reverse(result);
         return result;
     }
 
+    // implement segment tree
     private void update(int index, int value, int[] tree, int size) {
-        index+=size; //shift index to the leaf;
-
+        index += size; // shift the index to the leaf
+        // update from leaf to root
         tree[index] += value;
-        while(index > 1) {
+        while (index > 1) {
             index /= 2;
             tree[index] = tree[index * 2] + tree[index * 2 + 1];
         }
-
     }
 
     private int query(int left, int right, int[] tree, int size) {
+        // return sum of [left, right)
         int result = 0;
-        left+= size;
-        right+=size;
-
-        while(left < right) {
-
+        left += size; // shift the index to the leaf
+        right += size;
+        while (left < right) {
+            // if left is a right node
+            // bring the value and move to parent's right node
             if (left % 2 == 1) {
                 result += tree[left];
                 left++;
             }
-           
+            // else directly move to parent
+            left /= 2;
+            // if right is a right node
+            // bring the value of the left node and move to parent
             if (right % 2 == 1) {
                 right--;
                 result += tree[right];
             }
-          
-
-            left /= 2;
+            // else directly move to parent
             right /= 2;
-            
         }
-
         return result;
     }
 }
