3fb398652f48273d624d87f96585d6cd1b92feaa
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-31 17:35:38 +0530

    Time: 0 ms (100%), Space: 42.1 MB (67.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,34 +1,45 @@
 class Solution {
     public int search(int[] nums, int target) {
+
+        int n = nums.length;
         int left = 0;
-        int right = nums.length - 1;
+        int right = n -1;
 
         while(left <= right) {
-            int mid = left + (right - left)/2;
-            if(nums[mid] == target) return mid;
-
-            if(nums[mid] <= nums[right]) { //right sorted
-
-            if(target > nums[mid] && target <= nums[right]) {
+            int mid = (left + right)/2;
+            if(nums[mid] > nums[n-1]) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
-            } else {    //left sorted
-
-            if(target < nums[mid] && target >= nums[left]) {
-                right = mid - 1;
-            } else {
-                left = mid+1;
-            }
-
-
-            }
-
         }
 
+        int answer = binarySearch(nums, 0, left - 1, target);
+        if(answer!= -1) {
+            return answer;
+        }
 
-        return -1;
-        
+        return binarySearch(nums, left, n - 1, target);
     }
+
+        private int binarySearch(
+        int[] nums,
+        int leftBoundary,
+        int rightBoundary,
+        int target
+    ) {
+        int left = leftBoundary, right = rightBoundary;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
 }
