65209a53f20b59c8164b68ba829b1a363a048c07
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-31 16:44:10 +0530

    Time: 1 ms (100%), Space: 46.1 MB (59.14%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,5 +1,5 @@
 class Solution {
-    public double findMedianSortedArrays(int[] A, int[] B) {
+     public double findMedianSortedArrays(int[] A, int[] B) {
         int na = A.length, nb = B.length;
         int n = na + nb;
         if ((na + nb) % 2 == 1) {
@@ -15,41 +15,44 @@ class Solution {
 
     public int solve(
         int[] A,
-        int[] B,
-        int k,
+        int[] B, 
+        int k, 
         int aStart,
         int aEnd,
         int bStart,
         int bEnd
     ) {
-        // If the segment of on array is empty, it means we have passed all
-        // its element, just return the corresponding element in the other array.
-        if (aEnd < aStart) {
-            return B[k - aStart];
+        if(aEnd < aStart) {
+            return B[k - aStart]; // supposing aStart ends at the length
         }
-        if (bEnd < bStart) {
+
+        if(bEnd < bStart) {
             return A[k - bStart];
         }
 
-        // Get the middle indexes and middle values of A and B.
-        int aIndex = (aStart + aEnd) / 2, bIndex = (bStart + bEnd) / 2;
-        int aValue = A[aIndex], bValue = B[bIndex];
+        int aIndex = (aStart + aEnd)/ 2 ;
+        int bIndex = (bStart + bEnd)/ 2;
 
-        // If k is in the right half of A + B, remove the smaller left half.
-        if (aIndex + bIndex < k) {
-            if (aValue > bValue) {
-                return solve(A, B, k, aStart, aEnd, bIndex + 1, bEnd);
-            } else {
+        int aValue = A[aIndex];
+        int bValue = B[bIndex];
+
+        if(aIndex + bIndex < k) {
+            // agar k mids se bada hai
+            // to point smaller part of smaller array mei nahi ho sakta
+            // isliye ham usko let go krdege.
+            if(aValue < bValue) {
                 return solve(A, B, k, aIndex + 1, aEnd, bStart, bEnd);
-            }
-        }
-        // Otherwise, remove the larger right half.
-        else {
-            if (aValue > bValue) {
-                return solve(A, B, k, aStart, aIndex - 1, bStart, bEnd);
             } else {
-                return solve(A, B, k, aStart, aEnd, bStart, bIndex - 1);
+                return solve(A, B, k, aStart, aEnd, bIndex + 1, bEnd);
+            }
+            //agar point chota hai
+        } else {
+            if(aValue < bValue) {
+            return solve(A, B, k, aStart, aEnd, bStart, bIndex - 1);
+            } else{
+                  return solve(A, B, k, aStart, aIndex - 1, bStart, bEnd);
             }
         }
+
     }
 }
