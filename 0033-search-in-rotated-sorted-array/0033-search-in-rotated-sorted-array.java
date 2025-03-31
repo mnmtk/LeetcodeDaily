373dc41e5d3d804c463bfab76cd2661cecc855d1
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-03-31 17:54:45 +0530

    Time: 0 ms (100%), Space: 42.3 MB (28.61%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -5,12 +5,12 @@ class Solution {
         int left = 0;
         int right = n -1;
 
-        while(left <= right) {
+        while(left <right) {
             int mid = (left + right)/2;
             if(nums[mid] > nums[n-1]) {
                 left = mid + 1;
             } else {
-                right = mid - 1;
+                right = mid;
             }
         }
 
