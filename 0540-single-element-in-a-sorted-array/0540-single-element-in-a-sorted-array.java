412a65c571738c97e761286f132773c1161c5c56
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-04-02 09:31:13 +0530

    Time: 0 ms (100%), Space: 50.2 MB (80.37%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -2,24 +2,17 @@ class Solution {
     public int singleNonDuplicate(int[] nums) {
         int lo = 0;
         int hi = nums.length - 1;
+
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
-            boolean halvesAreEven = (hi - mid) % 2 == 0;
-            if (nums[mid + 1] == nums[mid]) {
-                if (halvesAreEven) {
-                    lo = mid + 2;
-                } else {
-                    hi = mid - 1;
-                }
-            } else if (nums[mid - 1] == nums[mid]) {
-                if (halvesAreEven) {
-                    hi = mid - 2;
-                } else {
-                    lo = mid + 1;
-                }
+
+            if (mid % 2 == 1) mid--;
+            if (nums[mid] == nums[mid + 1]) {
+                lo = mid + 2;
             } else {
-                return nums[mid];
+                hi = mid;
             }
+            
         }
         return nums[lo];
     }
