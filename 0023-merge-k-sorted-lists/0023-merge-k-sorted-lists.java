6cda7d8285ffefd1f16981ccf3bcc02beb2d2875
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-04-05 13:58:17 +0530

    Time: 2 ms (83.9%), Space: 44.1 MB (95.83%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -13,47 +13,35 @@ class Solution {
         if(lists == null || lists.length == 0) {
             return null;
         }
-        return merge(lists, 0, lists.length - 1);
+       return merge(lists, 0, lists.length-1);
     }
 
-    public ListNode merge(ListNode[] lists, int start, int end) {
-
+    public ListNode merge(ListNode[] lists, int start , int end) {
         if(start == end) {
             return lists[start];
         }
-        
-        int mid = start + (end - start) / 2;
+        int mid = start + (end-start)/2;
         ListNode left = merge(lists, start, mid);
-        ListNode right = merge(lists, mid + 1, end);
+        ListNode right = merge(lists, mid+1, end);
 
-        return merge(left, right);
+        return mergeTwoLists(left, right);
     }
 
-    public ListNode merge(ListNode l1, ListNode l2) {
-
-        ListNode dummy = new ListNode(0);
-        ListNode dt = dummy;
-
-        while(l1 != null && l2 != null) {
-            if(l1.val < l2.val) {
-                dt.next = l1;
-                l1 = l1.next;
-            } else {
-                dt.next = l2;
-                l2 = l2.next;
-            }
-            dt = dt.next;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
         }
 
-        if(l1 != null) {
-            dt.next = l1;
-        } 
-
-        if(l2 !=null ) {
-            dt.next = l2;
+        if(list2 == null) {
+            return list1;
         }
 
-        return dummy.next;
+        if(list1.val <= list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
     }
-    
 }
