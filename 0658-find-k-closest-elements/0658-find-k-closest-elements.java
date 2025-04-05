997ddf1399e73b2db697cf77a4d26d9371349e65
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-04-05 16:05:48 +0530

    Time: 4 ms (96.94%), Space: 46.6 MB (6.22%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,52 +1,22 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        List<Integer> result = new ArrayList<Integer>();
-        
-        // Base case
-        if (arr.length == k) {
-            for (int i = 0; i < k; i++) {
-                result.add(arr[i]);
-            }
-            
-            return result;
-        }
-        
-        // Binary search to find the closest element
+        // Initialize binary search bounds
         int left = 0;
-        int right = arr.length;
-        int mid = 0;
+        int right = arr.length - k;
+        
+        // Binary search against the criteria described
         while (left < right) {
-            mid = (left + right) / 2;
-            if (arr[mid] >= x) {
-                right = mid;
-            } else {
+            int mid = (left + right) / 2;
+            if (x - arr[mid] > arr[mid + k] - x) {
                 left = mid + 1;
+            } else {
+                right = mid;
             }
         }
         
-        // Initialize our sliding window's bounds
-        left -= 1;
-        right = left + 1;
-        
-        // While the window size is less than k
-        while (right - left - 1 < k) {
-            // Be careful to not go out of bounds
-            if (left == -1) {
-                right += 1;
-                continue;
-            }
-            
-            // Expand the window towards the side with the closer number
-            // Be careful to not go out of bounds with the pointers
-            if (right == arr.length || Math.abs(arr[left] - x) <= Math.abs(arr[right] - x)) {
-                left -= 1;
-            } else {
-                right += 1;
-            }
-        } 
-
-        // Build and return the window
-        for (int i = left + 1; i < right; i++) {
+        // Create output in correct format
+        List<Integer> result = new ArrayList<Integer>();
+        for (int i = left; i < left + k; i++) {
             result.add(arr[i]);
         }
         
