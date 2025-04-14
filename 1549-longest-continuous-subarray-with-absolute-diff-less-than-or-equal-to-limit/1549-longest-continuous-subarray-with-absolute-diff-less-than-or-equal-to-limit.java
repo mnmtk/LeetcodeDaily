d788dd26482a0f6ea4ec77d863ab9f28964b19de
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-04-15 03:03:10 +0530

    Time: 96 ms (37%), Space: 61 MB (5.22%) - LeetHub

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,33 +1,34 @@
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
+        PriorityQueue<int[]> minPq =  new PriorityQueue<>((a,b) -> a[0] - b[0]);
+        PriorityQueue<int[]> maxPq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
 
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
-                (a, b) -> b[0] - a[0]);
-
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(
-                Comparator.comparingInt(a -> a[0]));
-
+        int maxLength = 1;
         int left = 0;
-        int maxLength = 0;
 
-        for (int right = 0; right < nums.length; ++right) {
-            maxHeap.offer(new int[] { nums[right], right });
-            minHeap.offer(new int[] { nums[right], right });
+        for(int right = 0; right < nums.length; right++) {
+            minPq.offer(new int[] {nums[right], right});
+            maxPq.offer(new int[] {nums[right], right});
 
-            while (maxHeap.peek()[0] - minHeap.peek()[0] > limit) {
-                left = Math.min(maxHeap.peek()[1], minHeap.peek()[1]) + 1;
-                while (maxHeap.peek()[1] < left) {
-                    maxHeap.poll();
+            while(maxPq.peek()[0] - minPq.peek()[0] > limit) {
+
+                left = Math.min(minPq.peek()[1], maxPq.peek()[1]) + 1;
+                while(minPq.peek()[1] < left) {
+                    minPq.poll();
                 }
-                while (minHeap.peek()[1] < left) {
-                    minHeap.poll();
+
+                while(maxPq.peek()[1] < left) {
+                    maxPq.poll();
                 }
             }
 
-            maxLength = Math.max(maxLength, right - left + 1);
+
+                maxLength = Math.max(maxLength, right - left + 1);
+
         }
 
-        return maxLength;
 
+        return maxLength;
+        
     }
 }
