95015ec9277912d5675a76537d752568647e913e
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-04-18 17:59:06 +0530

    Time: 1 ms (99.16%), Space: 45 MB (37.37%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
@@ -1,34 +1,46 @@
 class Solution {
+
+    private int binarySearch(int[] row) {
+        int low = 0;
+        int high = row.length;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (row[mid] == 1) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
     public int[] kWeakestRows(int[][] mat, int k) {
         int m = mat.length;
         int n = mat[0].length;
 
-        // Calculate all the strengths and put strength/ index pairs into an array.
-        int[][] pairs = new int[m][2];
-
-        for (int i = 0; i < m; i++) {
-            int strength = 0;
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) break;
-                strength++;
-            }
-            pairs[i][0] = strength;
-            pairs[i][1] = i;
-        }
-
-        /* Sort the pairs, firstly on strength and secondly on index. We'll need
-         * to implement a comparator to do this. */
-        Arrays.sort(pairs, (a, b) -> {
-            if (a[0] == b[0]) return a[1] - b[1];
-            else return a[0] - b[0];
+        // Create a Priority Queue that measures firstly on strength and then indexes.
+        PriorityQueue<int[]> pq = new  PriorityQueue<>((a, b) -> {
+            if (a[0] == b[0]) return b[1] - a[1];
+            else return b[0] - a[0];
         });
 
-        // Pull out the first k indexes of the sorted array to return.
-        int[] indexes = new int[k];
-        for (int i = 0; i < k; i++) {
-            indexes[i] = pairs[i][1];
+        // Add strength/index pairs to the pq. Whenever length > k, remove the largest.
+        for (int i = 0; i < m; i++) {
+            int strength = binarySearch(mat[i]);
+            int[] entry = new int[]{strength, i};
+            pq.add(entry);
+            if (pq.size() > k) {
+                pq.poll();
+            }
         }
-        
+
+        // Pull the indexes out of the priority queue.
+        int[] indexes = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            int[] pair = pq.poll();
+            indexes[i] = pair[1];
+        }
+
         return indexes;
     }
 }
