808a67f0f9756a63f62f7e888d6e4a93c3bbf698
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-04-19 00:26:03 +0530

    Time: 0 ms (100%), Space: 42.1 MB (74.48%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,24 +1,28 @@
 class Solution {
     private double binaryExp(double x, long n) {
-        // Base case, to stop recursive calls.
         if (n == 0) {
             return 1;
         }
 
         // Handle case where, n < 0.
         if (n < 0) {
-            return 1.0 / binaryExp(x, -1 * n);
+            n = -1 * n;
+            x = 1.0 / x;
         }
 
         // Perform Binary Exponentiation.
-        // If 'n' is odd we perform Binary Exponentiation on 'n - 1' and multiply result with 'x'.
-        if (n % 2 == 1) {
-            return x * binaryExp(x * x, (n - 1) / 2);
+        double result = 1;
+        while (n != 0) {
+            // If 'n' is odd we multiply result with 'x' and reduce 'n' by '1'.
+            if (n % 2 == 1) {
+                result = result * x ;
+                n -= 1;
+            }
+            // We square 'x' and reduce 'n' by half, x^n => (x^2)^(n/2).
+            x = x * x;
+            n = n / 2;
         }
-        // Otherwise we calculate result by performing Binary Exponentiation on 'n'.
-        else {
-            return binaryExp(x * x, n / 2);
-        }
+        return result;
     }
 
     public double myPow(double x, int n) {
