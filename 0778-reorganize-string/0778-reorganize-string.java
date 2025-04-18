52c48460e9e16d1b4d2a304801868aaaecadc42a
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-04-18 16:46:36 +0530

    Time: 0 ms (100%), Space: 41.7 MB (60.48%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.java
+++ 0778-reorganize-string/0778-reorganize-string.java
@@ -4,38 +4,38 @@ class Solution {
         for (char c : s.toCharArray()) {
             charCounts[c - 'a']++;
         }
+        int maxCount = 0, letter = 0;
+        for (int i = 0; i < charCounts.length; i++) {
+            if (charCounts[i] > maxCount) {
+                maxCount = charCounts[i];
+                letter = i;
+            }
+        }
+        if (maxCount > (s.length() + 1) / 2) {
+            return "";
+        }
+        var ans = new char[s.length()];
+        int index = 0;
 
-        // Max heap ordered by character counts
-        var pq = new PriorityQueue<int[]>((a, b) -> Integer.compare(b[1], a[1]));
-        for (int i = 0; i < 26; i++) {
-            if (charCounts[i] > 0) {
-                pq.offer(new int[] {i + 'a', charCounts[i]});
+        // Place the most frequent letter
+        while (charCounts[letter] != 0) {
+            ans[index] = (char) (letter + 'a');
+            index += 2;
+            charCounts[letter]--;
+        }
+
+        // Place rest of the letters in any order
+        for (int i = 0; i < charCounts.length; i++) {
+            while (charCounts[i] > 0) {
+                if (index >= s.length()) { 
+                    index = 1;
+                }
+                ans[index] = (char) (i + 'a');
+                index += 2;
+                charCounts[i]--;
             }
         }
-            
-        var sb = new StringBuilder();
-        while (!pq.isEmpty()) {
-            var first = pq.poll();
-            if (sb.length() == 0 || first[0] != sb.charAt(sb.length() - 1)) {
-                sb.append((char) first[0]);
-                if (--first[1] > 0) {
-                    pq.offer(first);
-                }
-            } else {
-                if (pq.isEmpty()) {
-                    return "";
-                }
-                
-                var second = pq.poll();
-                sb.append((char) second[0]);
-                if (--second[1] > 0) {
-                    pq.offer(second);
-                }
-                
-                pq.offer(first);
-            }
-        }
-        
-        return sb.toString();
+
+        return String.valueOf(ans);
     }
 }
