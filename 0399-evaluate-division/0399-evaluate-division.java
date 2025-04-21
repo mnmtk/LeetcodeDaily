51af940351551e61a1f1b72578b6afb7a5efa369
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-04-21 14:08:56 +0530

    Time: 1 ms (99.22%), Space: 42.3 MB (72.93%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,67 +1,78 @@
 class Solution {
+
     public double[] calcEquation(List<List<String>> equations, double[] values,
             List<List<String>> queries) {
 
-        HashMap<String, HashMap<String, Double>> graph = new HashMap<>();
+        HashMap<String, Pair<String, Double>> gidWeight = new HashMap<>();
 
-        // Step 1). build the graph from the equations
+        // Step 1). build the union groups
         for (int i = 0; i < equations.size(); i++) {
             List<String> equation = equations.get(i);
             String dividend = equation.get(0), divisor = equation.get(1);
             double quotient = values[i];
 
-            if (!graph.containsKey(dividend))
-                graph.put(dividend, new HashMap<String, Double>());
-            if (!graph.containsKey(divisor))
-                graph.put(divisor, new HashMap<String, Double>());
-
-            graph.get(dividend).put(divisor, quotient);
-            graph.get(divisor).put(dividend, 1 / quotient);
+            union(gidWeight, dividend, divisor, quotient);
         }
 
-        // Step 2). Evaluate each query via bactracking (DFS)
-        // by verifying if there exists a path from dividend to divisor
+        // Step 2). run the evaluation, with "lazy" updates in find() function
         double[] results = new double[queries.size()];
         for (int i = 0; i < queries.size(); i++) {
             List<String> query = queries.get(i);
             String dividend = query.get(0), divisor = query.get(1);
 
-            if (!graph.containsKey(dividend) || !graph.containsKey(divisor))
+            if (!gidWeight.containsKey(dividend) || !gidWeight.containsKey(divisor))
+                // case 1). at least one variable did not appear before
                 results[i] = -1.0;
-            else if (dividend == divisor)
-                results[i] = 1.0;
             else {
-                HashSet<String> visited = new HashSet<>();
-                results[i] = backtrackEvaluate(graph, dividend, divisor, 1, visited);
+                Pair<String, Double> dividendEntry = find(gidWeight, dividend);
+                Pair<String, Double> divisorEntry = find(gidWeight, divisor);
+
+                String dividendGid = dividendEntry.getKey();
+                String divisorGid = divisorEntry.getKey();
+                Double dividendWeight = dividendEntry.getValue();
+                Double divisorWeight = divisorEntry.getValue();
+
+                if (!dividendGid.equals(divisorGid))
+                    // case 2). the variables do not belong to the same chain/group
+                    results[i] = -1.0;
+                else
+                    // case 3). there is a chain/path between the variables
+                    results[i] = dividendWeight / divisorWeight;
             }
         }
 
         return results;
     }
 
-    private double backtrackEvaluate(HashMap<String, HashMap<String, Double>> graph, String currNode, String targetNode, double accProduct, Set<String> visited) {
+    private Pair<String, Double> find(HashMap<String, Pair<String, Double>> gidWeight, String nodeId) {
+        if (!gidWeight.containsKey(nodeId))
+            gidWeight.put(nodeId, new Pair<String, Double>(nodeId, 1.0));
 
-        // mark the visit
-        visited.add(currNode);
-        double ret = -1.0;
-
-        Map<String, Double> neighbors = graph.get(currNode);
-        if (neighbors.containsKey(targetNode))
-            ret = accProduct * neighbors.get(targetNode);
-        else {
-            for (Map.Entry<String, Double> pair : neighbors.entrySet()) {
-                String nextNode = pair.getKey();
-                if (visited.contains(nextNode))
-                    continue;
-                ret = backtrackEvaluate(graph, nextNode, targetNode,
-                        accProduct * pair.getValue(), visited);
-                if (ret != -1.0)
-                    break;
-            }
+        Pair<String, Double> entry = gidWeight.get(nodeId);
+        // found inconsistency, trigger chain update
+        if (!entry.getKey().equals(nodeId)) {
+            Pair<String, Double> newEntry = find(gidWeight, entry.getKey());
+            gidWeight.put(nodeId, new Pair<String, Double>(
+                    newEntry.getKey(), entry.getValue() * newEntry.getValue()));
         }
 
-        // unmark the visit, for the next backtracking
-        visited.remove(currNode);
-        return ret;
+        return gidWeight.get(nodeId);
+    }
+
+    private void union(HashMap<String, Pair<String, Double>> gidWeight, String dividend, String divisor, Double value) {
+        Pair<String, Double> dividendEntry = find(gidWeight, dividend);
+        Pair<String, Double> divisorEntry = find(gidWeight, divisor);
+
+        String dividendGid = dividendEntry.getKey();
+        String divisorGid = divisorEntry.getKey();
+        Double dividendWeight = dividendEntry.getValue();
+        Double divisorWeight = divisorEntry.getValue();
+
+        // merge the two groups together,
+        // by attaching the dividend group to the one of divisor
+        if (!dividendGid.equals(divisorGid)) {
+            gidWeight.put(dividendGid, new Pair<String, Double>(divisorGid,
+                    divisorWeight * value / dividendWeight));
+        }
     }
 }
