557180ea2adc23a085ad37d3a58e1085a0b30148
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-04-21 14:42:17 +0530

    Time: 1 ms (89.02%), Space: 48.5 MB (8.97%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,53 +1,47 @@
+class UnionFind {
+    int[] parent;
+    int[] rank;
+    public UnionFind(int size) {
+        parent = new int[size];
+        for (int i = 0; i < size; i++)
+            parent[i] = i;
+        rank = new int[size];
+    }
+    public int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+    public void union_set(int x, int y) {
+        int xset = find(x), yset = find(y);
+        if (xset == yset) {
+            return;
+        } else if (rank[xset] < rank[yset]) {
+            parent[xset] = yset;
+        } else if (rank[xset] > rank[yset]) {
+            parent[yset] = xset;
+        } else {
+            parent[yset] = xset;
+            rank[xset]++;
+        }
+    }
+}
+
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int total = isConnected.length;
-        DisjointUnion dsu = new DisjointUnion(total);
+        int n = isConnected.length;
+        UnionFind dsu = new UnionFind(n);
+        int numberOfComponents = n;
 
-        for (int i = 0; i < total; i++) {
-            for (int j = 0; j < isConnected[0].length; j++) {
-                if (isConnected[i][j] == 1) {
-                    if (dsu.union(i  + 1, j + 1)) total--;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isConnected[i][j] == 1 && dsu.find(i) != dsu.find(j)) {
+                    numberOfComponents--;
+                    dsu.union_set(i, j);
                 }
             }
         }
 
-        return total;
-    }
-
-    public class DisjointUnion {
-        int[] parent;
-        int[] rank;
-
-        DisjointUnion(int n) {
-            parent = new int[n + 1];
-            rank = new int[n + 1];
-
-            for (int i = 0; i < n; i++) {
-                parent[i] = i;
-                rank[i] = 1;
-            }
-        }
-
-        int find(int x) {
-            if (parent[x] == x) return x;
-            return parent[x] = find(parent[x]);  // path compression
-        }
-
-        boolean union(int u, int v) {
-            int u1 = find(u);
-            int v1 = find(v);
-
-            if (u1 == v1) return false;
-
-            if (rank[u1] > rank[v1]) {
-                rank[u1] += rank[v1];
-                parent[v1] = u1;
-            } else {
-                rank[v1] += rank[u1];
-                parent[u1] = v1;
-            }
-
-            return true;
-        }
+        return numberOfComponents;
     }
 }
