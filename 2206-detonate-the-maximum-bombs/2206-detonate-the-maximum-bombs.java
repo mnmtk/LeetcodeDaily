24048efc96f46429ab60669475f86e7e8bf1cfe5
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-04-22 15:57:51 +0530

    Time: 195 ms (14.87%), Space: 44.9 MB (66.99%) - LeetHub

--- 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.java
+++ 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.java
@@ -5,6 +5,7 @@ class Solution {
 
         for(int i = 0;i < bombs.length; i++) { //O(Nsquare)
             for(int j = 0; j < bombs.length; j++) {
+
                 int x1 = bombs[i][0];
                 int x2 = bombs[j][0];
 
@@ -12,20 +13,7 @@ class Solution {
                 int y2 = bombs[j][1];
 
                 int r = bombs[i][2];
-                
-            //   System.out.println(r*r);
-
-            //     System.out.println((long)r*r);
-
-            //     System.out.println((x1 - x2) * (x1 - x2));
-
-            //     System.out.println((long)(x1 - x2) * (x1 - x2));
-
-            //     System.out.println((y1 - y2) * (y1 - y2));
-
-            //     System.out.println((long)(y1 - y2) * (y1 - y2));
-            //logs can cause TLE!
-
+                //logs 
 
                 if((long)r*r >= (long)(x1 - x2) * (x1 - x2) + (long)(y1 - y2) * (y1 - y2)) {
                     graph.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
@@ -63,3 +51,16 @@ class Solution {
         return visited.size();
     }
 }
+
+         //   System.out.println(r*r);
+
+            //     System.out.println((long)r*r);
+
+            //     System.out.println((x1 - x2) * (x1 - x2));
+
+            //     System.out.println((long)(x1 - x2) * (x1 - x2));
+
+            //     System.out.println((y1 - y2) * (y1 - y2));
+
+            //     System.out.println((long)(y1 - y2) * (y1 - y2));
+            //logs can cause TLE!
