d7990e8b255379931c4639955c8cac4d42cf7cf6
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-04-22 13:01:21 +0530

    Time: 73 ms (24.59%), Space: 47.4 MB (84.4%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.java
+++ 1558-course-schedule-iv/1558-course-schedule-iv.java
@@ -1,52 +1,60 @@
 public class Solution {
 
-    // Iterate over each node and perform BFS starting from it.
-    // Mark the starting node as a prerequisite to all the nodes in the BFS
-    // traversal.
-    private void preprocess(
-        int numCourses,
-        int[][] prerequisites,
-        Map<Integer, List<Integer>> adjList,
-        boolean[][] isPrerequisite
-    ) {
-        for (int i = 0; i < numCourses; i++) {
-            Queue<Integer> q = new LinkedList<>();
-            q.offer(i);
-
-            while (!q.isEmpty()) {
-                int node = q.poll();
-
-                for (int adj : adjList.getOrDefault(node, new ArrayList<>())) {
-                    // If we have marked i as a prerequisite of adj it implies we
-                    // have visited it. This is equivalent to using a visited
-                    // array.
-                    if (!isPrerequisite[i][adj]) {
-                        isPrerequisite[i][adj] = true;
-                        q.offer(adj);
-                    }
-                }
-            }
-        }
-    }
-
     public List<Boolean> checkIfPrerequisite(
         int numCourses,
         int[][] prerequisites,
         int[][] queries
     ) {
         Map<Integer, List<Integer>> adjList = new HashMap<>();
+        int[] indegree = new int[numCourses];
+
         for (int[] edge : prerequisites) {
             adjList
                 .computeIfAbsent(edge[0], k -> new ArrayList<>())
                 .add(edge[1]);
+            indegree[edge[1]]++;
         }
 
-        boolean[][] isPrerequisite = new boolean[numCourses][numCourses];
-        preprocess(numCourses, prerequisites, adjList, isPrerequisite);
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        // Map from the node as key to the set of prerequisite nodes.
+        Map<Integer, Set<Integer>> nodePrerequisites = new HashMap<>();
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+
+            for (int adj : adjList.getOrDefault(node, new ArrayList<>())) {
+                // Add node and prerequisites of the node to the prerequisites of adj
+                nodePrerequisites
+                    .computeIfAbsent(adj, k -> new HashSet<>())
+                    .add(node);
+                    
+                for (int prereq : nodePrerequisites.getOrDefault(
+                    node,
+                    new HashSet<>()
+                )) {
+                    nodePrerequisites.get(adj).add(prereq);
+                }
+
+                indegree[adj]--;
+                if (indegree[adj] == 0) {
+                    q.offer(adj);
+                }
+            }
+        }
 
         List<Boolean> answer = new ArrayList<>();
         for (int[] query : queries) {
-            answer.add(isPrerequisite[query[0]][query[1]]);
+            answer.add(
+                nodePrerequisites
+                    .getOrDefault(query[1], new HashSet<>())
+                    .contains(query[0])
+            );
         }
 
         return answer;
