e14ba41ff958b01c9cc444dd173a2790aa063905
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-04-25 12:10:11 +0530

    Time: 5 ms (76.05%), Space: 45.5 MB (89.63%) - LeetHub

--- 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.java
+++ 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.java
@@ -1,37 +1,47 @@
 class UnionFind {
     private final int n;
     private final int[] parents;
+    private final int[] size;
     public int components;
-    
+
     UnionFind(int n) {
         this.n = n;
         parents = new int[n];
+        size = new int[n];
         components = n;
-        
+
         for (int i = 0; i < n; i++) {
             parents[i] = i;
+            size[i] = 1;
         }
     }
-    
+
     public boolean union(int parent, int child) {
         int parentParent = find(parent);
         int childParent = find(child);
-        
+
         if (childParent != child || parentParent == childParent) {
             return false;
         }
-        
+
         components--;
-        parents[childParent] = parentParent;
+
+        // if (size[childParent] > size[parentParent]) {
+            parents[childParent] = parentParent;
+            size[parentParent] += size[childParent];
+        // } else {
+        //     parents[parentParent] = childParent;
+        //     size[parentParent] += size[childParent];
+        // }
 
         return true;
     }
-    
+
     private int find(int node) {
         if (parents[node] != node) {
             parents[node] = find(parents[node]);
         }
-        
+
         return parents[node];
     }
 }
@@ -40,18 +50,18 @@ class Solution {
     public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
         UnionFind uf = new UnionFind(n);
         for (int node = 0; node < n; node++) {
-            int[] children = {leftChild[node], rightChild[node]};
+            int[] children = { leftChild[node], rightChild[node] };
             for (int child : children) {
                 if (child == -1) {
                     continue;
                 }
-                
+
                 if (!uf.union(node, child)) {
                     return false;
                 }
             }
         }
-        
+
         return uf.components == 1;
     }
 }
