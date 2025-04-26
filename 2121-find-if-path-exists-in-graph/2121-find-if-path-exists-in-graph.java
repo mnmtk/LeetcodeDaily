c3ea7d83dba656b58292eeff4444592f432ebfc4
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-04-27 04:39:41 +0530

    Time: 128 ms (63.21%), Space: 197.4 MB (16.14%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
@@ -2,26 +2,51 @@ class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         Map<Integer, List<Integer>> graph = new HashMap<>();
 
-        for(int[] edge : edges) {
-            graph.computeIfAbsent(edge[0], k-> new ArrayList<>()).add(edge[1]);
-            graph.computeIfAbsent(edge[1], k-> new ArrayList<>()).add(edge[0]);
+        for (int[] edge : edges) {
+            graph.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
+            graph.computeIfAbsent(edge[1], k -> new ArrayList<>()).add(edge[0]);
         }
 
         int[] visited = new int[n];
 
-        return dfs(source, destination, graph, visited);
+        //return dfs(source, destination, graph, visited);
+        return bfs(source, destination, graph, visited);
     }
 
-    public boolean dfs(int source, int destination,  Map<Integer, List<Integer>> graph, 
-    int[] visited) {
+    public boolean dfs(int source, int destination, Map<Integer, List<Integer>> graph,
+            int[] visited) {
 
-        if(source == destination) return true;
+        if (source == destination)
+            return true;
 
         visited[source] = 1;
 
-        for(int nei : graph.get(source)) {
-            if(visited[nei] != 1) {
-            if (dfs(nei, destination, graph, visited)) return true;
+        for (int nei : graph.get(source)) {
+            if (visited[nei] != 1) {
+                if (dfs(nei, destination, graph, visited))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean bfs(int source, int destination, Map<Integer, List<Integer>> graph,
+            int[] visited) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(source);
+
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            if (curr == destination)
+                return true;
+
+     
+            for (int nei : graph.get(curr)) {
+                if (visited[nei] != 1) {
+                    visited[nei] = 1;
+                    queue.offer(nei);
+                }
             }
         }
 
