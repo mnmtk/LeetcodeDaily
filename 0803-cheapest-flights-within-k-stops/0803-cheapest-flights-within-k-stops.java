d06a16f7bc0abb30ede0b9411f9fe004a5ecb6cd
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-04-29 16:30:07 +0530

    Time: 7 ms (48.26%), Space: 44.4 MB (70.63%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
@@ -7,25 +7,23 @@ class Solution {
 
         int[] stops = new int[n];
         Arrays.fill(stops, Integer.MAX_VALUE);
-
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
 
-        // Format: [cost, current node, number of stops]
         pq.offer(new int[] { 0, src, 0 });
         int minCost = Integer.MAX_VALUE;
 
         while (!pq.isEmpty()) {
             int[] temp = pq.poll();
+
             int dist = temp[0];
             int node = temp[1];
             int steps = temp[2];
 
             if (steps > k + 1) continue;
-
+            
             if (node == dst) {
                 return dist;
             }
-
             if (steps > stops[node]) continue;
 
             stops[node] = steps;
@@ -34,6 +32,7 @@ class Solution {
             for (int[] a : adj.get(node)) {
                 pq.offer(new int[] { dist + a[1], a[0], steps + 1 });
             }
+
         }
 
         return minCost == Integer.MAX_VALUE ? -1 : minCost;
