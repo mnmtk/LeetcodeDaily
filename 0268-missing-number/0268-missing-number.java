f25b8a4a7cda5b9d26f9113ccdaf6cb4775c5854
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-04-29 23:36:05 +0530

    Time: 1 ms (35.13%), Space: 45.3 MB (56.39%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,8 +1,26 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int expectedSum = nums.length*(nums.length + 1)/2;
-        int actualSum = 0;
-        for (int num : nums) actualSum += num;
-        return expectedSum - actualSum;
+        int n = nums.length, i = 0;
+        
+        while (i < n) {
+            int correctIdx = nums[i];
+            if (nums[i] < n && nums[i] != nums[correctIdx]) {
+                int temp = nums[i];
+                nums[i] = nums[correctIdx];
+                nums[correctIdx] = temp;
+            } else {
+				i++;
+			}
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (j != nums[j]) {
+                return j;
+            }
+        }
+
+        return n;
     }
 }
+
+// TC: O(n), SC: O(1)
