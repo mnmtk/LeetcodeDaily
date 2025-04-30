457236c736db0d77750fa9eea97c3abcfca669be
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-01 00:18:39 +0530

    Time: 116 ms (24.01%), Space: 72.2 MB (30.38%) - LeetHub

--- 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.java
+++ 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.java
@@ -11,12 +11,14 @@ class Solution {
                 longestSubarray = i + 1;
             }
 
-
+            // If any subarray seen so far sums to k, then
+            // update the length of the longest_subarray. 
             if (indices.containsKey(prefixSum - k)) {
                 longestSubarray = Math.max(longestSubarray, i - indices.get(prefixSum - k));
             }
             
-
+            // Only add the current prefix_sum index pair to the 
+            // map if the prefix_sum is not already in the map.
             if (!indices.containsKey(prefixSum)) {
                 indices.put(prefixSum, i);
             }
