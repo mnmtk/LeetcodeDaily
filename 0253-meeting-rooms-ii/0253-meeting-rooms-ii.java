49fefa2390fef7308a40fddc33fc9f886c476a24
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-01 13:13:16 +0530

    Time: 8 ms (66.65%), Space: 46.6 MB (11.57%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,48 +1,48 @@
+//you can say that once a room is added to the heap, it stays there until it is replaced by another meeting that can reuse the same room. Here's how to frame this idea:
+
+//Room Addition: When a new meeting starts and no existing room is available 
+//(i.e., all rooms are still occupied by earlier meetings), a new room is added to the heap. 
+//This happens when the start time of the new meeting is less than the earliest end time in the heap.
+
+//Room Replacement: If a new meeting starts and there is a room that will become available before 
+//or at the start time of this new meeting (i.e., the earliest end time in the heap is less than or equal 
+//to the start time of the new meeting), then that room is "replaced" or reused. 
+//This is achieved by removing the earliest end time from the heap (freeing up the room) and adding the end time of the new meeting to the heap.
+
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        if(intervals.length == 0) return 0;
 
-        Integer[] start = new Integer[intervals.length];
-        Integer[] end = new Integer[intervals.length];
-
-        for(int i = 0; i < intervals.length; i++) {
-            start[i] = intervals[i][0];
-            end[i] = intervals[i][1];
+        if (intervals.length == 0) {
+            return 0;
         }
 
-          // Sort the intervals by end time
-    Arrays.sort(
-        end,
-        new Comparator<Integer>() {
-          public int compare(Integer a, Integer b) {
-            return a - b;
-          }
-        });
+        PriorityQueue<Integer> allocator = new PriorityQueue<Integer>( 
+                intervals.length,
+                new Comparator<Integer>() {
+                    public int compare(Integer a, Integer b) {
+                        return a - b;
+                    }
+                });
 
-    // Sort the intervals by start time
-    Arrays.sort(
-        start,
-        new Comparator<Integer>() {
-          public int compare(Integer a, Integer b) {
-            return a - b;
-          }
-        });
+        Arrays.sort(
+                intervals,
+                new Comparator<int[]>() {
+                    public int compare(final int[] a, final int[] b) {
+                        return a[0] - b[0];
+                    }
+                });
 
-     int usedRooms = 0;
+        allocator.add(intervals[0][1]);
 
-     int startPointer = 0, endPointer = 0;
+        for (int i = 1; i < intervals.length; i++) {
 
-    while (startPointer < intervals.length) {
+            if (!allocator.isEmpty() && intervals[i][0] >= allocator.peek()) {
+                allocator.poll();
+            }
 
-      if (start[startPointer] >= end[endPointer]) {
-        usedRooms -= 1;
-        endPointer += 1;
-      }
+            allocator.add(intervals[i][1]);
+        }
 
-      usedRooms += 1;
-      startPointer += 1;
-    }
-
-    return usedRooms;
+        return allocator.size();
     }
 }
