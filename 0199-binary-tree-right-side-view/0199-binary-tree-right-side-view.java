a34bf64334ebd42f0703f1fc644bc36329f5c73a
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-02 22:09:19 +0530

    Time: 1 ms (70.79%), Space: 42.3 MB (43.77%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) return new ArrayList<Integer>();
+
+        ArrayDeque<TreeNode> nextLevel = new ArrayDeque() {
+            {
+                offer(root);
+            }
+        };
+        ArrayDeque<TreeNode> currLevel = new ArrayDeque();
+        List<Integer> rightside = new ArrayList();
+
+        TreeNode node = null;
+        while (!nextLevel.isEmpty()) {
+            // prepare for the next level
+            currLevel = nextLevel;
+            nextLevel = new ArrayDeque<>();
+
+            while (!currLevel.isEmpty()) {
+                node = currLevel.poll();
+
+                // add child nodes of the current level
+                // in the queue for the next level
+                if (node.left != null) nextLevel.offer(node.left);
+                if (node.right != null) nextLevel.offer(node.right);
+            }
+
+            // The current level is finished.
+            // Its last element is the rightmost one.
+            rightside.add(node.val);
+        }
+        return rightside;
+    }
+}
