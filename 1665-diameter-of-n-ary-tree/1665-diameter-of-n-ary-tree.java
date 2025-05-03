8a365fc3eabb43cca68c0ac8093f854171db09f2
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-04 00:21:33 +0530

    Time: 0 ms (100%), Space: 43.8 MB (99.02%) - LeetHub

---
+++ 1665-diameter-of-n-ary-tree/1665-diameter-of-n-ary-tree.java
@@ -0,0 +1,56 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    
+    public Node() {
+        children = new ArrayList<Node>();
+    }
+    
+    public Node(int _val) {
+        val = _val;
+        children = new ArrayList<Node>();
+    }
+    
+    public Node(int _val,ArrayList<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    protected int diameter = 0;
+
+    protected int height(Node node) {
+        if(node.children.size() == 0) {
+            return 0;
+        }
+
+        int maxHeight1 = 0;
+        int maxHeight2 = 0;
+
+        for(Node child : node.children) {
+            int parentHeight = height(child) + 1;
+            if(parentHeight > maxHeight1) {
+                maxHeight2 = maxHeight1;
+                maxHeight1 = parentHeight;
+            } else if(parentHeight > maxHeight2) {
+                maxHeight2 = parentHeight;
+            }
+
+            int distance = maxHeight1 + maxHeight2;
+            this.diameter = Math.max(this.diameter, distance);
+        }
+
+        return maxHeight1;
+    }
+
+    public int diameter(Node root) {
+        this.diameter = 0;
+        height(root);
+        return diameter;
+    }
+}
