01d0334a4bcb413956be05723bd1d0b560e6215f
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-03 14:30:40 +0530

    Time: 13 ms (65.92%), Space: 42.6 MB (73.89%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.java
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.java
@@ -1,50 +1,44 @@
 class Solution {
 
-    Map<Integer, List<Integer>> graph;
-    List<Integer> answer;
-    Set<Integer> visited;
+    Map<TreeNode, TreeNode> parent;
 
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        graph = new HashMap<>();
-        buildGraph(root, null);
+        parent = new HashMap<>();
+        addParent(root, null);
 
-        answer = new ArrayList<>();
-        visited = new HashSet<>();
-        visited.add(target.val);
-
-        dfs(target.val, 0, k);
+        List<Integer> answer = new ArrayList<>();
+        Set<TreeNode> visited = new HashSet<>();
+        dfs(target, k, answer, visited);
 
         return answer;
     }
 
-    // Recursively build the undirected graph from the given binary tree.
-    private void buildGraph(TreeNode cur, TreeNode parent) {
-        if (cur != null && parent != null) {
-            graph
-                .computeIfAbsent(cur.val, k -> new ArrayList<>())
-                .add(parent.val);
-            graph
-                .computeIfAbsent(parent.val, k -> new ArrayList<>())
-                .add(cur.val);
-        }
-        if (cur.left != null) {
-            buildGraph(cur.left, cur);
-        }
-        if (cur.right != null) {
-            buildGraph(cur.right, cur);
+    private void addParent(TreeNode cur, TreeNode parent) {
+        if (cur != null) {
+            this.parent.put(cur, parent);
+            addParent(cur.left, cur);
+            addParent(cur.right, cur);
         }
     }
 
-    private void dfs(int cur, int distance, int k) {
-        if (distance == k) {
-            answer.add(cur);
+    private void dfs(
+        TreeNode cur,
+        int distance,
+        List<Integer> answer,
+        Set<TreeNode> visited
+    ) {
+        if (cur == null || visited.contains(cur)) {
             return;
         }
-        for (int neighbor : graph.getOrDefault(cur, new ArrayList<>())) {
-            if (!visited.contains(neighbor)) {
-                visited.add(neighbor);
-                dfs(neighbor, distance + 1, k);
-            }
+
+        visited.add(cur);
+        if (distance == 0) {
+            answer.add(cur.val);
+            return;
         }
+
+        dfs(parent.get(cur), distance - 1, answer, visited);
+        dfs(cur.left, distance - 1, answer, visited);
+        dfs(cur.right, distance - 1, answer, visited);
     }
 }
