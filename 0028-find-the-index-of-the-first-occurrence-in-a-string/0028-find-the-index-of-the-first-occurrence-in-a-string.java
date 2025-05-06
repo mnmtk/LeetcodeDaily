5206fe15c69b4f2835492e1e0055097106512e3c
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-07 02:25:54 +0530

    Time: 1 ms (21.98%), Space: 41.7 MB (34.08%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,19 +1,61 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int m = needle.length();
-        int n = haystack.length();
+       // using rabin karpalgo
+       //using basic hash functions
 
-        for (int windowStart = 0; windowStart <= n - m; windowStart++) {
-            for (int i = 0; i < m; i++) {
-                if (needle.charAt(i) != haystack.charAt(windowStart + i)) {
+       if(needle.length()>haystack.length())
+       {
+           return -1;
+       }
+
+       int hash1=0;
+       for(int i=0;i<needle.length();i++)
+       {
+           hash1+=needle.charAt(i)-'a';
+       }
+        int hash2=0;
+        
+        for(int j=0;j<needle.length();j++)
+        {
+            hash2+=haystack.charAt(j)-'a';
+        }
+    
+        if(hash1==hash2)
+        {
+            int i;
+            for( i=0;i<needle.length();i++)
+            {
+                if(needle.charAt(i)!=haystack.charAt(i))
+                {
                     break;
                 }
-                if (i == m - 1) {
-                    return windowStart;
-                }
+            }
+            if(i==needle.length())
+            {
+                return 0;
             }
         }
+       for(int j=needle.length();j<haystack.length();j++)
+       {
+           hash2-=haystack.charAt(j-needle.length())-'a';
+           hash2+=haystack.charAt(j)-'a';
+             if(hash1==hash2)
+        {
+            int i;
+            for( i=0;i<needle.length();i++)
+            {
+                if(needle.charAt(i)!=haystack.charAt(j-needle.length()+1+i))
+                {
+                    break;
+                }
+            }
+            if(i==needle.length())
+            {
+                return j-needle.length()+1;
+            }
+        }
+       }
+       return -1;
 
-        return -1;
     }
 }
