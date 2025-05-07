f062ee88ac5ed351618b6b055bc219b1ff6e28bb
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-07 17:52:25 +0530

    Time: 1 ms (16.59%), Space: 41.1 MB (66.83%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -1,35 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    int rootToLeaf = 0;
-
-    public void preorder(TreeNode r, int currNumber) {
-        if (r != null) {
-            currNumber = currNumber * 10 + r.val;
-            // if it's a leaf, update root-to-leaf sum
-            if (r.left == null && r.right == null) {
-                rootToLeaf += currNumber;
-            }
-            preorder(r.left, currNumber);
-            preorder(r.right, currNumber);
-        }
-    }
-
     public int sumNumbers(TreeNode root) {
-        preorder(root, 0);
+        int rootToLeaf = 0, currNumber = 0;
+        Deque<Pair<TreeNode, Integer>> stack = new ArrayDeque();
+        stack.push(new Pair(root, 0));
+
+        while (!stack.isEmpty()) {
+            Pair<TreeNode, Integer> p = stack.pop();
+            root = p.getKey();
+            currNumber = p.getValue();
+
+            if (root != null) {
+                currNumber = currNumber * 10 + root.val;
+                // if it's a leaf, update root-to-leaf sum
+                if (root.left == null && root.right == null) {
+                    rootToLeaf += currNumber;
+                } else {
+                    stack.push(new Pair(root.left, currNumber));
+                    stack.push(new Pair(root.right, currNumber));
+                }
+            }
+        }
         return rootToLeaf;
     }
 }
