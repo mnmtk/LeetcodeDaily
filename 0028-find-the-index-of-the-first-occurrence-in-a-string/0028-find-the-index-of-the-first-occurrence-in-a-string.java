2d9656de97890e0cc2da0b0963daffc861fed152
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-07 11:33:37 +0530

    Time: 1 ms (21.98%), Space: 41.4 MB (89.09%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,61 +1,58 @@
 class Solution {
+    public int hashValue(String string, int RADIX, int MOD, int m) {
+        long ans = 0;
+        long factor = 1;
+        for (int i = m - 1; i >= 0; i--) {
+            ans = (ans + (string.charAt(i) - 'a') * factor) % MOD;
+            factor = (factor * RADIX) % MOD;
+        }
+        return (int) ans;
+    }
+
     public int strStr(String haystack, String needle) {
-       // using rabin karpalgo
-       //using basic hash functions
+        int m = needle.length();
+        int n = haystack.length();
+        if (n < m) return -1;
 
-       if(needle.length()>haystack.length())
-       {
-           return -1;
-       }
+        // CONSTANTS
+        int RADIX = 26;
+        int MOD = 1000000033;
+        long MAX_WEIGHT = 1;
 
-       int hash1=0;
-       for(int i=0;i<needle.length();i++)
-       {
-           hash1+=needle.charAt(i)-'a';
-       }
-        int hash2=0;
-        
-        for(int j=0;j<needle.length();j++)
-        {
-            hash2+=haystack.charAt(j)-'a';
-        }
-    
-        if(hash1==hash2)
-        {
-            int i;
-            for( i=0;i<needle.length();i++)
-            {
-                if(needle.charAt(i)!=haystack.charAt(i))
-                {
-                    break;
+        for (int i = 0; i < m; i++) MAX_WEIGHT = (MAX_WEIGHT * RADIX) % MOD;
+
+        // Compute hash of needle
+        long hashNeedle = hashValue(needle, RADIX, MOD, m), hashHay = 0;
+
+        // Check for each m-substring of haystack, starting at index windowStart
+        for (int windowStart = 0; windowStart <= n - m; windowStart++) {
+            if (windowStart == 0) {
+                // Compute hash of the First Substring
+                hashHay = hashValue(haystack, RADIX, MOD, m);
+            } else {
+                // Update Hash using Previous Hash Value in O(1)
+                hashHay = (((hashHay * RADIX) % MOD) -
+                    (((int) (haystack.charAt(windowStart - 1) - 'a') *
+                            MAX_WEIGHT) %
+                        MOD) +
+                    (int) (haystack.charAt(windowStart + m - 1) - 'a') +
+                    MOD) %
+                MOD;
+            }
+            // If the hash matches, Check Character by Character.
+            // Because of Mod, spurious hits can be there.
+            if (hashNeedle == hashHay) {
+                for (int i = 0; i < m; i++) {
+                    if (needle.charAt(i) != haystack.charAt(i + windowStart)) {
+                        break;
+                    }
+                    if (i == m - 1) {
+                        return windowStart;
+                    }
                 }
             }
-            if(i==needle.length())
-            {
-                return 0;
-            }
         }
-       for(int j=needle.length();j<haystack.length();j++)
-       {
-           hash2-=haystack.charAt(j-needle.length())-'a';
-           hash2+=haystack.charAt(j)-'a';
-             if(hash1==hash2)
-        {
-            int i;
-            for( i=0;i<needle.length();i++)
-            {
-                if(needle.charAt(i)!=haystack.charAt(j-needle.length()+1+i))
-                {
-                    break;
-                }
-            }
-            if(i==needle.length())
-            {
-                return j-needle.length()+1;
-            }
-        }
-       }
-       return -1;
 
+        return -1;
     }
 }
