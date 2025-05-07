13da42153c1fa188128b7394aa7e5107f8348aa7
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-07 14:30:43 +0530

    Time: 1 ms (84.78%), Space: 42.6 MB (8.62%) - LeetHub

--- 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.java
+++ 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.java
@@ -1,26 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isCompleteTree(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
+        if(root == null) return true;
 
-        boolean nullNodeFound = false;
+        boolean nullFound = false;
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
 
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()) {
             TreeNode node = q.poll();
 
-            if (node == null) {
-                nullNodeFound = true;
+            if(node == null) {
+                nullFound = true;
             } else {
-                if (nullNodeFound) {
-                    return false;
-                }
+                if(nullFound) return false;
+
                 q.offer(node.left);
                 q.offer(node.right);
             }
         }
+
         return true;
     }
 }
