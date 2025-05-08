213129a2149a4a63f2ef9cca44a2e05090a6ebb1
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-08 15:26:56 +0530

    Time: 53 ms (80.19%), Space: 51.2 MB (20.29%) - LeetHub

--- 0914-random-point-in-non-overlapping-rectangles/0914-random-point-in-non-overlapping-rectangles.java
+++ 0914-random-point-in-non-overlapping-rectangles/0914-random-point-in-non-overlapping-rectangles.java
@@ -7,7 +7,7 @@ class Solution {
 
     public Solution(int[][] rects) {
         this.rects = rects;
-        for (int[] x : rects){
+        for (int[] x : rects) {
             tot += (x[2] - x[0] + 1) * (x[3] - x[1] + 1);
             psum.add(tot);
         }
@@ -20,8 +20,10 @@ class Solution {
         int hi = rects.length - 1;
         while (lo != hi) {
             int mid = (lo + hi) / 2;
-            if (targ >= psum.get(mid)) lo = mid + 1;
-            else hi = mid;
+            if (targ >= psum.get(mid))
+                lo = mid + 1;
+            else
+                hi = mid;
         }
 
         int[] x = rects[lo];
@@ -30,11 +32,28 @@ class Solution {
         int base;
 
         if (lo == 0) {
-         base = psum.get(lo) - width * height;
+            base = psum.get(lo) - width * height;
         } else {
-            base = psum.get(lo-1);
+            base = psum.get(lo - 1);
         }
+
+        return new int[] { x[0] + (targ - base) % width, x[1] + (targ - base) / width };
+
+        //When converting a 1D offset to 2D coordinates:
+
+        // The column (x) is found with offset % width (modulus by number of columns).
+        // The row (y) is found with offset / width (integer division by number of columns)
+
+        /*
+        For a given offset:
+        The X-coordinate (column) is:
+        x0 + (offset mod width)
+
+        This moves you horizontally across the grid. Each time the offset increases by 1, you move to the next column. 
+        When you reach the end of a row (i.e., offset % width = 0), you wrap to the next row.
         
-        return new int[]{x[0] + (targ - base) % width, x[1] + (targ - base) / width};
+        The Y-coordinate (row) is:
+        y0+(offset÷width)
+        This moves you vertically. Each time you complete a row (i.e., after width increments), you move down to the next row.*/
     }
 }
