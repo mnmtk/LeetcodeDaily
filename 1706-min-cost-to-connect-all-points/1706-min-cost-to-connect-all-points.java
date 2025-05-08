b9c6d17b2f694a05520ea3c0207983c57deb5390
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-08 23:28:24 +0530

    Time: 92 ms (63.98%), Space: 78.4 MB (24.03%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.java
@@ -1,75 +1,41 @@
-class UnionFind {
-    public int[] group;
-    public int[] rank;
-
-    public UnionFind(int size) {
-        group = new int[size];
-        rank = new int[size];
-        for (int i = 0; i < size; ++i) {
-            group[i] = i;
-        }
-    }
-
-    public int find(int node) {
-        if (group[node] != node) {
-            group[node] = find(group[node]);
-        }
-        return group[node];
-    }
-
-    public boolean union(int node1, int node2) {
-        int group1 = find(node1);
-        int group2 = find(node2);
-        
-        // node1 and node2 already belong to same group.
-        if (group1 == group2) {
-            return false;
-        }
-
-        if (rank[group1] > rank[group2]) {
-            group[group2] = group1;
-        } else if (rank[group1] < rank[group2]) {
-            group[group1] = group2;
-        } else {
-            group[group1] = group2;
-            rank[group2] += 1;
-        }
-
-        return true;
-    }
-}
-
 class Solution {
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
-        ArrayList<int[]> allEdges = new ArrayList<>();
         
-        // Storing all edges of our complete graph.
-        for (int currNext = 0; currNext < n; ++currNext) {
-            for (int nextNext = currNext + 1; nextNext < n; ++nextNext) {
-                int weight = Math.abs(points[currNext][0] - points[nextNext][0]) + 
-                             Math.abs(points[currNext][1] - points[nextNext][1]);
-                
-                int[] currEdge = {weight, currNext, nextNext};
-                allEdges.add(currEdge);
-            }
-        }
+        // Min-heap to store minimum weight edge at top.
+        PriorityQueue<Pair<Integer, Integer>> heap = new PriorityQueue<>((a, b) -> (a.getKey() - b.getKey()));;
         
-        // Sort all edges in increasing order.
-        Collections.sort(allEdges, (a, b) -> Integer.compare(a[0], b[0]));   
+        // Track nodes which are included in MST.
+        boolean[] inMST = new boolean[n];
         
-        UnionFind uf = new UnionFind(n);
+        heap.add(new Pair(0, 0));
         int mstCost = 0;
         int edgesUsed = 0;
         
-        for (int i = 0; i < allEdges.size() && edgesUsed < n - 1; ++i) {
-            int node1 = allEdges.get(i)[1];
-            int node2 = allEdges.get(i)[2];
-            int weight = allEdges.get(i)[0];
+        while (edgesUsed < n) {
+            Pair<Integer, Integer> topElement = heap.poll();
             
-            if (uf.union(node1, node2)) {
-                mstCost += weight;
-                edgesUsed++;
+            int weight = topElement.getKey();
+            int currNode = topElement.getValue();
+            
+            // If node was already included in MST we will discard this edge.
+            if (inMST[currNode]) {
+                continue;
+            }
+            
+            inMST[currNode] = true;
+            mstCost += weight;
+            edgesUsed++;
+            
+            for (int nextNode = 0; nextNode < n; ++nextNode) {
+                // If next node is not in MST, then edge from curr node
+                // to next node can be pushed in the priority queue.
+                if (!inMST[nextNode]) {
+                    int nextWeight = Math.abs(points[currNode][0] - points[nextNode][0]) + 
+                                     Math.abs(points[currNode][1] - points[nextNode][1]);
+        
+                    heap.add(new Pair(nextWeight, nextNode));
+                }
             }
         }
         
