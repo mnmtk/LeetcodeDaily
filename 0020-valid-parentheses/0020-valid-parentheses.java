a44d18589f63834c90dccdbe4c63b4c90c49d496
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-08 18:23:17 +0530

    Time: 3 ms (41.58%), Space: 41.8 MB (65.75%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,20 +2,18 @@ class Solution {
     public boolean isValid(String s) {
         Map<Character, Character> map = new HashMap<>();
         map.put('{', '}');
-        map.put('[', ']');
         map.put('(', ')');
+        map.put('[', ']');
 
         Stack<Character> stack = new Stack<>();
-
         for(char c : s.toCharArray()) {
             if(map.containsKey(c)) {
                 stack.push(c);
-            } else if(stack.isEmpty() || map.get(stack.pop()) != c) {
+            } else if ( stack.isEmpty() || c != map.get(stack.pop())) {
                 return false;
             }
         }
-
-        return stack.isEmpty();
         
+        return stack.isEmpty();
     }
 }
