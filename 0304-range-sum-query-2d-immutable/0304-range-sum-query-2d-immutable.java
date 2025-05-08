759fd02ed8774fa4a527a2708b0f798703a96a62
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-08 17:50:02 +0530

    Time: 107 ms (33.09%), Space: 66.2 MB (88.1%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -2,7 +2,8 @@ class NumMatrix {
     private int[][] dp;
 
     public NumMatrix(int[][] matrix) {
-        if (matrix.length == 0 || matrix[0].length == 0) return;
+        if (matrix.length == 0 || matrix[0].length == 0)
+            return;
         dp = new int[matrix.length + 1][matrix[0].length + 1];
 
         for (int r = 0; r < matrix.length; r++) {
@@ -13,9 +14,10 @@ class NumMatrix {
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return 
-        dp[row2 + 1][col2 + 1] 
-        - dp[row1][col2 + 1] 
-        - dp[row2 + 1][col1] + dp[row1][col1];
+        return dp[row2 + 1][col2 + 1]
+                - dp[row1][col2 + 1]
+
+                - dp[row2 + 1][col1]
+                + dp[row1][col1];
     }
 }
