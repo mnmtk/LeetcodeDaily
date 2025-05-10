4086ff0e1057873f533a5e33050f9249eac72c0f
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-10 17:39:05 +0530

    Time: 4 ms (77.51%), Space: 60.8 MB (86.95%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,17 +1,22 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buyOne = Integer.MAX_VALUE;
-        int SellOne = 0;
-        int buyTwo = Integer.MAX_VALUE;
-        int SellTwo = 0;
+        int k = 2;
+        if (prices.length == 0 || k == 0)
+            return 0;
+        int n = prices.length;
 
-        for(int p : prices) {
-            buyOne = Math.min(buyOne, p);
-            SellOne = Math.max(SellOne, p - buyOne);
-            
-            buyTwo = Math.min(buyTwo, p - SellOne);
-            SellTwo = Math.max(SellTwo, p - buyTwo);
+        int[] buy = new int[k + 1];
+        int[] sell = new int[k + 1];
+        Arrays.fill(buy, Integer.MIN_VALUE);
+
+        for (int price : prices) {
+            for (int j = 1; j <= k; j++) {
+                buy[j] = Math.max(buy[j], sell[j - 1] - price); // choose to buy
+                sell[j] = Math.max(sell[j], buy[j] + price); // choose to sell
+            }
         }
-        return SellTwo;
+
+        return sell[k];
     }
+
 }
