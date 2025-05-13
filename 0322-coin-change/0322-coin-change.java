9f208c3c6d8e192c55b3c17a7ada909e7dedca58
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-13 10:18:35 +0530

    Time: 39 ms (14%), Space: 46.1 MB (6.7%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,19 +1,44 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int max = amount + 1;
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, max);
-        dp[0] = 0;
 
-        for(int i = 1; i <= amount; i++) {
-            for(int coin : coins) {
-                if(coin <= i) {
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-                }
-            }
+        if(amount == 0) return 0;
+
+        if(coins.length == 0) return -1;
+
+
+        Integer[][] dp = new Integer[coins.length][amount + 1];
+
+
+        int possible = ks(coins, amount, 0, dp);
+
+        return possible == Integer.MAX_VALUE ? -1 : possible;
+
+    }
+
+    public int ks(int[] coins, int amount, int index, Integer[][] dp) {
+
+        if(amount == 0) {
+            return 0;
         }
 
-        return dp[amount] > amount ? -1 : dp[amount];
+        if(amount < 0 || coins.length == index) {
+            return Integer.MAX_VALUE;
+        }
+
+        if(dp[index][amount] != null) {
+            return dp[index][amount];
+        };
+
+        if(index >= coins.length || amount < 0) return Integer.MAX_VALUE;
         
+        int coinsInclude = ks(coins, amount - coins[index], index, dp);
+
+        if(coinsInclude != Integer.MAX_VALUE) {
+            coinsInclude++;
+        }
+
+        int coinsExclude = ks(coins, amount, index+1, dp);
+
+        return dp[index][amount] = Math.min(coinsInclude, coinsExclude);
     }
 }
