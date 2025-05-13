76bb6054213473e2c81e74984b8bd4a4dd0b07c5
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-13 10:30:21 +0530

    Time: 21 ms (14.79%), Space: 58.5 MB (34.72%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,48 +1,13 @@
-// class Solution {
-//     public boolean containsDuplicate(int[] nums) {
-//         HashMap<Integer,Integer> map = new HashMap<>();
-//         for (int i = 0; i < nums.length; i++) {
-
-//             if (map.containsKey(nums[i])) {
-//                 return true;
-//             }
-
-//             map.put(nums[i],1);
-//         }
-//         return false;
-//     }
-// }
-
-// class Solution {
-//     public boolean containsDuplicate(int[] nums) {
-//         Arrays.sort(nums);  //nlogn
-
-//         for (int i = 0; i < nums.length - 1; i++) {
-//                 if (nums[i] == nums[i+1]) {
-//                     return true;
-//                 }
-//         }
-        
-//         return false;
-//     }
-    
-// }
-
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i - 1] == nums[i])
-                return true;
-            else if (nums[i] < nums[i - 1]){
-                int temp = nums[i - 1];
-                for(int j = i - 2; j >= 0; j--){
-                    if(nums[i] == nums[j])
-                        return true;
-                }
-                nums[i - 1] = nums[i];
-                nums[i] = temp;
-            }
+    Arrays.sort(nums);
+    for (int i = 1; i < nums.length; i++) {
+        if (nums[i] == nums[i - 1]) {
+            return true;
         }
-        return false;
     }
+    return false;
+}
+
+
 }
