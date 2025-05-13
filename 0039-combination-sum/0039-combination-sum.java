705cc64e29042863b402861b7fa73a453e0e9fc4
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-13 20:06:24 +0530

    Time: 2 ms (82.88%), Space: 44.4 MB (96.59%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,31 +1,31 @@
 class Solution {
-    List<List<Integer>> v = new ArrayList<>();
+    List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
-        combine(candidates, 0, target, new ArrayList<>());
-        return v;
+        if(candidates.length == 0) return new ArrayList<>();
+
+        combinations(candidates, target, 0, new ArrayList<>());
+
+        return ans;
     }
 
-    public void combine(int[] candidates, int index, int target, List<Integer> list) {
+    public void combinations(int[] combinations, int target, int index, List<Integer> arrays) {
 
-
-        if(target < 0 || index >= candidates.length) {
+        if(target < 0 || index >= combinations.length) {
             return;
         }
 
-        if(target == 0) {
-            v.add(new ArrayList<>(list));
+        if(target == 0)  {
+            ans.add(new ArrayList<>(arrays));
             return;
         }
 
+        combinations(combinations, target, index+1, arrays);
 
-        //dont include
-        combine(candidates, index + 1, target, list);
+        arrays.add(combinations[index]);
+        combinations(combinations, target - combinations[index], index, arrays);
 
-         //include
-        list.add(candidates[index]);
-        combine(candidates, index, target - candidates[index], list);
-        list.remove(list.size() - 1);
+        arrays.remove(arrays.size() - 1);
+
     }
-
 }
