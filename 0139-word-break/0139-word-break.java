7b48c37e123e0d5361006deed12612ffee7f8dcc
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-14 00:53:23 +0530

    Time: 9 ms (21.9%), Space: 45 MB (15.18%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,19 +1,28 @@
 class Solution {
-    public static boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> wordSet = new HashSet<>(wordDict); // Use HashSet for O(1) lookups
-        int n = s.length();
-        boolean[] dp = new boolean[n + 1];
-        dp[0] = true; 
-        
-    
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && wordSet.contains(s.substring(j, i))) {
-                    dp[i] = true;
-                    break; 
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> words = new HashSet<>(wordDict);
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] seen = new boolean[s.length() + 1];
+        queue.add(0);
+
+        while (!queue.isEmpty()) {
+            int start = queue.remove();
+            if (start == s.length()) {
+                return true;
+            }
+
+            for (int end = start + 1; end <= s.length(); end++) {
+                if (seen[end]) {
+                    continue;
+                }
+
+                if (words.contains(s.substring(start, end))) {
+                    queue.add(end);
+                    seen[end] = true;
                 }
             }
         }
-        return dp[n];
+
+        return false;
     }
 }
