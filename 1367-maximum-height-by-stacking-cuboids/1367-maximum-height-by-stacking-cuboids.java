11cdaa0cac6f80833be9865272d849a6b0844f39
mnmtk <137998025+mnmtk@users.noreply.github.com>
2025-05-27 22:48:50 +0530

    Time: 8 ms (77.67%), Space: 42.7 MB (61.93%) - LeetHub

--- 1367-maximum-height-by-stacking-cuboids/1367-maximum-height-by-stacking-cuboids.java
+++ 1367-maximum-height-by-stacking-cuboids/1367-maximum-height-by-stacking-cuboids.java
@@ -6,13 +6,17 @@ class Solution {
             Arrays.sort(arr);
 
         Arrays.sort(cuboids,(a,b)->(a[0]*a[1]*a[2]-b[0]*b[1]*b[2]));
+
         int dp[] = new int[n];
         int ans = cuboids[0][2];
         
         dp[0] = ans;
         for(int i=1;i<n;i++){
             for(int j=i-1;j>=0;j--){
-                if(cuboids[j][0]<=cuboids[i][0]&&cuboids[j][1]<=cuboids[i][1]&&cuboids[j][2]<=cuboids[i][2]){
+                if(
+                cuboids[j][0]<=cuboids[i][0]
+                &&cuboids[j][1]<=cuboids[i][1]
+                &&cuboids[j][2]<=cuboids[i][2]){
                     dp[i] = Math.max(dp[i],dp[j]);
                 }
             }
