80994c1b4d0190e9241d4d20abf3fcc1b13806ec
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-01-25 02:51:32 +0530

    Time: 1 ms (99.67%), Space: 94.6 MB (22.94%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,4 +1,11 @@
 class Solution {
+
+    //think like this, if i sell today? how can i get max profit?
+    // may be if i sold least before me then -> maintain it,
+    // but we expect it to increase? -> so we maintain at each point
+
+    // TC -> O(n)
+    // sc -> O(1)
     public int maxProfit(int[] prices) {
         int maxProfit = 0;
         int lastMin = prices[0];
