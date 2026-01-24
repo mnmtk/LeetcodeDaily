41373490f8e30b098770e8aa4827ab06ac0722f0
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-01-25 02:54:41 +0530

    Time: 11 ms (92.28%), Space: 97.6 MB (14.2%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-    Arrays.sort(nums);
-    for (int i = 1; i < nums.length; i++) {
-        if (nums[i] == nums[i - 1]) {
-            return true;
-        }
+        //brute -> find for each O(n2)
+        //kinda O(n)*O(1) -> but discuss how it can affect the hashset bla bla
+
+        Set<Integer> hashSet = new HashSet<>();
+
+        for(int num : nums) if(hashSet.add(num) == false) return true;
+        
+        return false;
     }
-    return false;
-}
-
-
 }
