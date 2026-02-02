9004ab55ea1a11fcbdb3aaeeb5234e259b094abf
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-02-02 15:04:40 +0530

    Time: 551 ms (12.97%), Space: 61 MB (5.72%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,25 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> res = new HashSet<>();
-        Set<Integer> dups = new HashSet<>();
-        Map<Integer, Integer> seen = new HashMap<>();
-        for (int i = 0; i < nums.length; ++i) if (dups.add(nums[i])) {
-            for (int j = i + 1; j < nums.length; ++j) {
-                
-                int complement = -nums[i] - nums[j];
-                if (seen.containsKey(complement) && seen.get(complement) == i) {
-                    List<Integer> triplet = Arrays.asList(
-                        nums[i],
-                        nums[j],
-                        complement
-                    );
-                    Collections.sort(triplet);
-                    res.add(triplet);
+        Arrays.sort(nums);
+
+        Set<List<Integer>> ans = new HashSet<>();
+
+        for(int i = 0; i < nums.length; i ++) {
+            int p1 = i + 1;
+            int p2 = nums.length - 1;
+
+            while(p1 < p2) {
+                int sum = nums[i] + nums[p1] + nums[p2];
+
+                if(sum == 0) {
+                    List<Integer> set = new ArrayList<>();
+                    set.add(nums[i]);
+                    set.add(nums[p1]);
+                    set.add(nums[p2]);
+
+                    ans.add(set);
+                    p1++;
+                } else if (sum < 0){
+                    p1++;
+                } else {
+                    p2--;
                 }
-                //important
-                seen.put(nums[j], i);
             }
+
+
+            
         }
-        return new ArrayList(res);
+        return new ArrayList<>(ans);
     }
 }
