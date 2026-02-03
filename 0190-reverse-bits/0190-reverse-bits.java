df54e115568a4b87938e2571dcbafdbd2393da22
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-02-03 10:34:00 +0530

    Time: 0 ms (100%), Space: 42.7 MB (28.86%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,14 +1,17 @@
-public class Solution {
-    // you need treat n as an unsigned value
+class Solution {
     public int reverseBits(int n) {
-        int ret = 0, power = 31;
 
-        while (n != 0) {
-            ret += (n & 1) << power;
-            n = n >>> 1;
-            power -= 1;
-        }
-        
-        return ret;
+        // treat n as unsigned value
+
+        int ret = 0;
+        int power = 31;
+
+        while( n != 0) {
+            ret += (n&1) << power;
+            n = n >> 1;
+            power -=1;
+        }     
+
+        return ret;   
     }
 }
