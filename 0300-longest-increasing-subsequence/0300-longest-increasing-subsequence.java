505e13c29183037644a585325683ec6271c6b38d
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-02-04 00:19:54 +0530

    Time: 6 ms (87.4%), Space: 46.5 MB (29.75%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,21 +1,23 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
+        ArrayList<Integer> sub = new ArrayList<>();
+        sub.add(nums[0]);
         
         for (int i = 1; i < nums.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+            int num = nums[i];
+            if (num > sub.get(sub.size() - 1)) {
+                sub.add(num);
+            } else {
+                // Find the first element in sub that is greater than or equal to num
+                int j = 0;
+                while (num > sub.get(j)) {
+                    j += 1;
                 }
+                
+                sub.set(j, num);
             }
         }
         
-        int longest = 0;
-        for (int c: dp) {
-            longest = Math.max(longest, c);
-        }
-        
-        return longest;
+        return sub.size();
     }
 }
