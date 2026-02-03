04f4f897324d3fa9c0b9aca282c838189c7fa5fe
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-02-03 17:31:51 +0530

    Time: 0 ms (100%), Space: 42.1 MB (48.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,22 @@
-class Solution {
+public class Solution {
     public int climbStairs(int n) {
+        int memo[] = new int[n + 1];
 
-        int[] dp = new int[n+1];
-        
-        dp[0] = 1;
-        dp[1] = 1;
+        return climb_Stairs(0, n, memo);
+    }
 
-        for(int i = 2;i<=n ; i++) {
-            dp[i] = dp[i-2] + dp[i-1];
+    public int climb_Stairs(int i, int n, int memo[]) {
+        if (i > n) {
+            return 0;
         }
-
-        return dp[n];
+        if (i == n) {
+            return 1;
+        }
+        if (memo[i] > 0) {
+            return memo[i];
+        }
+        
+        memo[i] = climb_Stairs(i + 1, n, memo) + climb_Stairs(i + 2, n, memo);
+        return memo[i];
     }
 }
