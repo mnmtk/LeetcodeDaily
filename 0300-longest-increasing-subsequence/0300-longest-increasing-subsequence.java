f916a3c621c8a7f15fc732f732715998101ede6e
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-02-04 01:05:57 +0530

    Time: 5 ms (89.63%), Space: 46.2 MB (66.73%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -8,16 +8,32 @@ class Solution {
             if (num > sub.get(sub.size() - 1)) {
                 sub.add(num);
             } else {
-                // Find the first element in sub that is greater than or equal to num
-                int j = 0;
-                while (num > sub.get(j)) {
-                    j += 1;
-                }
-                
+                int j = binarySearch(sub, num);
                 sub.set(j, num);
             }
         }
         
         return sub.size();
+    }
+    
+    private int binarySearch(ArrayList<Integer> sub, int num) {
+        int left = 0;
+        int right = sub.size() - 1;
+        int mid = (left + right) / 2;
+        
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (sub.get(mid) == num) {
+                return mid;
+            }
+            
+            if (sub.get(mid) < num) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        return left;
     }
 }
