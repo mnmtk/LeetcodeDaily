b916e2612204f91440fec1b7cf6fc09a70e56132
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-02-05 19:18:16 +0530

    Time: 18 ms (92.87%), Space: 46.8 MB (93.37%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,20 +1,34 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-
-        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
-
-
-        for(int col = text2.length() -1; col >=0; col--) {
-            for(int row = text1.length() - 1; row >=0; row --) {
-
-                if(text1.charAt(row) == text2.charAt(col)) {
-                    dp[row][col] = 1 + dp[row+1][col+1];
-                } else {
-                    dp[row][col] = Math.max(dp[row + 1][col], dp[row][col + 1]);
-                }
-            }
-        }
-        
-        return dp[0][0];
+    
+  public int longestCommonSubsequence(String text1, String text2) {    
+    
+    // If text1 doesn't reference the shortest string, swap them.
+    if (text2.length() < text1.length()) {
+      String temp = text1;
+      text1 = text2;
+      text2 = temp;
     }
+      
+    // The previous column starts with all 0's and like before is 1
+    // more than the length of the first word.
+    int[] previous = new int[text1.length() + 1];
+      
+    // Iterate through each column, starting from the last one.
+    for (int col = text2.length() - 1; col >= 0; col--) {
+      // Create a new array to represent the current column.
+      int[] current = new int[text1.length() + 1];  
+      for (int row = text1.length() - 1; row >= 0; row--) {
+        if (text1.charAt(row) == text2.charAt(col)) {
+          current[row] = 1 + previous[row + 1];
+        } else {
+          current[row] = Math.max(previous[row], current[row + 1]);
+        }
+      }
+      // The current column becomes the previous one.
+      previous = current;
+    }
+        
+    // The original problem's answer is in previous[0]. Return it.
+    return previous[0];
+  }
 }
