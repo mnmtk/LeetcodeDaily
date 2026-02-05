718e7e4b226b7b3ea959a265ee5093a07fbd78dc
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-02-05 19:18:49 +0530

    Time: 17 ms (94.17%), Space: 42.7 MB (98.47%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -9,14 +9,13 @@ class Solution {
       text2 = temp;
     }
       
-    // The previous column starts with all 0's and like before is 1
-    // more than the length of the first word.
+    // The previous and current column starts with all 0's and like 
+    // before is 1 more than the length of the first word.
     int[] previous = new int[text1.length() + 1];
+    int[] current = new int[text1.length() + 1];
       
     // Iterate through each column, starting from the last one.
     for (int col = text2.length() - 1; col >= 0; col--) {
-      // Create a new array to represent the current column.
-      int[] current = new int[text1.length() + 1];  
       for (int row = text1.length() - 1; row >= 0; row--) {
         if (text1.charAt(row) == text2.charAt(col)) {
           current[row] = 1 + previous[row + 1];
@@ -24,8 +23,10 @@ class Solution {
           current[row] = Math.max(previous[row], current[row + 1]);
         }
       }
-      // The current column becomes the previous one.
+      // The current column becomes the previous one, and vice versa.
+      int[] temp = previous;
       previous = current;
+      current = temp;
     }
         
     // The original problem's answer is in previous[0]. Return it.
