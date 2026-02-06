14660ce40e5b2ca7e79d0f6356941f0b563b73bf
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-02-06 20:03:58 +0530

    Time: 3 ms (78.62%), Space: 43.5 MB (77.2%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,39 +1,24 @@
 class Solution {
-    private String s;
-    private List<String> wordDict;
-    private int[] memo;
-
-    private boolean dp(int i) {
-        if (i < 0) return true;
-
-        if (memo[i] != -1) {
-            return memo[i] == 1;
-        }
-
-        for (String word : wordDict) {
-            
-            if (i - word.length() + 1 < 0) {
-                continue;
-            }
-
-            if (
-                s.substring(i - word.length() + 1, i + 1).equals(word) &&
-                dp(i - word.length())
-            ) {
-                memo[i] = 1;
-                return true;
-            }
-        }
-
-        memo[i] = 0;
-        return false;
-    }
-
     public boolean wordBreak(String s, List<String> wordDict) {
-        this.s = s;
-        this.wordDict = wordDict;
-        this.memo = new int[s.length()];
-        Arrays.fill(this.memo, -1);
-        return dp(s.length() - 1);
+
+        boolean[] dp = new boolean[s.length()];
+
+        for(int i = 0; i < s.length(); i++) {
+            for(String word : wordDict) {
+                if(i < word.length() - 1) {
+                    continue;
+                }
+
+
+                if(i == word.length() - 1 || dp[i - word.length()]) {
+                    if(s.substring(i - word.length() + 1, i + 1).equals(word)) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return dp[s.length() - 1];
     }
 }
