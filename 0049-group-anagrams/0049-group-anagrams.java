653bf544f529278e9081f9b7b403d37bc97b0e2a
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-02-17 21:57:09 +0530

    Time: 18 ms (26.7%), Space: 50.6 MB (16.17%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,17 +1,25 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-     if(strs.length == 0) return new ArrayList<>();
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        int[] count = new int[26];
 
-     Map<String, List> ans = new HashMap<String, List>();
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) count[c - 'a']++;
 
-      for (String s : strs) {
-            char[] ca = s.toCharArray();
-            Arrays.sort(ca);
-            String key = String.valueOf(ca);
+            StringBuilder sb = new StringBuilder("");
+            
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+
+            String key = sb.toString();
             if (!ans.containsKey(key)) ans.put(key, new ArrayList());
             ans.get(key).add(s);
         }
 
-            return new ArrayList(ans.values());
+        return new ArrayList(ans.values());
     }
 }
