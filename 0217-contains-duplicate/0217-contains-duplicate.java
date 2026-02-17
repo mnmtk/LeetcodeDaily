b8929c89aa9d0312195504553bef022fb6fc4d00
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-02-17 10:16:24 +0530

    Time: 25 ms (13.62%), Space: 76.8 MB (84.66%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -3,7 +3,7 @@ class Solution {
 
         Arrays.sort(nums);
 
-        if(nums.length == 2) return nums[0] == nums[1];
+        // if(nums.length == 2) return nums[0] == nums[1];
 
         for(int i = 0; i < nums.length - 1; i++) {
             if(nums[i] == nums[i+1]) return true;
