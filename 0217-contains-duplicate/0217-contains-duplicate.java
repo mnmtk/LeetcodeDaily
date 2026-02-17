f487ef31dc386356f6cc299fbcb82966bf015ee9
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-02-17 10:17:44 +0530

    Time: 11 ms (92.19%), Space: 98.2 MB (7.64%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,15 +1,21 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
 
-        Arrays.sort(nums);
+        Set<Integer> set = new HashSet<>();
 
-        // if(nums.length == 2) return nums[0] == nums[1];
-
-        for(int i = 0; i < nums.length - 1; i++) {
-            if(nums[i] == nums[i+1]) return true;
+        for(int num : nums) {
+            if(set.add(num) == false) return true;
         }
-
-        return false;
         
+        return false;
     }
+
+
+            // Arrays.sort(nums);
+
+        // for(int i = 0; i < nums.length - 1; i++) {
+        //     if(nums[i] == nums[i+1]) return true;
+        // }
+
+        // return false;
 }
