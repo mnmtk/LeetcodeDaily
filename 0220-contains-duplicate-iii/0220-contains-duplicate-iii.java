dc8f73f42df2e7caedb75bf0fa3a5c61920dce72
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-02-17 10:42:02 +0530

    Time: 196 ms (32.47%), Space: 119.7 MB (57.39%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
@@ -1,30 +1,19 @@
 class Solution {
-    // Get the ID of the bucket from element value x and bucket width w
-    // Java's division `/` rounds towards zero, but we need floor division for correct bucketing.
-    private long getID(long x, long w) {
-        return Math.floorDiv(x, w);
-    }
-
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        if (t < 0) return false;
-        Map<Long, Long> buckets = new HashMap<>();
-        long w = (long) t + 1;
+        TreeSet<Integer> set = new TreeSet<>();
         for (int i = 0; i < nums.length; ++i) {
-            long bucket = getID(nums[i], w);
-            // check if current bucket is empty, each bucket may contain at most one element
-            if (buckets.containsKey(bucket)) return true;
-            // check the neighbor buckets for almost duplicate
-            if (
-                buckets.containsKey(bucket - 1) &&
-                Math.abs(nums[i] - buckets.get(bucket - 1)) < w
-            ) return true;
-            if (
-                buckets.containsKey(bucket + 1) &&
-                Math.abs(nums[i] - buckets.get(bucket + 1)) < w
-            ) return true;
-            // now bucket is empty and no almost duplicate in neighbor buckets
-            buckets.put(bucket, (long) nums[i]);
-            if (i >= k) buckets.remove(getID(nums[i - k], w));
+            // Find the successor of current element
+            Integer s = set.ceiling(nums[i]);
+            if (s != null && (long) s <= nums[i] + t) return true;
+
+            // Find the predecessor of current element
+            Integer g = set.floor(nums[i]);
+            if (g != null && nums[i] <= (long) g + t) return true;
+
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
         }
         return false;
     }
