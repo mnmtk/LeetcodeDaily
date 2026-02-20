762cde1ccf254bd7355447bdd30d3b54f7ecb324
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-02-21 00:48:02 +0530

    Time: 25 ms (25.37%), Space: 47.8 MB (73.33%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    public boolean feasible(int speed, int[] piles, int h) {
+
+        int sum = 0;
+
+        for(int pile : piles) {
+            sum += Math.ceil((double) pile / speed); 
+        //    sum += (pile + speed - 1) / speed; 
+        }
+
+      //  5/2 -> 3    6 / 2 
+
+        return sum <= h;
+    }
+
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int left = 1;
+        int right = 1;
+
+        for(int pile : piles) {
+            right = Math.max(right, pile);
+        }
+
+        while(left < right) {
+            int mid = left + (right - left)/2;
+            
+            if(feasible(mid, piles, h)) {
+                right = mid;
+            } else {
+                left = mid + 1; 
+            }
+        }
+
+        return left;
+    }
+}
