925240a66af455779e2862f24860ecd6404e419c
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-04-25 20:32:47 +0530

    Time: 28 ms (100%), Space: 46.6 MB (49.35%) - LeetHub

---
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -0,0 +1,26 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+};
+*/
+
+class Solution {
+    public Node lowestCommonAncestor(Node p, Node q) {
+        
+        Node a = p.parent;
+        Node b = q.parent;
+
+        while(a!=b){
+
+            a = a != null ? a.parent : q;
+            b = b != null ? b.parent : p;
+        }    
+
+        return a;
+        
+        }
+}
