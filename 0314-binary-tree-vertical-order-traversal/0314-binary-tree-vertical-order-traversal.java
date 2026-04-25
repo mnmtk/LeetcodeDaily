6f7517c8e4c06604e47c97cf7f65bfe51b112072
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-04-25 15:13:03 +0530

    Time: 4 ms (29.12%), Space: 44.8 MB (14.06%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.java
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.java
@@ -1,32 +1,46 @@
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 class Solution {
-    public List<List<Integer>> verticalOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if (root == null) return ans;
-
-        Map<Integer, List<Integer>> map = new TreeMap<>();
-        Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();
-        queue.offer(new Pair<>(root, 0));
-
-        while (!queue.isEmpty()) {
-            Pair<TreeNode, Integer> curr = queue.poll();
-            TreeNode node = curr.getKey();
-            int col = curr.getValue();
-
-            map.computeIfAbsent(col, k -> new ArrayList<>()).add(node.val);
-
-            if (node.left != null) {
-                queue.offer(new Pair<>(node.left, col - 1));
-            }
-            if (node.right != null) {
-                queue.offer(new Pair<>(node.right, col + 1));
-            }
-        }
-
-        for (List<Integer> colList : map.values()) {
-            ans.add(colList);
-        }
-
-        return ans;
+  public List<List<Integer>> verticalOrder(TreeNode root) {
+    List<List<Integer>> output = new ArrayList();
+    if (root == null) {
+      return output;
     }
+
+    Map<Integer, ArrayList> columnTable = new HashMap();
+    Queue<Pair<TreeNode, Integer>> queue = new ArrayDeque();
+    int column = 0;
+    queue.offer(new Pair(root, column));
+
+    while (!queue.isEmpty()) {
+      Pair<TreeNode, Integer> p = queue.poll();
+      root = p.getKey();
+      column = p.getValue();
+
+      if (root != null) {
+        if (!columnTable.containsKey(column)) {
+          columnTable.put(column, new ArrayList<Integer>());
+        }
+        columnTable.get(column).add(root.val);
+
+        queue.offer(new Pair(root.left, column - 1));
+        queue.offer(new Pair(root.right, column + 1));
+      }
+    }
+
+    List<Integer> sortedKeys = new ArrayList<Integer>(columnTable.keySet());
+    Collections.sort(sortedKeys);
+    for(int k : sortedKeys) {
+      output.add(columnTable.get(k));
+    }
+
+    return output;
+  }
 }
