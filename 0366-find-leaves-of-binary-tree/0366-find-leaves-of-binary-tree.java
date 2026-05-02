11bd38fe4d9566ea9436671195d0f4a5c458e246
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-05-02 14:16:11 +0530

    Time: 0 ms (100%), Space: 43.3 MB (72.73%) - LeetHub

--- 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.java
+++ 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.java
@@ -1,27 +1,47 @@
 class Solution {
-    private Map<Integer, List<Integer>> map = new HashMap<>();
-    private int maxHeight = -1;
-
-    private int getHeight(TreeNode root) {
-        if (root == null) return -1;
-        int left = getHeight(root.left);
-        int right = getHeight(root.right);
-
-        int height = Math.max(left, right) + 1;
-
-        map.computeIfAbsent(height, k -> new ArrayList<>()).add(root.val);
-
-        maxHeight = Math.max(maxHeight, height);
-        
-        return height;
-    }
-
     public List<List<Integer>> findLeaves(TreeNode root) {
-        getHeight(root);
-        List<List<Integer>> result = new ArrayList<>();
-        for (int i = 0; i <= maxHeight; i++) {
-            result.add(map.get(i));
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) {
+            return res;   
         }
-        return result;
+        while(!isLeaf(root)) { //multiple calls to same tree!
+            bfs(root, res);
+        }
+        res.add(Arrays.asList(root.val)); // Now the root is a leaf.
+        return res;
+    }
+    
+    /*
+        Collect leaf values and put it in a list.
+    */
+    private void bfs(TreeNode n, List<List<Integer>> res) {
+
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(n);
+
+        List<Integer> list = new ArrayList<>();
+
+        while(!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+
+            if(isLeaf(cur.left)) {
+                list.add(cur.left.val);
+                cur.left = null;
+            } else if(cur.left != null) {
+                queue.offer(cur.left);   
+            }
+            if(isLeaf(cur.right)) {
+                list.add(cur.right.val);
+                cur.right = null;
+            } else if(cur.right != null) {
+                queue.offer(cur.right);   
+            }
+        }
+
+        res.add(list);
+    }
+    
+    private boolean isLeaf(TreeNode n) {
+        return n != null && n.left == null && n.right == null;   
     }
 }
