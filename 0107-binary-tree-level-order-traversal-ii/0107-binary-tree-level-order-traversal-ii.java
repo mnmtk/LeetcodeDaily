1d83be4af35d6f5f9e08828acc199ca8de94d249
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-05-16 16:56:32 +0530

    Time: 1 ms (96.68%), Space: 44.7 MB (7.89%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -1,31 +1,26 @@
 class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        List<List<Integer>> levels = new ArrayList<List<Integer>>();
-        if (root == null) return levels;
-
-        ArrayDeque<TreeNode> nextLevel = new ArrayDeque() {
-            {
-                offer(root);
-            }
-        };
-        ArrayDeque<TreeNode> currLevel = new ArrayDeque();
-
-        while (!nextLevel.isEmpty()) {
-            currLevel = nextLevel.clone();
-            nextLevel.clear();
-            levels.add(new ArrayList<Integer>());
-
-            for (TreeNode node : currLevel) {
-                // append the current node value
-                levels.get(levels.size() - 1).add(node.val);
-
-                // process child nodes for the next level
-                if (node.left != null) nextLevel.offer(node.left);
-                if (node.right != null) nextLevel.offer(node.right);
-            }
+        LinkedList<List<Integer>> result = new LinkedList<>();
+        if (root == null) {
+            return result;
         }
-
-        Collections.reverse(levels);
-        return levels;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.addFirst(level);
+        }
+        return result;
     }
 }
