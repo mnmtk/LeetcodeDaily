ffe52793c376bbd8daece50b3b20d3bbf5bf3fa2
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-05-16 16:56:10 +0530

    Time: 2 ms (4.82%), Space: 44.9 MB (7.89%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -1,24 +1,31 @@
 class Solution {
-    List<List<Integer>> levels = new ArrayList<List<Integer>>();
-
-    public void helper(TreeNode node, int level) {
-        // start the current level
-        if (levels.size() == level) levels.add(new ArrayList<Integer>());
-
-        // append the current node value
-        levels.get(level).add(node.val);
-
-        // process child nodes for the next level
-        if (node.left != null) helper(node.left, level + 1);
-        if (node.right != null) helper(node.right, level + 1);
-    }
-
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> levels = new ArrayList<List<Integer>>();
         if (root == null) return levels;
-        helper(root, 0);
+
+        ArrayDeque<TreeNode> nextLevel = new ArrayDeque() {
+            {
+                offer(root);
+            }
+        };
+        ArrayDeque<TreeNode> currLevel = new ArrayDeque();
+
+        while (!nextLevel.isEmpty()) {
+            currLevel = nextLevel.clone();
+            nextLevel.clear();
+            levels.add(new ArrayList<Integer>());
+
+            for (TreeNode node : currLevel) {
+                // append the current node value
+                levels.get(levels.size() - 1).add(node.val);
+
+                // process child nodes for the next level
+                if (node.left != null) nextLevel.offer(node.left);
+                if (node.right != null) nextLevel.offer(node.right);
+            }
+        }
 
         Collections.reverse(levels);
-        
         return levels;
     }
 }
