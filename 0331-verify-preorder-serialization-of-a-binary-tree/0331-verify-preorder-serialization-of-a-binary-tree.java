be16f33632dcf3c51fb71e6c95e4bdd0b594d45b
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-05-17 23:40:51 +0530

    Time: 4 ms (94.6%), Space: 44.5 MB (89.2%) - LeetHub

---
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValidSerialization(String preorder) {
+        int slots = 1;
+
+        for(String node : preorder.split(",")) {
+            --slots;
+
+            if(slots < 0) return false;
+
+            if(!node.equals("#")) slots += 2;
+
+        }
+
+        return slots == 0;
+    }
+}
