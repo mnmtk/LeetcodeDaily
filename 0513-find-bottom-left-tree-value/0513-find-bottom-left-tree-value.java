482cd131314d977efe68f225f831cb580c194f08
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-05-19 00:42:00 +0530

    Time: 2 ms (74.2%), Space: 46.2 MB (77.76%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -1,23 +1,19 @@
 class Solution {
-    private int maxDepth;
-    private int bottomLeftValue;
-
     public int findBottomLeftValue(TreeNode root) {
-        maxDepth = -1;
-        bottomLeftValue = 0;
-        dfs(root, 0);
-        return bottomLeftValue;
-    }
+        Queue<TreeNode> queue = new LinkedList<>();
+        TreeNode current = root;
+        queue.add(current);
 
-    private void dfs(TreeNode current, int depth) {
-        if (current == null) {
-            return;
+        while (!queue.isEmpty()) {
+            current = queue.poll();
+            if (current.right != null) {
+                queue.add(current.right);
+            }
+            if (current.left != null) {
+                queue.add(current.left);
+            }
         }
-        if (depth > maxDepth) {  // If true, we discovered a new level // only the first time we store
-            maxDepth = depth;
-            bottomLeftValue = current.val;
-        }
-        dfs(current.left, depth + 1);
-        dfs(current.right, depth + 1);
+        return current.val;
     }
 }
+
