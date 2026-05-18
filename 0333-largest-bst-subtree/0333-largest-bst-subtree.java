6cca62768f96260d5bcbb11584be4d5ab4d28dae
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-05-18 22:26:56 +0530

    Time: 0 ms (100%), Space: 44.5 MB (98.55%) - LeetHub

--- 0333-largest-bst-subtree/0333-largest-bst-subtree.java
+++ 0333-largest-bst-subtree/0333-largest-bst-subtree.java
@@ -1,71 +1,56 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-
-    public boolean isValidBST(TreeNode root) {
-        if (root == null)
-            return true;
-
-        int leftMax = findMax(root.left);
-        if (leftMax >= root.val)
-            return false;
-
-        int rightMin = findMin(root.right);
-        if (rightMin <= root.val)
-            return false;
-
-        if (isValidBST(root.left) && isValidBST(root.right)) {
+    // Track previous node while doing inorder traversal.
+    private TreeNode previous;
+    
+    // Function to check if given tree is a valid Binary Search Tree or not.
+    private boolean isValidBST(TreeNode root) {
+        // An empty tree is a valid Binary Search Tree.
+        if (root == null) {
             return true;
         }
 
-        return false;
-
-    }
-
-    private int findMax(TreeNode root) {
-        if (root == null) {
-            return Integer.MIN_VALUE;
+        // If left subtree is not a valid BST return false.
+        if(!isValidBST(root.left)) {
+            return false;
         }
-
-        return Math.max(Math.max(root.val, findMax(root.left)), findMax(root.right));
-    }
-
-    private int findMin(TreeNode root) {
-        if (root == null) {
-            return Integer.MAX_VALUE;
+        
+        // If current node's value is not greater than the previous 
+        // node's value in the in-order traversal return false.
+        if (previous != null && previous.val >= root.val) {
+            return false;
         }
-
-        return Math.min(Math.min(root.val, findMin(root.left)), findMin(root.right));
+        
+        // Update previous node to current node.
+        previous = root;
+        
+        // If right subtree is not a valid BST return false.
+        return isValidBST(root.right);
     }
 
     private int countNodes(TreeNode root) {
-        if(root == null) return 0;
-
+        if (root == null) {
+            return 0;
+        }
+        
+        // Add nodes in left and right subtree.
+        // Add 1 and return total size.
         return 1 + countNodes(root.left) + countNodes(root.right);
     }
-
+    
     public int largestBSTSubtree(TreeNode root) {
         if (root == null) {
             return 0;
         }
-
+        
+        // Set previous node to NULL initially.
+        previous = null;
+        
+        // If current subtree is a validBST, its children will have smaller size BST.
         if (isValidBST(root)) {
             return countNodes(root);
         }
-
+        
+        // Find BST in left and right subtrees of current nodes.
         return Math.max(largestBSTSubtree(root.left), largestBSTSubtree(root.right));
     }
 }
