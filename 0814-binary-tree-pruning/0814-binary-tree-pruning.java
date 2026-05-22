c7ee0556942401e0c276384ea8ce044b10e5ac11
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-05-22 08:47:41 +0530

    Time: 0 ms (100%), Space: 43.2 MB (24.93%) - LeetHub

--- 0814-binary-tree-pruning/0814-binary-tree-pruning.java
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.java
@@ -1,19 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public TreeNode pruneTree(TreeNode root) {
-        // Base case: if the node is null, nothing to prune
-        if (root == null) return null; 
 
-        // 1. Recursively prune left and right subtrees and update pointers
+        if(root == null) return null; 
+        
         root.left = pruneTree(root.left);
         root.right = pruneTree(root.right);
 
-        // 2. Decide if the current node itself needs to be pruned.
-        // It should be pruned ONLY if it's now a leaf and its value is 0.
-        if (root.left == null && root.right == null && root.val == 0) {
-            return null; // Prune this node
-        }
+        if(root.left == null && root.right == null && root.val == 0) return null;
 
-        // Otherwise, keep this node
         return root;
     }
 }
