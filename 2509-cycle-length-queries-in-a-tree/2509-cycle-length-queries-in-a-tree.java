efac919dddbbab498ccda2c03fbbf9645279c6ce
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-05-26 20:26:20 +0530

    Time: 20 ms (58.62%), Space: 101.8 MB (62.07%) - LeetHub

---
+++ 2509-cycle-length-queries-in-a-tree/2509-cycle-length-queries-in-a-tree.java
@@ -0,0 +1,19 @@
+class Solution {
+        public int[] cycleLengthQueries(int n, int[][] queries) {
+        int m = queries.length, res[] = new int[m];
+
+        for (int i = 0; i < m; ++i) {
+            res[i]++;
+            int x = queries[i][0], y = queries[i][1];
+            while (x != y) {
+                if (x > y)
+                    x /= 2;
+                else
+                    y /= 2;
+                    
+                res[i]++;
+            }
+        }
+        return res;
+    }
+}
