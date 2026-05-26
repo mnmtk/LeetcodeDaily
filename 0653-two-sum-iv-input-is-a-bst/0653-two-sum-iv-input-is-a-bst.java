e4e5c36c83eff72ae6e8308eff11f6521d434173
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-05-26 20:50:15 +0530

    Time: 2 ms (97.81%), Space: 47.2 MB (70.25%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -1,21 +1,24 @@
 public class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        Set < Integer > set = new HashSet();
-        Queue < TreeNode > queue = new LinkedList();
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            if (queue.peek() != null) {
-                TreeNode node = queue.remove();
-                if (set.contains(k - node.val))
-                    return true;
-                set.add(node.val);
-                queue.add(node.right);
-                queue.add(node.left);
-            } else
-                queue.remove();
+        List < Integer > list = new ArrayList();
+        inorder(root, list);
+        int l = 0, r = list.size() - 1;
+        while (l < r) {
+            int sum = list.get(l) + list.get(r);
+            if (sum == k)
+                return true;
+            if (sum < k)
+                l++;
+            else
+                r--;
         }
-        
         return false;
+    }
+    public void inorder(TreeNode root, List < Integer > list) {
+        if (root == null)
+            return;
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
     }
 }
