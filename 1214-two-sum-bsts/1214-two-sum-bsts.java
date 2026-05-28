e9d8b12380a38bec2947df9820ec5234d5628782
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-05-28 17:22:01 +0530

    Time: 2 ms (85.82%), Space: 47.6 MB (59.57%) - LeetHub

--- 1214-two-sum-bsts/1214-two-sum-bsts.java
+++ 1214-two-sum-bsts/1214-two-sum-bsts.java
@@ -13,33 +13,113 @@
  *     }
  * }
  */
-class Solution {
-    private void dfs(TreeNode currNode, List<Integer> nodeList) {
-        if (currNode == null) {
-            return;
-        }
-        dfs(currNode.left, nodeList);
-        nodeList.add(currNode.val);
-        dfs(currNode.right, nodeList);
-    }
-    
-    public boolean twoSumBSTs(TreeNode root1, TreeNode root2, int target) {
-        List<Integer> nodeList1 = new ArrayList<>();
-        List<Integer> nodeList2 = new ArrayList<>();
-        dfs(root1, nodeList1);
-        dfs(root2, nodeList2);
 
-        int pointer1 = 0, pointer2 = nodeList2.size() - 1;
-        while (pointer1 < nodeList1.size() && pointer2 >= 0) {
-            if (nodeList1.get(pointer1) + nodeList2.get(pointer2) == target) {
-                return true;
-            } else if (nodeList1.get(pointer1) + nodeList2.get(pointer2) < target) {
-                pointer1++;
+class MorrisIterator implements Iterator<Integer> {
+    private TreeNode current;
+    private TreeNode pre;
+
+    public MorrisIterator(TreeNode root) {
+        current = root;
+        pre = null;
+    }
+
+    public boolean hasNext() {
+        return current != null;
+    }
+
+    public Integer next() {
+        Integer val = null;
+        
+        while (current != null) {
+            if (current.left == null) {
+                val = current.val;
+                current = current.right;
+                break;
             } else {
-                pointer2--;
+                pre = current.left;
+                while (pre.right != null && pre.right != current) {
+                    pre = pre.right;
+                }
+                if (pre.right == null) {
+                    pre.right = current;
+                    current = current.left;
+                } else {
+                    pre.right = null;
+                    val = current.val;
+                    current = current.right;
+                    break;
+                }
+            }
+        }
+        return val;
+    }
+}
+
+class ReversedMorrisIterator implements Iterator<Integer> {
+    private TreeNode current;
+    private TreeNode pre;
+
+    public ReversedMorrisIterator(TreeNode root) {
+        current = root;
+        pre = null;
+    }
+
+    public boolean hasNext() {
+        return current != null;
+    }
+
+    public Integer next() {
+        Integer val = null;
+        while (current != null) {
+            if (current.right == null) {
+                val = current.val;
+                current = current.left;
+                break;
+            } else {
+                pre = current.right;
+                while (pre.left != null && pre.left != current) {
+                    pre = pre.left;
+                }
+                if (pre.left == null) {
+                    pre.left = current;
+                    current = current.right;
+                } else {
+                    pre.left = null;
+                    val = current.val;
+                    current = current.left;
+                    break;
+                }
+            }
+        }
+        return val;
+    }
+}
+
+class Solution {
+    public boolean twoSumBSTs(TreeNode root1, TreeNode root2, int target) {
+        MorrisIterator iterator1 = new MorrisIterator(root1);
+        ReversedMorrisIterator iterator2 = new ReversedMorrisIterator(root2);
+        
+        int value1 = iterator1.next(), value2 = iterator2.next();
+        
+        while (value1 != Integer.MIN_VALUE && value2 != Integer.MIN_VALUE) {
+            if (value1 + value2 == target) {
+                return true;
+            } else if (value1 + value2 < target) {
+                if (iterator1.hasNext()) {
+                    value1 = iterator1.next();
+                } else {
+                    value1 = Integer.MIN_VALUE;
+                }
+            } else {
+                if (iterator2.hasNext()) {
+                    value2 = iterator2.next();
+                } else {
+                    value2 = Integer.MIN_VALUE;
+                }
             }
         }
         
         return false;
-    }   
+    }
 }
