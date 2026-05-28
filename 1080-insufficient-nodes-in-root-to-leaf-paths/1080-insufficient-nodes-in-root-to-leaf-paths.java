9e0519e28f39d4512c6ca63c93e4298c96a4661d
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-05-28 15:19:03 +0530

    Time: 0 ms (100%), Space: 47.1 MB (71.69%) - LeetHub

--- 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.java
+++ 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.java
@@ -14,15 +14,20 @@
  * }
  */
 class Solution {
-        public TreeNode sufficientSubset(TreeNode root, int limit) {
+    public TreeNode sufficientSubset(TreeNode root, int limit) {
         if (root == null)
             return null;
 
         if (root.left == null && root.right == null)
             return root.val < limit ? null : root;
-            
+
         root.left = sufficientSubset(root.left, limit - root.val);
         root.right = sufficientSubset(root.right, limit - root.val);
-        return root.left == root.right ? null : root;
+
+        if (root.left == null && root.right == null) {
+            return null;
+        }
+        
+        return root;
     }
 }
