fede7274727392d4d51889ec6d4c6f8785f6e358
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-06-03 14:31:47 +0530

    Time: 2 ms (97.22%), Space: 42.9 MB (61.11%) - LeetHub

---
+++ 1628-design-an-expression-tree-with-evaluate-function/1628-design-an-expression-tree-with-evaluate-function.java
@@ -0,0 +1,55 @@
+abstract class Node {
+    public abstract int evaluate();
+    // define your fields here
+};
+
+class NumNode extends Node {
+    private int val;
+    public NumNode(int val) {
+        this.val = val;
+    }
+    public int evaluate() {
+        return this.val;
+    }
+};
+
+class OpNode extends Node {
+    private char op;
+    private Node left;
+    private Node right;
+    public OpNode(char op, Node left, Node right) {
+        this.op = op;
+        this.left = left;
+        this.right = right;
+    }
+    public int evaluate() {
+        switch(this.op) {
+            case '+':
+                return this.left.evaluate() + this.right.evaluate();
+            case '-':
+                return this.left.evaluate() - this.right.evaluate();
+            case '*':
+                return this.left.evaluate() * this.right.evaluate();
+            case '/':
+                return this.left.evaluate() / this.right.evaluate();
+            default:
+                return 0;
+        }
+    }
+};
+
+class TreeBuilder {
+    Node buildTree(String[] postfix) {
+        Stack<Node> st = new Stack<Node>();
+        for (String s : postfix){
+            if(Character.isDigit(s.charAt(0))){
+                st.add(new NumNode(Integer.parseInt(s)));
+            }else{
+                Node right = st.pop();
+                Node left = st.pop();
+                st.add(new OpNode(s.charAt(0), left, right));
+            }
+        }
+        return st.peek();
+    }
+};
