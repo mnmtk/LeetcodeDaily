354364c9f9d4982c5b64b96657d9c727593e56b0
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-06-06 03:05:35 +0530

    Time: 108 ms (24.84%), Space: 125.1 MB (31.21%) - LeetHub

---
+++ 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.java
@@ -0,0 +1,26 @@
+class Solution {
+    long dfs(List<List<Integer>> al, long[] s, int i) {
+        long prod = 1, sum = 1;
+        for (int j : al.get(i)) {
+            long cnt = dfs(al, s, j);
+            prod *= cnt;
+            sum += cnt;
+        }
+        s[i] = prod * (Math.max(1, al.size() - sum));
+        return sum;
+    }
+
+    public int countHighestScoreNodes(int[] p) {
+        List<List<Integer>> al = new ArrayList<>();
+        
+        for (int i = 0; i < p.length; ++i)
+            al.add(new ArrayList<Integer>());
+
+        long[] s = new long[p.length];
+        for (int i = 1; i < p.length; ++i)
+            al.get(p[i]).add(i);
+        dfs(al, s, 0);
+        long max_val = Arrays.stream(s).max().getAsLong();
+        return (int) Arrays.stream(s).filter(v -> v == max_val).count();
+    }
+}
