5570711e257e052087abfb5026f182cf4c6ca3d4
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-06-16 20:56:57 +0530

    Time: 558 ms (8.33%), Space: 133.4 MB (75%) - LeetHub

--- 1902-depth-of-bst-given-insertion-order/1902-depth-of-bst-given-insertion-order.java
+++ 1902-depth-of-bst-given-insertion-order/1902-depth-of-bst-given-insertion-order.java
@@ -1,28 +1,12 @@
 class Solution {
     public int maxDepthBST(int[] order) {
-        int n = order.length, maxDepth = 1;
+    TreeMap<Integer, Integer> m = new TreeMap<>();
+
+    for (int i : order) {
+        Map.Entry<Integer, Integer> l = m.floorEntry(i), r = m.ceilingEntry(i);
         
-        // our map will associate a value in our BST to its depth
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        map.put(order[0], 1);
-        
-        for (int i = 1; i < n; i++) {
-            int val = order[i];
-            
-            // getting left and right nextdoor entries! (and their corresponding depths)
-            Map.Entry<Integer, Integer> left = map.floorEntry(val);
-            Map.Entry<Integer, Integer> right = map.ceilingEntry(val);
-            int leftDepth = 0, rightDepth = 0;
-            
-            if (left != null) leftDepth = left.getValue();
-            if (right != null) rightDepth = right.getValue();
-            
-            // the depth of our value is the highest of the two, plus one (since it's a child)
-            int depth = Math.max(leftDepth, rightDepth) + 1;
-            maxDepth = Math.max(maxDepth, depth);
-            
-            map.put(val, depth);
-        }
-        return maxDepth;
+        m.put(i, 1 + Math.max(l == null ? 0 : l.getValue(), r == null ? 0 : r.getValue()));
     }
+    return m.entrySet().stream().max((a, b) -> a.getValue().compareTo(b.getValue())).get().getValue();
+}
 }
