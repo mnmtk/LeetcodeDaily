6f8fd074b71d655fcf668c5ee2871b508544a27b
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-06-27 18:40:17 +0530

    Time: 0 ms (100%), Space: 43.4 MB (14.62%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -15,44 +15,21 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        
-        ArrayList<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> res = new ArrayList<>();
+
         TreeNode curr = root;
 
-        while (curr != null) {
-            if (curr.left == null) {
-              
-                // If no left child, visit this node 
-                // and go right
-                res.add(curr.val);
-                curr = curr.right;
-            } 
-            else {
-              
-                // Find the inorder predecessor of curr
-                TreeNode prev = curr.left;
-                while (prev.right != null && 
-                                   prev.right != curr) {
-                    prev = prev.right;
-                }
-
-                // Make curr the right child of its 
-                // inorder predecessor
-                if (prev.right == null) {
-                    prev.right = curr;
-                    curr = curr.left;
-                } 
-                else {
-                  
-                    // Revert the changes made in 
-                    // the tree structure
-                    prev.right = null;
-                    res.add(curr.val);
-                    curr = curr.right;
-                }
+        while(curr != null || !stack.isEmpty()) {
+            while(curr != null) {
+                stack.push(curr);
+                curr = curr.left;
             }
+
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
         }
         return res;
-
     }
 }
