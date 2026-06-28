9f79e879a86aaf169c5205616dc954d62bb51a91
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-06-28 19:03:59 +0530

    Time: 0 ms (100%), Space: 44.4 MB (64.3%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -14,10 +14,14 @@
  * }
  */
 class Solution {
+    
     public int maxDepth(TreeNode root) {
+        
         if(root == null) return 0;
 
-        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
-        
+        int left = maxDepth(root.left) + 1;
+        int right = maxDepth(root.right) + 1;
+
+        return Math.max(left, right);
     }
 }
