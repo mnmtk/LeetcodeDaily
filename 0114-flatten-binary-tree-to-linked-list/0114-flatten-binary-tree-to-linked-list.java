e5c8c6ec9c14d0fe2c83d7e6e8bfe00fc3c40bd2
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-02 23:53:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -4,43 +4,36 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    private TreeNode flattenTree(TreeNode node) {
-        // Handle the null scenario
-        if (node == null) {
-            return null;
+
+    public Tree flattenTree(TreeNode root) {
+        if(root == null) return null;
+
+        if(root.left == null && root.right == null) return root;
+
+
+        TreeNode leftTail = flattenTree(root.left);
+        TreeNode rightTail = flattenTree(root.right);
+
+
+        if(leftTrail != null) {
+            leftTail.right = root.right;
+            root.right = root.left;
+            root.left = null;
         }
 
-        // For a leaf node, we simply return the
-        // node as is.
-        if (node.left == null && node.right == null) {
-            return node;
-        }
-
-        //Recursively flatten the left subtree
-        TreeNode leftTail = this.flattenTree(node.left);
-
-        // Recursively flatten the right subtree
-        TreeNode rightTail = this.flattenTree(node.right);
-
-        // If there was a left subtree, we shuffle the connections
-        // around so that there is nothing on the left side
-        // anymore.
-        if (leftTail != null) {
-            leftTail.right = node.right;
-            node.right = node.left;
-            node.left = null;
-        }
-
-        // We need to return the "rightmost" node after we are
-        // done wiring the new connections.
-        return rightTail == null ? leftTail : rightTail;
-    }
-
+        return rightTail == null ? leftTail : rightTail; //upr ki dsna
+     }
     public void flatten(TreeNode root) {
-        this.flattenTree(root);
+        return flattenTree(root);
     }
 }
