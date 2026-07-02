d1bd120ba20cd8703760affdcb69b807b23d620e
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-02 16:24:51 +0530

    Time: 3 ms (30.63%), Space: 45.6 MB (76.01%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -19,6 +19,7 @@ class Solution {
     }
 
     public TreeNode buildTreeHelper(int[] preorder, int[] inorder, int pStart, int iStart, int iEnd) {
+        
         if(iStart > iEnd || pStart >= preorder.length) {
             return null;
         }
@@ -26,12 +27,15 @@ class Solution {
         int rootVal = preorder[pStart];
         TreeNode root = new TreeNode(rootVal);
         int inorderIndex = iStart;
+
         for(int i = iStart; i<= iEnd;i++) {
             if(inorder[i] == rootVal) {
                 inorderIndex = i;
             };
         }
+
         int treeSize = inorderIndex - iStart;
+
         TreeNode left = buildTreeHelper(preorder, inorder, pStart + 1, iStart, inorderIndex - 1);
         TreeNode right = buildTreeHelper(preorder, inorder, pStart + treeSize + 1, inorderIndex + 1, iEnd);
 
