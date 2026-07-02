af8fc0e24cad63dea59ce9013c1dd4894f770f7e
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-02 16:12:12 +0530

    Time: 1 ms (35.9%), Space: 46.6 MB (68.62%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -15,20 +15,23 @@
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
+        
         Stack<TreeNode> stack = new Stack<>();
 
-        while(root!= null || !stack.isEmpty()) {
-        while(root != null) {
-            stack.push(root);
-            root = root.left;
-        }
+        while(!stack.isEmpty() || root != null) {
+            
+            while(root != null) {
+                stack.push(root);
+                root = root.left;
+            }
 
-        root = stack.pop();
-        k--;
-        if(k == 0) return root.val;
+            root = stack.pop();
+            k--;
 
+            if(k == 0) return root.val;
 
-        root = root.right;
+            root = root.right;
+
         }
 
         return -1;
