23ade44e0c75a982b9cfa6cfd37f80802e0581b8
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-11 15:42:33 +0530

    Time: 17 ms (95.44%), Space: 50.4 MB (8.28%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.java
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.java
@@ -1,48 +1,47 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode(int x) { val = x; }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class BSTIterator {
-    ArrayList<Integer> nodesSorted;
-    int index;
+
+    List<Integer> sortedList = new ArrayList<>();
+    int pointer = -1;
 
     public BSTIterator(TreeNode root) {
-        // Array containing all the nodes in the sorted order
-        this.nodesSorted = new ArrayList<Integer>();
-
-        // Pointer to the next smallest element in the BST
-        this.index = -1;
-
-        // Call to flatten the input binary search tree
-        this._inorder(root);
+        inorder(root);
     }
 
-    private void _inorder(TreeNode root) {
-        if (root == null) {
+    private void inorder(TreeNode root) {
+        if (root == null)
             return;
-        }
-
-        this._inorder(root.left);
-        this.nodesSorted.add(root.val);
-        this._inorder(root.right);
+        inorder(root.left);
+        sortedList.add(root.val);
+        inorder(root.right);
     }
 
-    /**
-     * @return the next smallest number
-     */
     public int next() {
-        return this.nodesSorted.get(++this.index);
+        return sortedList.get(++pointer);
     }
 
-    /**
-     * @return whether we have a next smallest number
-     */
     public boolean hasNext() {
-        return this.index + 1 < this.nodesSorted.size();
+        return pointer + 1 < sortedList.size();
     }
 }
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
