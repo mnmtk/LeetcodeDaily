cd37b10b8bc88c4fe7abc8abe3d68bdfd46afa62
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-13 14:09:49 +0530

    Time: 2 ms (99.62%), Space: 53.4 MB (90.83%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,22 +1,23 @@
 class Solution {
-    public int findJudge(int n, int[][] trust) {
-        int[] trusting = new int[n + 1];
-        int[] trusted = new int[n + 1];
-
-        for (int i = 0; i < trust.length; i++) {
-            trusting[trust[i][0]]++;
-            trusted[trust[i][1]]++;
-        }
-
-        int ans = -1;
-
-        for (int i = 1; i <= n; i++) {
-            if (trusting[i] == 0 && trusted[i] == n - 1) {
-                ans = i;
-                break; 
-            }
-        }
-
-        return ans;
+    public int findJudge(int N, int[][] trust) {
+    
+    if (trust.length < N - 1) {
+        return -1;
     }
+    
+    int[] indegrees = new int[N + 1];
+    int[] outdegrees = new int[N + 1];
+
+    for (int[] relation : trust) {
+        outdegrees[relation[0]]++;
+        indegrees[relation[1]]++; 
+    }
+
+    for (int i = 1; i <= N; i++) {
+        if (indegrees[i] == N - 1 && outdegrees[i] == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
 }
