17ad4a284003bdea82c87d5713b1e4fd6d876657
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-17 01:31:57 +0530

    Time: 29 ms (15.18%), Space: 46.9 MB (98.61%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -3,6 +3,7 @@ class Solution {
     private static final int[][] directions = 
         new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
     
+    
     public int shortestPathBinaryMatrix(int[][] grid) {
         
         // Firstly, we need to check that the start and target cells are open.
@@ -12,28 +13,33 @@ class Solution {
         
         // Set up the BFS.
         Queue<int[]> queue = new ArrayDeque<>();
-        grid[0][0] = 1;
-        queue.add(new int[]{0, 0});
+        queue.add(new int[]{0, 0, 1}); // Put distance on the queue
+        boolean[][] visited = new boolean[grid.length][grid[0].length]; // Used as visited set.
+        visited[0][0] = true;
         
         // Carry out the BFS
         while (!queue.isEmpty()) {
             int[] cell = queue.remove();
             int row = cell[0];
             int col = cell[1];
-            int distance = grid[row][col];
+            int distance = cell[2];
+            // Check if this is the target cell.
             if (row == grid.length - 1 && col == grid[0].length - 1) {
                 return distance;
             }
             for (int[] neighbour : getNeighbours(row, col, grid)) {
                 int neighbourRow = neighbour[0];
                 int neighbourCol = neighbour[1];
-                queue.add(new int[]{neighbourRow, neighbourCol});
-                grid[neighbourRow][neighbourCol] = distance + 1;
+                if (visited[neighbourRow][neighbourCol]) {
+                    continue;
+                }
+                visited[neighbourRow][neighbourCol] = true;
+                queue.add(new int[]{neighbourRow, neighbourCol, distance + 1});
             }
         }
         
         // The target was unreachable.
-        return -1;
+        return -1;  
     }
     
     private List<int[]> getNeighbours(int row, int col, int[][] grid) {
