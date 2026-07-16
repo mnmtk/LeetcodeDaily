113e2db9af73a1b8a58502c52445910078681f48
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-16 22:08:15 +0530

    Time: 26 ms (17.22%), Space: 47.5 MB (70.3%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,62 +1,100 @@
 class Solution {
     
+    // Candidate represents a possible option for travelling to the cell
+    // at (row, col).
+    class Candidate {
+        
+        public int row;
+        public int col;
+        public int distanceSoFar;
+        public int totalEstimate;
+        
+        public Candidate(int row, int col, int distanceSoFar, int totalEstimate) {
+            this.row = row;
+            this.col = col;
+            this.distanceSoFar = distanceSoFar;
+            this.totalEstimate = totalEstimate;
+        }
+    }
+    
+    
     private static final int[][] directions = 
         new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
     
     
     public int shortestPathBinaryMatrix(int[][] grid) {
         
+        // Firstly, we need to check that the start and target cells are open.
         if (grid[0][0] != 0 || grid[grid.length - 1][grid[0].length - 1] != 0) {
             return -1;
         }
         
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[]{0, 0, 1}); 
-        boolean[][] visited = new boolean[grid.length][grid[0].length]; 
-        visited[0][0] = true;
+        // Set up the A* search.
+        Queue<Candidate> pq = new PriorityQueue<>((a,b) -> a.totalEstimate - b.totalEstimate);
+        pq.add(new Candidate(0, 0, 1, estimate(0, 0, grid)));
         
-        while (!queue.isEmpty()) {
-            int[] cell = queue.remove();
-
-            int row = cell[0];
-            int col = cell[1];
-            int distance = cell[2];
-
-            if (row == grid.length - 1 && col == grid[0].length - 1) {
-                return distance;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        
+        // Carry out the A* search.
+        while (!pq.isEmpty()) {
+            
+            Candidate best = pq.remove();
+            
+            // Is this for the target cell?
+            if (best.row == grid.length - 1 && best.col == grid[0].length - 1) {
+                return best.distanceSoFar;
             }
-
-            for (int[] neighbour : getNeighbours(row, col, grid)) {
+            
+            // Have we already found the best path to this cell?
+            if (visited[best.row][best.col]) {
+                continue;
+            }
+            
+            visited[best.row][best.col] = true;
+            
+            for (int[] neighbour : getNeighbours(best.row, best.col, grid)) {
                 int neighbourRow = neighbour[0];
                 int neighbourCol = neighbour[1];
-
+                
+                // This check isn't necessary for correctness, but it greatly
+                // improves performance.
                 if (visited[neighbourRow][neighbourCol]) {
                     continue;
                 }
-                visited[neighbourRow][neighbourCol] = true;
                 
-                queue.add(new int[]{neighbourRow, neighbourCol, distance + 1});
-            }
+                // Otherwise, we need to create a Candidate object for the option
+                // of going to neighbor through the current cell.
+                int newDistance = best.distanceSoFar + 1;
+                int totalEstimate = newDistance + estimate(neighbourRow, neighbourCol, grid);
+                Candidate candidate = new Candidate(neighbourRow, neighbourCol, newDistance, totalEstimate);
 
+                pq.add(candidate);
+            }   
         }
-        
+        // The target was unreachable.
         return -1;  
     }
     
     private List<int[]> getNeighbours(int row, int col, int[][] grid) {
         List<int[]> neighbours = new ArrayList<>();
         for (int i = 0; i < directions.length; i++) {
-
             int newRow = row + directions[i][0];
             int newCol = col + directions[i][1];
-
-            if (newRow < 0 || newCol < 0 || newRow >= grid.length || newCol >= grid[0].length || grid[newRow][newCol] != 0) {
+            if (newRow < 0 || newCol < 0 || newRow >= grid.length 
+                    || newCol >= grid[0].length
+                    || grid[newRow][newCol] != 0) {
                 continue;    
             }
-
             neighbours.add(new int[]{newRow, newCol});
         }
         return neighbours; 
+    }
+    
+    // Get the best case estimate of how much further it is to the bottom-right cell.
+    private int estimate(int row, int col, int[][] grid) {
+        int remainingRows = grid.length - row - 1;
+        int remainingCols = grid[0].length - col - 1;
+        return Math.max(remainingRows, remainingCols);
     }
     
 }
