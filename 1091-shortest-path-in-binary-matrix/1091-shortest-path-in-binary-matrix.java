b3f37d8ef20f81f2df99fa4d61b10f479d940fc6
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-17 01:23:12 +0530

    Time: 21 ms (20.53%), Space: 47.6 MB (70.45%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,26 +1,7 @@
 class Solution {
     
-    // Candidate represents a possible option for travelling to the cell
-    // at (row, col).
-    class Candidate {
-        
-        public int row;
-        public int col;
-        public int distanceSoFar;
-        public int totalEstimate;
-        
-        public Candidate(int row, int col, int distanceSoFar, int totalEstimate) {
-            this.row = row;
-            this.col = col;
-            this.distanceSoFar = distanceSoFar;
-            this.totalEstimate = totalEstimate;
-        }
-    }
-    
-    
     private static final int[][] directions = 
         new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
-    
     
     public int shortestPathBinaryMatrix(int[][] grid) {
         
@@ -29,50 +10,30 @@ class Solution {
             return -1;
         }
         
-        // Set up the A* search.
-        Queue<Candidate> pq = new PriorityQueue<>((a,b) -> a.totalEstimate - b.totalEstimate);
-        pq.add(new Candidate(0, 0, 1, estimate(0, 0, grid)));
+        // Set up the BFS.
+        Queue<int[]> queue = new ArrayDeque<>();
+        grid[0][0] = 1;
+        queue.add(new int[]{0, 0});
         
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
-        
-        // Carry out the A* search.
-        while (!pq.isEmpty()) {
-            
-            Candidate best = pq.remove();
-            
-            // Is this for the target cell?
-            if (best.row == grid.length - 1 && best.col == grid[0].length - 1) {
-                return best.distanceSoFar;
+        // Carry out the BFS
+        while (!queue.isEmpty()) {
+            int[] cell = queue.remove();
+            int row = cell[0];
+            int col = cell[1];
+            int distance = grid[row][col];
+            if (row == grid.length - 1 && col == grid[0].length - 1) {
+                return distance;
             }
-            
-            // Have we already found the best path to this cell?
-            if (visited[best.row][best.col]) {
-                continue;
-            }
-            
-            visited[best.row][best.col] = true;
-            
-            for (int[] neighbour : getNeighbours(best.row, best.col, grid)) {
+            for (int[] neighbour : getNeighbours(row, col, grid)) {
                 int neighbourRow = neighbour[0];
                 int neighbourCol = neighbour[1];
-                
-                // This check isn't necessary for correctness, but it greatly
-                // improves performance.
-                if (visited[neighbourRow][neighbourCol]) {
-                    continue;
-                }
-                
-                // Otherwise, we need to create a Candidate object for the option
-                // of going to neighbor through the current cell.
-                int newDistance = best.distanceSoFar + 1;
-                int totalEstimate = newDistance + estimate(neighbourRow, neighbourCol, grid);
-                Candidate candidate = new Candidate(neighbourRow, neighbourCol, newDistance, totalEstimate);
-
-                pq.add(candidate);
-            }   
+                queue.add(new int[]{neighbourRow, neighbourCol});
+                grid[neighbourRow][neighbourCol] = distance + 1;
+            }
         }
+        
         // The target was unreachable.
-        return -1;  
+        return -1;
     }
     
     private List<int[]> getNeighbours(int row, int col, int[][] grid) {
@@ -88,13 +49,6 @@ class Solution {
             neighbours.add(new int[]{newRow, newCol});
         }
         return neighbours; 
-    }
-    
-    // Get the best case estimate of how much further it is to the bottom-right cell.
-    private int estimate(int row, int col, int[][] grid) {
-        int remainingRows = grid.length - row - 1;
-        int remainingCols = grid[0].length - col - 1;
-        return Math.max(remainingRows, remainingCols);
     }
     
 }
