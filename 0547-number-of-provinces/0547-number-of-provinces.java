c28020fafe4a7a64d26e70997a6d1680c61f6562
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-21 14:34:44 +0530

    Time: 1 ms (87.25%), Space: 47.4 MB (31.63%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,10 +1,18 @@
 class Solution {
 
-    public void dfs(int node, int[][] isConnected, boolean[] visit) {
+    public void bfs(int node, int[][] isConnected, boolean[] visit) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(node);
         visit[node] = true;
-        for (int i = 0; i < isConnected.length; i++) {
-            if (isConnected[node][i] == 1 && !visit[i]) {
-                dfs(i, isConnected, visit);
+
+        while (!q.isEmpty()) {
+            node = q.poll();
+
+            for (int i = 0; i < isConnected.length; i++) {
+                if (isConnected[node][i] == 1 && !visit[i]) {
+                    q.offer(i);
+                    visit[i] = true;
+                }
             }
         }
     }
@@ -17,7 +25,7 @@ class Solution {
         for (int i = 0; i < n; i++) {
             if (!visit[i]) {
                 numberOfComponents++;
-                dfs(i, isConnected, visit);
+                bfs(i, isConnected, visit);
             }
         }
 
