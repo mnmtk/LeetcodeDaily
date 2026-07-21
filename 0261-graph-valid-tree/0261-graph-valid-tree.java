6c0da1515bef13a8e358fed4ad1c9f93779119bb
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-21 17:03:29 +0530

    Time: 10 ms (6.14%), Space: 46.5 MB (37.69%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.java
+++ 0261-graph-valid-tree/0261-graph-valid-tree.java
@@ -1,37 +1,69 @@
 class Solution {
+
+
+    public class DisjointSetUnion {
+        int[] parents;
+        int[] size; 
+
+        DisjointSetUnion(int n) {
+            parents = new int[n];
+            size = new int[n];
+
+            for(int i = 0 ; i < n ; i++) {
+                parents[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        int find(int x) {
+            if(parents[x] == x) return x;
+            return parents[x] = find(parents[x]);
+        }
+
+        boolean union(int u, int v) {
+            int u1 = find(u);
+            int v1 = find(v);
+
+            if(u1 == v1) {
+                return false;
+            }
+
+            if(size[u1] >= size[v1]) {
+                parents[v1] = u1;
+                size[u1] += size[v1];
+
+            } else {
+                parents[u1] = v1;
+                size[v1] += size[u1];
+            }
+
+            return true;
+
+        }
+    }
+
     public boolean validTree(int n, int[][] edges) {
-    
-    if (edges.length != n - 1) return false;
-    
 
-    //tp checkk disconnected ones
-    // Make the adjacency list.
-    List<List<Integer>> adjacencyList = new ArrayList<>();
-    for (int i = 0; i < n; i++) {
-        adjacencyList.add(new ArrayList<>());
-    }
-    for (int[] edge : edges) {
-        adjacencyList.get(edge[0]).add(edge[1]);
-        adjacencyList.get(edge[1]).add(edge[0]);
-    }
-    
-    Queue<Integer> queue = new LinkedList<>();
-    Set<Integer> seen = new HashSet<>();
-    queue.offer(0);
-    seen.add(0);
-    
-    while (!queue.isEmpty()) {
-        int node = queue.poll();
-
-        for (int neighbour : adjacencyList.get(node)) {
-            if (seen.contains(neighbour)) continue;
-
-            seen.add(neighbour);
-            queue.offer(neighbour);
+        DisjointSetUnion dsu = new DisjointSetUnion(n);
+        for(int[] edge : edges) {
+            if (!dsu.union(edge[0], edge[1])) return false;
         }
         
-    }
+     
+        int sim = dsu.parents[0];
 
-    return seen.size() == n;   
-}
+        for(int i=0; i<n; i++) {
+            dsu.find(i);
+        }
+
+        for(int each : dsu.parents) 
+            System.out.println(each);
+
+        for(int each : dsu.parents) {
+
+            if (each != sim) return false;
+        }
+
+        return true;
+    }
 }
