ac54aee4677a12407e5d4484eff4eabb3a901aef
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-21 14:14:21 +0530

    Time: 4 ms (91.56%), Space: 47.2 MB (34.37%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,39 +1,46 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] indegree = new int[numCourses];
-        List<List<Integer>> adj = new ArrayList<>(numCourses);
+    public boolean dfs(int node, List<List<Integer>> adj, boolean[] visit, boolean[] inStack) {
+        // If the node is already in the stack, we have a cycle.
+        if (inStack[node]) {
+            return true;
+        }
+        
+        if (visit[node]) {
+            return false;
+        }
 
+        // Mark the current node as visited and part of current recursion stack.
+        visit[node] = true;
+        inStack[node] = true;
+
+        for (int neighbor : adj.get(node)) {
+            if (dfs(neighbor, adj, visit, inStack)) {
+                return true;
+            }
+        }
+
+        // Remove the node from the stack.
+        inStack[node] = false;
+        return false;
+    }
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>(numCourses);
         for (int i = 0; i < numCourses; i++) {
             adj.add(new ArrayList<>());
         }
 
         for (int[] prerequisite : prerequisites) {
             adj.get(prerequisite[1]).add(prerequisite[0]);
-            indegree[prerequisite[0]]++;
         }
 
-        Queue<Integer> queue = new LinkedList<>();
-        // Push all the nodes with indegree zero in the queue.
+        boolean[] visit = new boolean[numCourses];
+        boolean[] inStack = new boolean[numCourses];
         for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                queue.offer(i);
+            if (dfs(i, adj, visit, inStack)) {
+                return false;
             }
         }
-
-        int nodesVisited = 0;
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-            nodesVisited++;
-
-            for (int neighbor : adj.get(node)) {
-                // Delete the edge "node -> neighbor".
-                indegree[neighbor]--;
-                if (indegree[neighbor] == 0) {
-                    queue.offer(neighbor);
-                }
-            }
-        }
-
-        return nodesVisited == numCourses;
+        return true;
     }
 }
