21534f8634542328ab8b369b6560057d580c8314
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-21 14:37:21 +0530

    Time: 1 ms (87.25%), Space: 47 MB (88.38%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,31 +1,47 @@
-class Solution {
+class UnionFind {
 
-    public void bfs(int node, int[][] isConnected, boolean[] visit) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(node);
-        visit[node] = true;
+    int[] parent;
+    int[] rank;
 
-        while (!q.isEmpty()) {
-            node = q.poll();
+    public UnionFind(int size) {
+        parent = new int[size];
+        for (int i = 0; i < size; i++) parent[i] = i;
+        rank = new int[size];
+    }
 
-            for (int i = 0; i < isConnected.length; i++) {
-                if (isConnected[node][i] == 1 && !visit[i]) {
-                    q.offer(i);
-                    visit[i] = true;
-                }
-            }
+    public int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    public void union_set(int x, int y) {
+        int xset = find(x), yset = find(y);
+        if (xset == yset) {
+            return;
+        } else if (rank[xset] < rank[yset]) {
+            parent[xset] = yset;
+        } else if (rank[xset] > rank[yset]) {
+            parent[yset] = xset;
+        } else {
+            parent[yset] = xset;
+            rank[xset]++;
         }
     }
+}
+
+class Solution {
 
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        int numberOfComponents = 0;
-        boolean[] visit = new boolean[n];
+        UnionFind dsu = new UnionFind(n);
+        int numberOfComponents = n;
 
         for (int i = 0; i < n; i++) {
-            if (!visit[i]) {
-                numberOfComponents++;
-                bfs(i, isConnected, visit);
+            for (int j = i + 1; j < n; j++) {
+                if (isConnected[i][j] == 1 && dsu.find(i) != dsu.find(j)) {
+                    numberOfComponents--;
+                    dsu.union_set(i, j);
+                }
             }
         }
 
