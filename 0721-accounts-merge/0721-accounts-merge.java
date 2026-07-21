b3da9527739aa25acf71bdeca0eb5174f3cd38a3
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-21 21:51:05 +0530

    Time: 32 ms (50.73%), Space: 50 MB (35.2%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -1,64 +1,98 @@
-class Solution {
-    HashSet<String> visited = new HashSet<>();
-    Map<String, List<String>> adjacent = new HashMap<String, List<String>>();
+class DSU {
+    int representative [];
+    int size [];
     
-    private void DFS(List<String> mergedAccount, String email) {
-        visited.add(email);
-        // Add the email vector that contains the current component's emails
-        mergedAccount.add(email);
+    DSU(int sz) {
+        representative = new int[sz];
+        size = new int[sz];
         
-        if (!adjacent.containsKey(email)) {
-            return;
-        }
-        
-        for (String neighbor : adjacent.get(email)) {
-            if (!visited.contains(neighbor)) {
-                DFS(mergedAccount, neighbor);
-            }
+        for (int i = 0; i < sz; ++i) {
+            // Initially each group is its own representative
+            representative[i] = i;
+            // Intialize the size of all groups to 1
+            size[i] = 1;
         }
     }
     
+    // Finds the representative of group x
+    public int findRepresentative(int x) {
+        if (x == representative[x]) {
+            return x;
+        }
+        
+        // This is path compression
+        return representative[x] = findRepresentative(representative[x]);
+    }
+    
+    // Unite the group that contains "a" with the group that contains "b"
+    public void unionBySize(int a, int b) {
+        int representativeA = findRepresentative(a);
+        int representativeB = findRepresentative(b);
+        
+        // If nodes a and b already belong to the same group, do nothing.
+        if (representativeA == representativeB) {
+            return;
+        }
+        
+        // Union by size: point the representative of the smaller
+        // group to the representative of the larger group.
+        if (size[representativeA] >= size[representativeB]) {
+            size[representativeA] += size[representativeB];
+            representative[representativeB] = representativeA;
+        } else {
+            size[representativeB] += size[representativeA];
+            representative[representativeA] = representativeB;
+        }
+    }
+}
+
+class Solution {
     public List<List<String>> accountsMerge(List<List<String>> accountList) {
         int accountListSize = accountList.size();
+        DSU dsu = new DSU(accountListSize);
         
-        for (List<String> account : accountList) {
-            int accountSize = account.size();
+        // Maps email to their component index
+        Map<String, Integer> emailGroup = new HashMap<>();
+        
+        for (int i = 0; i < accountListSize; i++) {
+            int accountSize = accountList.get(i).size();
             
-            // Building adjacency list
-            // Adding edge between first email to all other emails in the account
-            String accountFirstEmail = account.get(1);
-            for (int j = 2; j < accountSize; j++) {
-                String accountEmail = account.get(j);
+            for (int j = 1; j < accountSize; j++) {
+                String email = accountList.get(i).get(j);
+                String accountName = accountList.get(i).get(0);
                 
-                if (!adjacent.containsKey(accountFirstEmail)) {
-                    adjacent.put(accountFirstEmail, new ArrayList<String>());
+                // If this is the first time seeing this email then
+                // assign component group as the account index
+                if (!emailGroup.containsKey(email)) {
+                    emailGroup.put(email, i);
+                } else {
+                    // If we have seen this email before then union this
+                    // group with the previous group of the email
+                    dsu.unionBySize(i, emailGroup.get(email));
                 }
-                adjacent.get(accountFirstEmail).add(accountEmail);
-                
-                if (!adjacent.containsKey(accountEmail)) {
-                    adjacent.put(accountEmail, new ArrayList<String>());
-                }
-                adjacent.get(accountEmail).add(accountFirstEmail);
             }
         }
         
-        // Traverse over all th accounts to store components
-        List<List<String>> mergedAccounts = new ArrayList<>();
-        for (List<String> account : accountList) {
-            String accountName = account.get(0);
-            String accountFirstEmail = account.get(1);
+        // Store emails corresponding to the component's representative
+        Map<Integer, List<String>> components = new HashMap<Integer, List<String>>();
+        for (String email : emailGroup.keySet()) {
+            int group = emailGroup.get(email);
+            int groupRep = dsu.findRepresentative(group);
             
-            // If email is visited, then it's a part of different component
-            // Hence perform DFS only if email is not visited yet
-            if (!visited.contains(accountFirstEmail)) {
-                List<String> mergedAccount = new ArrayList<>();
-                // Adding account name at the 0th index
-                mergedAccount.add(accountName);
-                
-                DFS(mergedAccount, accountFirstEmail);
-                Collections.sort(mergedAccount.subList(1, mergedAccount.size())); 
-                mergedAccounts.add(mergedAccount);
+            if (!components.containsKey(groupRep)) {
+                components.put(groupRep, new ArrayList<String>());
             }
+            
+            components.get(groupRep).add(email);
+        }
+        
+        // Sort the components and add the account name
+        List<List<String>> mergedAccounts = new ArrayList<>();
+        for (int group : components.keySet()) {
+            List<String> component = components.get(group);
+            Collections.sort(component); 
+            component.add(0, accountList.get(group).get(0));
+            mergedAccounts.add(component);
         }
         
         return mergedAccounts;
