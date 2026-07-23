5584f56863c1c5d2c36bdc5a485920fe2c165c36
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-24 03:31:16 +0530

    Time: 1 ms (60.07%), Space: 48.5 MB (28.85%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,29 +1,29 @@
 class Solution {
     public int trap(int[] height) {
-        if (height == null || height.length == 0) return 0;
-        
+        // Case of empty height array
+        if (height.length == 0) return 0;
+        int ans = 0;
         int size = height.length;
-        int[] leftMax = new int[size];
-        int[] rightMax = new int[size];
-        
-        // Calculate the maximum height to the left of each bar
-        leftMax[0] = height[0];
+        // Create left and right max arrays
+        int[] left_max = new int[size];
+        int[] right_max = new int[size];
+        // Initialize first height into left max
+        left_max[0] = height[0];
         for (int i = 1; i < size; i++) {
-            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+            // update left max with current max
+            left_max[i] = Math.max(height[i], left_max[i - 1]);
         }
-
-        // Calculate the maximum height to the right of each bar
-        rightMax[size - 1] = height[size - 1];
+        // Initialize last height into right max
+        right_max[size - 1] = height[size - 1];
         for (int i = size - 2; i >= 0; i--) {
-            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+            // update right max with current max
+            right_max[i] = Math.max(height[i], right_max[i + 1]);
         }
-        
-        int water = 0;
-        // Calculate the amount of water trapped
-        for (int i = 0; i < size; i++) {
-            water += Math.max(0, Math.min(leftMax[i], rightMax[i]) - height[i]);
+        // Calculate the trapped water
+        for (int i = 1; i < size - 1; i++) {
+            ans += Math.min(left_max[i], right_max[i]) - height[i];
         }
-        
-        return water;
+        // Return amount of trapped water
+        return ans;
     }
 }
