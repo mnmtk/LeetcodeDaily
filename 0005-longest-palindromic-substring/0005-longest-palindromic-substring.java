643b19a39bfc07c9e14d8016715529a5113457bf
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-24 03:25:29 +0530

    Time: 15 ms (59.02%), Space: 43.6 MB (69.39%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -4,6 +4,7 @@ class Solution {
 
         for (int i = 0; i < s.length(); i++) {
             int oddLength = expand(i, i, s);
+            
             if (oddLength > ans[1] - ans[0] + 1) {
                 int dist = oddLength / 2;
                 ans[0] = i - dist;
@@ -11,6 +12,7 @@ class Solution {
             }
 
             int evenLength = expand(i, i + 1, s);
+            
             if (evenLength > ans[1] - ans[0] + 1) {
                 int dist = (evenLength / 2) - 1;
                 ans[0] = i - dist;
