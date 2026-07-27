e14009ffb442636e463b6cfc160d4ef239564f96
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-27 20:30:39 +0530

    Time: 13 ms (99.95%), Space: 42.1 MB (35.94%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,20 +3,18 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-
-        int left = 1; 
+        int left = 1;
         int right = n;
 
-        while(left < right) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
-
-            if(isBadVersion(mid)) {
+            if (isBadVersion(mid)) {
                 right = mid;
             } else {
                 left = mid + 1;
             }
         }
-
+        
         return left;
     }
 }
