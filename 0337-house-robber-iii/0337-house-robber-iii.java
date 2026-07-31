3151d83010c8b1fc2247a25395529c5ebb2cd30d
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-31 16:10:30 +0530

    Time: 1 ms (45.73%), Space: 46.4 MB (73.23%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.java
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -1,36 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    HashMap<TreeNode, Integer> robResult = new HashMap<>();
-    HashMap<TreeNode, Integer> notRobResult = new HashMap<>();
+    public int rob(TreeNode root) {
 
-    public int helper(TreeNode node, boolean parentRobbed) {
-        if (node == null) {
-            return 0;
-        }
-        if (parentRobbed) {
-            if (robResult.containsKey(node)) {
-                return robResult.get(node);
-            }
+        int[] result = helper(root);
 
-            int result = helper(node.left, false) + helper(node.right, false);
-            robResult.put(node, result);
-
-            return result;
-        } else {
-            if (notRobResult.containsKey(node)) {
-                return notRobResult.get(node);
-            }
-
-            int rob = node.val + helper(node.left, true) + helper(node.right, true);
-            int notRob = helper(node.left, false) + helper(node.right, false);
-            
-            int result = Math.max(rob, notRob);
-            notRobResult.put(node, result);
-
-            return result;
-        }
+        return Math.max(result[0], result[1]);
     }
 
-    public int rob(TreeNode root) {
-        return helper(root, false);
+    private int[] helper(TreeNode node) {
+        if(node == null) {
+            return new int[] {0, 0};
+        }
+
+
+        int[] left = helper(node.left);
+        int[] right = helper(node.right);
+
+        int withoutNode = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+
+        int withNode = node.val + left[0] + right[0];
+
+        return new int[]{withoutNode, withNode};
     }
 }
