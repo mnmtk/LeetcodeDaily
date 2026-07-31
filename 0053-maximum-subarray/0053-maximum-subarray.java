b22330328b5249da9fc58904b6bd879da2a343f3
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-31 17:03:56 +0530

    Time: 1 ms (99.95%), Space: 77.1 MB (83.35%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,15 +1,17 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        //at each point i can either include and move or leave previois and move.
+        // Initialize our variables using the first element.
+        int currentSubarray = nums[0];
+        int maxSubarray = nums[0];
 
-        int max = nums[0];
-        int sum = 0;
-        for(int num : nums) {
-            int currMax = Math.max(sum + num, num);
-            max = Math.max(currMax, max);
-            sum = currMax;
+        // Start with the 2nd element since we already used the first one.
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            // If current_subarray is negative, throw it away. Otherwise, keep adding to it.
+            currentSubarray = Math.max(num, currentSubarray + num);
+            maxSubarray = Math.max(maxSubarray, currentSubarray);
         }
 
-        return max;
+        return maxSubarray;
     }
 }
