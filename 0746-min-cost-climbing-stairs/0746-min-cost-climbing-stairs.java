ecffc519b5aab728ec228c086b195995437c517e
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-31 14:07:00 +0530

    Time: 1 ms (39.25%), Space: 44.9 MB (59.11%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,25 +1,30 @@
-class Solution {
-    private HashMap<Integer, Integer> memo = new HashMap<Integer, Integer>();
+import java.util.Arrays;
 
+class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        return minimumCost(cost.length, cost);
+        int n = cost.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+
+        // You can start at index 0 OR index 1
+        return Math.min(min_cost(0, cost, dp), min_cost(1, cost, dp)); 
     }
 
-    private int minimumCost(int i, int[] cost) {
-        // Base case, we are allowed to start at either step 0 or step 1
-        if (i <= 1) {
+    private int min_cost(int index, int[] cost, int[] dp) {
+        // Base case: Reached or surpassed the top floor
+        if (index >= cost.length) {
             return 0;
         }
 
-        // Check if we have already calculated minimumCost(i)
-        if (memo.containsKey(i)) {
-            return memo.get(i);
+        // Return memoized result if already computed
+        if (dp[index] != -1) {
+            return dp[index];
         }
 
-        // If not, cache the result in our hash map and return it
-        int downOne = cost[i - 1] + minimumCost(i - 1, cost);
-        int downTwo = cost[i - 2] + minimumCost(i - 2, cost);
-        memo.put(i, Math.min(downOne, downTwo));
-        return memo.get(i);
+        // Cost for current step + min cost of taking 1 or 2 steps forward
+        int oneStep = min_cost(index + 1, cost, dp);
+        int twoStep = min_cost(index + 2, cost, dp);
+
+        return dp[index] = cost[index] + Math.min(oneStep, twoStep);
     }
 }
