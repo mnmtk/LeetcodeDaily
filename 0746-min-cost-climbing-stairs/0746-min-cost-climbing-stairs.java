76812993bd3c321e2b03917e66404b3acbb09480
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-31 14:07:33 +0530

    Time: 0 ms (100%), Space: 44.9 MB (59.11%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,30 +1,14 @@
-import java.util.Arrays;
-
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int n = cost.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
+        int prev2 = cost[0];
+        int prev1 = cost[1];
 
-        // You can start at index 0 OR index 1
-        return Math.min(min_cost(0, cost, dp), min_cost(1, cost, dp)); 
-    }
-
-    private int min_cost(int index, int[] cost, int[] dp) {
-        // Base case: Reached or surpassed the top floor
-        if (index >= cost.length) {
-            return 0;
+        for (int i = 2; i < cost.length; i++) {
+            int current = cost[i] + Math.min(prev1, prev2);
+            prev2 = prev1;
+            prev1 = current;
         }
 
-        // Return memoized result if already computed
-        if (dp[index] != -1) {
-            return dp[index];
-        }
-
-        // Cost for current step + min cost of taking 1 or 2 steps forward
-        int oneStep = min_cost(index + 1, cost, dp);
-        int twoStep = min_cost(index + 2, cost, dp);
-
-        return dp[index] = cost[index] + Math.min(oneStep, twoStep);
+        return Math.min(prev1, prev2);
     }
 }
