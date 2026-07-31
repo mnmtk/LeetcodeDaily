bfaf1eeb930c06061471ff3ac8080de27b2b8088
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-07-31 16:15:17 +0530

    Time: 0 ms (100%), Space: 46.5 MB (73.23%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.java
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -15,20 +15,20 @@
  */
 class Solution {
     public int rob(TreeNode root) {
-
         int[] result = helper(root);
 
         return Math.max(result[0], result[1]);
     }
 
-    private int[] helper(TreeNode node) {
-        if(node == null) {
-            return new int[] {0, 0};
-        }
+    public int[] helper(TreeNode node) {
 
+        if(node == null) {
+            return new int[]{0, 0};
+        }
 
         int[] left = helper(node.left);
         int[] right = helper(node.right);
+
 
         int withoutNode = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
 
