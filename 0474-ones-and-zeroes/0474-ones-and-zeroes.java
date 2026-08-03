f99b5817781562a00efb99263de644adef1a9af2
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-08-03 17:05:14 +0530

    Time: 28 ms (53.66%), Space: 43.7 MB (68.44%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.java
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.java
@@ -1,20 +1,13 @@
 public class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
-        int[][][] memo = new int[strs.length][m + 1][n + 1];
-        return calculate(strs, 0, m, n, memo);
-    }
-    public int calculate(String[] strs, int i, int zeroes, int ones, int[][][] memo) {
-        if (i == strs.length)
-            return 0;
-        if (memo[i][zeroes][ones] != 0)
-            return memo[i][zeroes][ones];
-        int[] count = countzeroesones(strs[i]);
-        int taken = -1;
-        if (zeroes - count[0] >= 0 && ones - count[1] >= 0)
-            taken = calculate(strs, i + 1, zeroes - count[0], ones - count[1], memo) + 1;
-        int not_taken = calculate(strs, i + 1, zeroes, ones, memo);
-        memo[i][zeroes][ones] = Math.max(taken, not_taken);
-        return memo[i][zeroes][ones];
+        int[][] dp = new int[m + 1][n + 1];
+        for (String s: strs) {
+            int[] count = countzeroesones(s);
+            for (int zeroes = m; zeroes >= count[0]; zeroes--)
+                for (int ones = n; ones >= count[1];  ones--)
+                    dp[zeroes][ones] = Math.max(1 + dp[zeroes - count[0]][ones - count[1]], dp[zeroes][ones]);
+        }
+        return dp[m][n];
     }
     public int[] countzeroesones(String s) {
         int[] c = new int[2];
