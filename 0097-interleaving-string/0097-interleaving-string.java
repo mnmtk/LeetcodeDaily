fdd75bdba42f36ac8beced621454c82c7310fbc3
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-08-09 21:23:49 +0530

    Time: 5 ms (38.75%), Space: 43.4 MB (39.59%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -7,6 +7,7 @@ public class Solution {
         
         for (int i = 0; i <= s1.length(); i++) {
             for (int j = 0; j <= s2.length(); j++) {
+                
                 if (i == 0 && j == 0) {
                     dp[i][j] = true;
                 } else if (i == 0) {
