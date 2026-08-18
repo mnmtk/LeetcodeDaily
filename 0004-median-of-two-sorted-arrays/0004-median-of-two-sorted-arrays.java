322ce5c45a9137652ea74ef40d1f5f5400dca0b4
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-08-19 01:54:12 +0530

    Time: 1 ms (100%), Space: 48.8 MB (52.62%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,58 +1,31 @@
 class Solution {
-     public double findMedianSortedArrays(int[] A, int[] B) {
-        int na = A.length, nb = B.length;
-        int n = na + nb;
-        if ((na + nb) % 2 == 1) {
-            return solve(A, B, n / 2, 0, na - 1, 0, nb - 1);
-        } else {
-            return (
-                (double) (solve(A, B, n / 2, 0, na - 1, 0, nb - 1) +
-                    solve(A, B, n / 2 - 1, 0, na - 1, 0, nb - 1)) /
-                2
-            );
+    private int p1 = 0, p2 = 0;
+
+    // Get the smaller value between nums1[p1] and nums2[p2] and move the pointer forwards.
+
+    private int getMin(int[] nums1, int[] nums2) {
+        if (p1 < nums1.length && p2 < nums2.length) {
+            return nums1[p1] < nums2[p2] ? nums1[p1++] : nums2[p2++];
+        } else if (p1 < nums1.length) {
+            return nums1[p1++];
+        } else if (p2 < nums2.length) {
+            return nums2[p2++];
         }
+        return -1;
     }
 
-    public int solve(
-        int[] A,
-        int[] B, 
-        int k, 
-        int aStart,
-        int aEnd,
-        int bStart,
-        int bEnd
-    ) {
-        if(aEnd < aStart) {
-            return B[k - aStart]; 
-        }
-
-        if(bEnd < bStart) {
-            return A[k - bStart];
-        }
-
-        int aIndex = (aStart + aEnd)/ 2 ;
-        int bIndex = (bStart + bEnd)/ 2;
-
-        int aValue = A[aIndex];
-        int bValue = B[bIndex];
-
-        if(aIndex + bIndex < k) {
-            // agar k mids se bada hai
-            // to point smaller part of smaller array mei nahi ho sakta
-            // isliye ham usko let go krdege.
-            if(aValue < bValue) {
-                return solve(A, B, k, aIndex + 1, aEnd, bStart, bEnd);
-            } else {
-                return solve(A, B, k, aStart, aEnd, bIndex + 1, bEnd);
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        if ((m + n) % 2 == 0) {
+            for (int i = 0; i < (m + n) / 2 - 1; ++i) {
+                int tmp = getMin(nums1, nums2);
             }
-            //agar point chota hai
+            return (double) (getMin(nums1, nums2) + getMin(nums1, nums2)) / 2;
         } else {
-            if(aValue < bValue) {
-            return solve(A, B, k, aStart, aEnd, bStart, bIndex - 1);
-            } else{
-                  return solve(A, B, k, aStart, aIndex - 1, bStart, bEnd);
+            for (int i = 0; i < (m + n) / 2; ++i) {
+                int tmp = getMin(nums1, nums2);
             }
+            return getMin(nums1, nums2);
         }
-
     }
 }
