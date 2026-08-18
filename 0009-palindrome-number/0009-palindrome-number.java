fcca3cedfe55d5dcdb7e4e70a03623f7b95f7472
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-08-18 23:25:48 +0530

    Time: 5 ms (81.45%), Space: 45.9 MB (72.55%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,15 +1,17 @@
 public class Solution {
     public boolean isPalindrome(int x) {
+
+        int num = x;
         
         if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
 
         int revertedNumber = 0;
-        while (x > revertedNumber) {
+        while (x > 0) {
             revertedNumber = revertedNumber * 10 + (x % 10);
             x /= 10;
         }
-        return x == revertedNumber || x == revertedNumber / 10;
+        return num == revertedNumber;
     }
 }
