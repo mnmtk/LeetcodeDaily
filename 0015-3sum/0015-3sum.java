62e6bd821ac29f511fe95e6469c1f4e88cb1f533
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-08-19 03:00:16 +0530

    Time: 27 ms (98.83%), Space: 59.3 MB (45.76%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,7 +2,7 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < nums.length ; ++i) if (
+        for (int i = 0; i < nums.length && nums[i] <= 0; ++i) if (
             i == 0 || nums[i - 1] != nums[i]
         ) {
             twoSumII(nums, i, res);
