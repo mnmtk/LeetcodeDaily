b746b2489dc13a1ee2bbb1cc0b649ad97903e3f4
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-08-19 03:07:04 +0530

    Time: 450 ms (12.31%), Space: 55.5 MB (97.6%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,9 +3,13 @@ class Solution {
         Set<List<Integer>> res = new HashSet<>();
         Set<Integer> dups = new HashSet<>();
         Map<Integer, Integer> seen = new HashMap<>();
-        for (int i = 0; i < nums.length; ++i) if (dups.add(nums[i])) {
+        for (int i = 0; i < nums.length; ++i) 
+        if (dups.add(nums[i])) {
+
             for (int j = i + 1; j < nums.length; ++j) {
+
                 int complement = -nums[i] - nums[j];
+
                 if (seen.containsKey(complement) && seen.get(complement) == i) {
                     List<Integer> triplet = Arrays.asList(
                         nums[i],
@@ -15,8 +19,11 @@ class Solution {
                     Collections.sort(triplet);
                     res.add(triplet);
                 }
+
+                
                 seen.put(nums[j], i);
             }
+
         }
         return new ArrayList(res);
     }
