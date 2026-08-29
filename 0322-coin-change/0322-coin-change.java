eb3f910f79133403fb71d2f35bf1ba5d03773c15
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-08-30 02:04:00 +0530

    Time: 10 ms (99.77%), Space: 46.4 MB (52.06%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,6 +1,6 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int max = Integer.MAX_VALUE - 1;   // now INF + 1 doesn't wrap
+        int max = amount + 1;
 
         int[] dp = new int[amount + 1];
 
@@ -14,6 +14,6 @@ class Solution {
             }
         }
 
-        return dp[amount] >= Integer.MAX_VALUE - 1 ? -1 : dp[amount];
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
