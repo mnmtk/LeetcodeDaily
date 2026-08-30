4f1c5b736a58eb6d212b68d3f4eb3a10949e4975
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-08-30 16:50:34 +0530

    Time: 0 ms (100%), Space: 42.4 MB (96.24%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,30 +1,19 @@
 class Solution {
-    int[][] memo;
-    
-    private int maxDiff(int[] nums, int left, int right) {
-        if (memo[left][right] != -1) {
-            return memo[left][right];
-        }
-        if (left == right) {
-            return nums[left];
-        }
-        
-        int scoreByLeft = nums[left] - maxDiff(nums, left + 1, right);
-        int scoreByRight = nums[right] - maxDiff(nums, left, right - 1);
-        
-        memo[left][right] = Math.max(scoreByLeft, scoreByRight);
-        
-        return memo[left][right];
-    }
-    
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
-
-        memo = new int[n][n];
+        int[][] dp = new int[n][n];
         for (int i = 0; i < n; ++i) {
-            Arrays.fill(memo[i], -1);
+            dp[i][i] = nums[i];
         }
         
-        return maxDiff(nums, 0, n - 1) >= 0;
+        for (int diff = 1; diff < n; ++diff) {
+            for (int left = 0; left < n - diff; ++left) {
+                int right = left + diff;
+                dp[left][right] = Math.max(nums[left] - dp[left + 1][right],
+                                          nums[right] - dp[left][right - 1]);
+            }
+        }
+        
+        return dp[0][n - 1] >= 0;
     }
 }
