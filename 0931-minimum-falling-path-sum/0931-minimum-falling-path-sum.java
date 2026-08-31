c44ee100964c0026a28c0b79389fc77347f8c8cb
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-09-01 01:14:10 +0530

    Time: 6 ms (59.71%), Space: 47 MB (79.37%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,36 +1,24 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int minFallingSum = Integer.MAX_VALUE;
-        Integer memo[][] = new Integer[matrix.length][matrix[0].length];
+        int dp[][] = new int[matrix.length + 1][matrix.length + 1];
 
-        // start a DFS (with memoization) from each cell in the top row
+        for (int row = matrix.length - 1; row >= 0; row--) {
+            for (int col = 0; col < matrix.length; col++) {
+                if (col == 0) {
+                    dp[row][col] = Math.min(dp[row + 1][col], dp[row + 1][col + 1]) + matrix[row][col];
+                } else if (col == matrix.length - 1) {
+                    dp[row][col] = Math.min(dp[row + 1][col], dp[row + 1][col - 1]) + matrix[row][col];
+                } else {
+                    dp[row][col] = Math.min(dp[row + 1][col],
+                            Math.min(dp[row + 1][col + 1], dp[row + 1][col - 1])) + matrix[row][col];
+                }
+            }
+        }
+        
+        int minFallingSum = Integer.MAX_VALUE;
         for (int startCol = 0; startCol < matrix.length; startCol++) {
-            minFallingSum = Math.min(minFallingSum,
-                findMinFallingPathSum(matrix, 0, startCol, memo));
+            minFallingSum = Math.min(minFallingSum, dp[0][startCol]);
         }
         return minFallingSum;
-    }
-
-    public int findMinFallingPathSum(int[][] matrix, int row, int col, Integer[][] memo) {
-        //base cases
-        if (col < 0 || col == matrix.length) {
-            return Integer.MAX_VALUE;
-        }
-        //check if we have reached the last row
-        if (row == matrix.length - 1) {
-            return matrix[row][col];
-        }
-        //check if the results are calculated before
-        if (memo[row][col] != null) {
-            return memo[row][col];
-        }
-
-        // calculate the minimum falling path sum starting from each possible next step
-        int left = findMinFallingPathSum(matrix, row + 1, col, memo);
-        int middle = findMinFallingPathSum(matrix, row + 1, col + 1, memo);
-        int right = findMinFallingPathSum(matrix, row + 1, col - 1, memo);
-
-        memo[row][col] = Math.min(left, Math.min(middle, right)) + matrix[row][col];
-        return memo[row][col];
     }
 }
