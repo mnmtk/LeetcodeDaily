861bd2355ff8d091f399502909115fd68be8b155
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-09-25 23:39:38 +0530

    Time: 148 ms (5.14%), Space: 95.2 MB (5.4%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,44 +1,51 @@
 class Solution {
-    public boolean isMatch(String s, String p) {
-        int sLen = s.length(), pLen = p.length();
+    Map<String, Boolean> dp = new HashMap<>();
+    String s;
+    String p;
 
-        int sIdx = 0;
-        int pIdx = 0;
-        
-        int starIdx = -1, sTmpIdx = -1;
+    String remove_duplicate_stars(String p) {
 
-        while (sIdx < sLen) {
+        StringBuilder new_string = new StringBuilder();
 
-            if (pIdx < pLen && (p.charAt(pIdx) == '?' || p.charAt(pIdx) == s.charAt(sIdx)) ) { 
-
-                ++sIdx;
-                ++pIdx;
-
-            } else if (pIdx < pLen && p.charAt(pIdx) == '*') {
-
-                starIdx = pIdx;
-                sTmpIdx = sIdx;
-                ++pIdx;
-
-            } else if (starIdx == -1) {
-
-                return false;
-
-            } else {
-
-                pIdx = starIdx + 1;
-                sIdx = sTmpIdx + 1;
-                sTmpIdx = sIdx;
-
-            }
+        for (char c : p.toCharArray()) {
+            if (new_string.length() == 0 || c != '*') new_string.append(c);
+            else if (
+                new_string.charAt(new_string.length() - 1) != '*'
+            ) new_string.append(c);
         }
 
-        for (int i = pIdx; i < pLen; i++) {
-            if (p.charAt(i) != '*') {
-                return false;
-            }
-        }
-
-        return true;
+        return new_string.toString();
     }
+
+    boolean helper(int si, int pi) {
+        String key = si + "," + pi;
+
+        if (dp.containsKey(key)) return dp.get(key);
+
+        if (pi == p.length()) dp.put(key, si == s.length());
+
+        else if (si == s.length()) dp.put(
+            key,
+            (pi + 1 == p.length() && p.charAt(pi) == '*')
+        );
+        
+        else if (p.charAt(pi) == s.charAt(si) || p.charAt(pi) == '?') dp.put(
+            key,
+            helper(si + 1, pi + 1)
+        );
+        else if (p.charAt(pi) == '*') dp.put(
+            key,
+            helper(si, pi + 1) || helper(si + 1, pi)
+        );
+        else dp.put(key, false);
+        return dp.get(key);
+    }
+
+    public boolean isMatch(String s, String p) {
+        dp.clear();
+        this.s = s;
+        this.p = remove_duplicate_stars(p);
+        return helper(0, 0);
+    }
+
 }
