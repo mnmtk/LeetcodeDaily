29c1169c672917b25706cdb4d13fdbaa00565504
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-09-25 20:33:52 +0530

    Time: 2 ms (92.98%), Space: 45.2 MB (92.75%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int sLen = s.length(), pLen = p.length();
+
+        int sIdx = 0;
+        int pIdx = 0;
+        
+        int starIdx = -1, sTmpIdx = -1;
+
+        while (sIdx < sLen) {
+
+            if (pIdx < pLen && (p.charAt(pIdx) == '?' || p.charAt(pIdx) == s.charAt(sIdx)) ) { 
+
+                ++sIdx;
+                ++pIdx;
+
+            } else if (pIdx < pLen && p.charAt(pIdx) == '*') {
+
+                starIdx = pIdx;
+                sTmpIdx = sIdx;
+                ++pIdx;
+
+            } else if (starIdx == -1) {
+
+                return false;
+
+            } else {
+
+                pIdx = starIdx + 1;
+                sIdx = sTmpIdx + 1;
+                sTmpIdx = sIdx;
+
+            }
+        }
+
+        for (int i = pIdx; i < pLen; i++) {
+            if (p.charAt(i) != '*') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
