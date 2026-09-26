44251852d488218a0adfaae07078ea4168ac9fcb
mnmtk <137998025+mnmtk@users.noreply.github.com>
2026-09-26 14:31:24 +0530

    Time: 3 ms (5.35%), Space: 48.4 MB (30.97%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,29 +1,23 @@
-class Solution {
+public class Solution {
     public int trap(int[] height) {
-        // Case of empty height array
-        if (height.length == 0) return 0;
-        int ans = 0;
-        int size = height.length;
-        // Create left and right max arrays
-        int[] left_max = new int[size];
-        int[] right_max = new int[size];
-        // Initialize first height into left max
-        left_max[0] = height[0];
-        for (int i = 1; i < size; i++) {
-            // update left max with current max
-            left_max[i] = Math.max(height[i], left_max[i - 1]);
+        int ans = 0, current = 0;
+        Deque<Integer> st = new LinkedList<Integer>();
+        while (current < height.length) {
+            while (!st.isEmpty() && height[current] > height[st.peek()]) {
+                
+                int top = st.peek();
+                st.pop();
+
+                if (st.isEmpty()) break;
+                int distance = current - st.peek() - 1;
+
+                int bounded_height =
+                    Math.min(height[current], height[st.peek()]) - height[top];
+
+                ans += distance * bounded_height;
+            }
+            st.push(current++);
         }
-        // Initialize last height into right max
-        right_max[size - 1] = height[size - 1];
-        for (int i = size - 2; i >= 0; i--) {
-            // update right max with current max
-            right_max[i] = Math.max(height[i], right_max[i + 1]);
-        }
-        // Calculate the trapped water
-        for (int i = 1; i < size - 1; i++) {
-            ans += Math.min(left_max[i], right_max[i]) - height[i];
-        }
-        // Return amount of trapped water
         return ans;
     }
 }
